0a017dd1bb1a2f630bcff01ebdb54399be3ce952
Houtan Bastani <houtan@dynare.org>
2016-12-28 12:48:30 +0100

    var test, add estimation for nkm model, fix plotting

--- tests/ECB/run_var_comparison.m
+++ tests/ECB/run_var_comparison.m
@@ -13,7 +13,7 @@ save('nkm_saved_data.mat')
 %% Estimate VAR using simulated data
 disp('VAR Estimation');
 
-
+%% UNCOMMENT IF RUNNING EXAMPLE1
 % MLS estimate of mu and B (autoregressive_matrices)
 % Y = mu + B*Z
 % from New Introduction to Multiple Time Series Analysis
@@ -32,6 +32,17 @@ mu = B(:, 1);
 autoregressive_matrices{1} = B(:, 2:3);
 autoregressive_matrices{2} = B(:, 4:5);
 autoregressive_matrices{3} = B(:, 6:7);
+
+%% UNCOMMENT IF RUNNING ESTIMATION FOR NKM
+% % FOR NKM
+% Y = oo_.endo_simul(2:3, 2:end);
+% Z = [ ...
+%     ones(1, size(Y,2)); ...
+%     oo_.endo_simul(2:3, 1:end-1); ...
+%     ];
+% B = Y*Z'/(Z*Z');
+% mu = B(:, 1);
+% autoregressive_matrices{1} = B(:, 2:3);
 
 % Sims
 % (provides same result as above)
@@ -66,9 +77,9 @@ for i = 1:length(exo_names)
     for j = 1:length(endo_names)
         subplot(ridx, cidx, j);
         hold on
-        title(endo_names(j));
-        plot(nv.oo_.irfs.([endo_names(j) '_' exo_names(i)]));
-        plot(wv.oo_.irfs.([endo_names(j) '_' exo_names(i)]), '--');
+        title(endo_names(j,:));
+        plot(nv.oo_.irfs.([deblank(endo_names(j,:)) '_' deblank(exo_names(i,:))]));
+        plot(wv.oo_.irfs.([deblank(endo_names(j,:)) '_' deblank(exo_names(i,:))]), '--');
         hold off
     end
 end
