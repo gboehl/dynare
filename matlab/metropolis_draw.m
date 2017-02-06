c02de3c652e89cc794841f668635d1140a8a97fa
Marco Ratto <marco.ratto@jrc.ec.europa.eu>
2017-02-06 08:22:39 +0100

    fixed text to error message

--- matlab/metropolis_draw.m
+++ matlab/metropolis_draw.m
@@ -74,7 +74,7 @@ if init
     else
         if options_.sub_draws>NumberOfDraws*mh_nblck
             skipline()
-            disp(['Estimation::mcmc: The value of option sub_draws (' num2str(options_.sub_draws) ') is greater than the number of available draws in the MCMC (' num2str(NumberOfDraws) ')!'])
+            disp(['Estimation::mcmc: The value of option sub_draws (' num2str(options_.sub_draws) ') is greater than the number of available draws in the MCMC (' num2str(NumberOfDraws*mh_nblck) ')!'])
             disp('Estimation::mcmc: You can either change the value of sub_draws, reduce the value of mh_drop, or run another mcmc (with the load_mh_file option).')
             skipline()
             xparams = 1; % xparams is interpreted as an error flag
