gboehl/dynare

commit cad7adc01ffcfb7ce2f516f383214c22d2510cd3

Added routine for testing if we are currently estimating a model.

matlab/utilities/general/isestimation.m
@@ -1,0 +1,26 @@
+function b = isestimation(option)
+
+% Returns true if we are currently estimating a model.
+
+% Copyright (C) 2017 Dynare Team
+%
+% This file is part of Dynare.
+%
+% Dynare is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Dynare is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
+
+b = false;
+
+if option.mode_compute || option.mh_replic
+    b = true;
+end

commit 06326d4477edb8370ca907d518dfba8a7cc2d93c

Fixed bug introduced in cad7adc01ffcfb7ce2f516f383214c22d2510cd3.

matlab/utilities/general/isestimation.m
@@ -21,6 +21,6 @@ function b = isestimation(option)
 
 b = false;
 
-if option.mode_compute || option.mh_replic
+if ischar(option.mode_compute) || option.mode_compute || option.mh_replic
     b = true;
 end