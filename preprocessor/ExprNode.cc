gboehl/dynare

commit 3ea37bb2f83b2011d6fb6ed24842b3223aecf1fc

preprocessor: re-implement 2bb97a90d6a346244ca992b9caff6e4c55dd7f66

preprocessor/ExprNode.cc
@@ -769,10 +769,10 @@ VariableNode::writeOutput(ostream &output, ExprNodeOutputType output_type,
           break;
         case oMatlabOutsideModel:
           assert(lag == 0);
-          output <<  "oo_.exo_steady_state(" << i << ")";
+          output <<  "steady_state_x(" << i << ")";
           break;
         case oMatlabDynamicSteadyStateOperator:
-          output <<  "oo_.exo_steady_state(" << i << ")";
+          output <<  "steady_state_x(" << i << ")";
           break;
         case oJuliaSteadyStateFile:
         case oSteadyStateFile:

preprocessor/DynamicModel.cc
@@ -1527,7 +1527,7 @@ DynamicModel::writeDynamicMFile(const string &dynamic_basename) const
       cerr << "Error: Can't open file " << filename << " for writing" << endl;
       exit(EXIT_FAILURE);
     }
-  mDynamicModelFile << "function [residual, g1, g2, g3] = " << dynamic_basename << "(y, x, params, steady_state, it_)" << endl
+  mDynamicModelFile << "function [residual, g1, g2, g3] = " << dynamic_basename << "(y, x, params, steady_state, steady_state_x, it_)" << endl
                     << "%" << endl
                     << "% Status : Computes dynamic model for Dynare" << endl
                     << "%" << endl

commit ee783a1f792957064e22e012bc180a0c7bd07dd7

preprocessor: fix bug introduced in 3ea37bb2f83b2011d6fb6ed24842b3223aecf1fc

preprocessor/ExprNode.cc
@@ -769,7 +769,7 @@ VariableNode::writeOutput(ostream &output, ExprNodeOutputType output_type,
           break;
         case oMatlabOutsideModel:
           assert(lag == 0);
-          output <<  "steady_state_x(" << i << ")";
+          output << "oo_.exo_steady_state(" << i << ")";
           break;
         case oMatlabDynamicSteadyStateOperator:
           output <<  "steady_state_x(" << i << ")";