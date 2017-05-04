2e9152fed0dc7b1b02b1382feb56128f1b9ff853
Houtan Bastani <houtan@dynare.org>
2017-05-04 16:54:47 +0200

    preprocessor: clean up commit 63180c9e5922d3f198684647051231344486c55d

--- preprocessor/DynamicModel.cc
+++ preprocessor/DynamicModel.cc
@@ -3239,10 +3239,7 @@ DynamicModel::addEquationsForVar(map<string, pair<SymbolList, int> > var_model_i
     {
       map<string, int>::const_iterator it1 = model_endos_and_lags.find(it->first);
       if (it1 == model_endos_and_lags.end())
-        {
-          cerr << "WARNING: Variable used in var that is not used in the model: " << it->first << endl;
-          //          exit(EXIT_FAILURE);
-        }
+        cerr << "WARNING: Variable used in VAR that is not used in the model: " << it->first << endl;
       else
         if (it->second < it1->second)
           {
