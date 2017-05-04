d4376c1dfef87d162bc5f9043097d817a9800488
Houtan Bastani <houtan@dynare.org>
2017-05-04 16:32:21 +0200

    var_model: fix comments, copyright

--- matlab/writeVarExpectationFunction.m
+++ matlab/writeVarExpectationFunction.m
@@ -1,5 +1,33 @@
 function writeVarExpectationFunction(var_model_name, horizon)
-%function writeVarExpectationFunction(model_name)
+% function writeVarExpectationFunction(var_model_name, horizon)
+% Writes the var_forecast_<<var_model_name>>.m file
+%
+% INPUTS
+%
+%   var_model_name   [string]        the name of the VAR model
+%
+%   horizon          [int]           the forecast horizon
+%    
+% OUTPUTS
+%
+%   NONE
+
+% Copyright (C) 2017 Dynare Team
+%
+% This file is part of Dynare.
+%
+% Dynare is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Dynare is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
 
 %%
 global M_;
