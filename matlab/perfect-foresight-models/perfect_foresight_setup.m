gboehl/dynare

commit 97b894a1cdb2f47171e42448c4a6f416d26c8f31

Provide meaningful error message if oo_.exo_simul is larger than options_.periods

matlab/perfect-foresight-models/perfect_foresight_setup.m
@@ -51,6 +51,10 @@ if options_.periods == 0
     error('PERFECT_FORESIGHT_SETUP: number of periods for the simulation isn''t specified')
 end
 
+if options_.periods<max([M_.det_shocks.periods])
+    error('PERFECT_FORESIGHT_SETUP: number of periods for the simulation is specified to be smaller than the period of the last shock')
+end
+
 if ~options_.initval_file
     if isempty(options_.datafile)
         oo_=make_ex_(M_,options_,oo_);

commit 10047feee5e04610606bd6b65517d794a4a4aaf1

Test if shocks are declared.
Fixes bug introduced in 97b894a1cdb2f47171e42448c4a6f416d26c8f31, if no shocks
are declared (for instance if the user is interested in the transition to the
steady state).

matlab/perfect-foresight-models/perfect_foresight_setup.m
@@ -51,7 +51,7 @@ if options_.periods == 0
     error('PERFECT_FORESIGHT_SETUP: number of periods for the simulation isn''t specified')
 end
 
-if options_.periods<max([M_.det_shocks.periods])
+if ~isempty(M_.det_shocks) && options_.periods<max([M_.det_shocks.periods])
     error('PERFECT_FORESIGHT_SETUP: number of periods for the simulation is specified to be smaller than the period of the last shock')
 end
 