1a4257ac4bd75f1db6988a650ef79c2cb9f4cc40
Stéphane Adjemian (Scylla) <stephane.adjemian@univ-lemans.fr>
2017-08-16 15:44:00 +0200

    Efficiency change.
    Reduced the size of the unnecessary large array for the exogenous variables
    introduced in 57fd56c90a41bba62f413e0ced53ddc1a15f39f8.

--- matlab/perfect-foresight-models/sim1_linear.m
+++ matlab/perfect-foresight-models/sim1_linear.m
@@ -108,11 +108,12 @@ if verbose
 end
 
 dynamicmodel = str2func([M.fname,'_dynamic']);
+
 z = steadystate_y([ip; ic; in]);
+x = repmat(transpose(steadystate_x), 1+M.maximum_exo_lag+M.maximum_exo_lead, 1);
 
 % Evaluate the Jacobian of the dynamic model at the deterministic steady state.
-[d1,jacobian] = dynamicmodel(z, repmat(transpose(steadystate_x),options.periods+M.maximum_lag+M.maximum_lead,1),...
-    params, steadystate_y, M.maximum_lag+1);
+[d1, jacobian] = dynamicmodel(z, x, params, steadystate_y, M.maximum_lag+1);
 
 % Check that the dynamic model was evaluated at the steady state.
 if max(abs(d1))>1e-12
