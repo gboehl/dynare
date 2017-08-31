c46848857555208f39306571f5e9513ed52e1413
Stéphane Adjemian (Scylla) <stephane.adjemian@univ-lemans.fr>
2017-08-31 14:55:49 +0200

    Fixed dimension of exo_simul (always add one row).

--- matlab/backward/simul_backward_linear_model.m
+++ matlab/backward/simul_backward_linear_model.m
@@ -57,11 +57,7 @@ else
 end
 Y = DynareOutput.endo_simul;
 
-if ~DynareModel.max_exo_lag_orig
-    if DynareModel.max_endo_lag_orig>1
-        DynareOutput.exo_simul = [ zeros(DynareModel.max_endo_lag_orig-1, DynareModel.exo_nbr); DynareOutput.exo_simul];
-    end
-end
+DynareOutput.exo_simul = [ zeros(1, DynareModel.exo_nbr); DynareOutput.exo_simul];
 
 % Get coefficients
 [cst, jacob] = model_dynamic(zeros(DynareModel.endo_nbr+ny1,1), ...
