a2da79742345c40abf635e1024d21f7985f59981
Houtan Bastani <houtan@dynare.org>
2018-02-09 15:22:25 +0100

    get_ar_matrices: handle ECM part if it exists

--- matlab/get_ar_matrices.m
+++ matlab/get_ar_matrices.m
@@ -73,10 +73,59 @@ end
 assert(length(M_.var.(var_model_name).lhs) == rows(g1));
 
 % Initialize AR matrices
-for i = 1:max(M_.var.(var_model_name).rhs.lag)+1
-    oo_.var.(var_model_name).ar{i} = zeros(length(M_.var.(var_model_name).lhs), length(M_.var.(var_model_name).lhs));
+% ECM
+rhsvars = [];
+rhslag = [];
+maxlag = max(M_.var.(var_model_name).rhs.lag);
+for i = 1:length(M_.var.(var_model_name).rhs.vars_at_eq)
+    rhsvars = union(rhsvars, M_.var.(var_model_name).rhs.vars_at_eq{i}.var);
+    rhslag = union(rhslag, M_.var.(var_model_name).rhs.vars_at_eq{i}.lag);
+end
+if ~isempty(rhslag)
+    maxlag = max(maxlag, max(abs(rhslag)));
 end
 
+Bvars = setdiff(rhsvars, M_.var.(var_model_name).lhs);
+orig_diff_var_vec = M_.var.(var_model_name).orig_diff_var(M_.var.(var_model_name).diff);
+diff_vars = M_.var.(var_model_name).lhs(M_.var.(var_model_name).lhs > M_.orig_endo_nbr);
+drop_avs_related_to = [];
+for i = 1:length(diff_vars)
+    av = M_.aux_vars([M_.aux_vars.endo_index] == diff_vars(i));
+    assert(any(orig_diff_var_vec == av.orig_index));
+    if av.type == 8
+        drop_diff_avs_related_to = [drop_avs_related_to av.orig_index];
+    end
+end
+keep = true(length(Bvars), 1);
+Bvars_diff_index = zeros(length(Bvars), 1);
+for i = sum(Bvars <= M_.orig_endo_nbr)+1:length(Bvars)
+    av = M_.aux_vars([M_.aux_vars.endo_index] == Bvars(i));
+    if av.type == 8
+        if any(drop_diff_avs_related_to == av.orig_index)
+            assert(any(orig_diff_var_vec == av.orig_index));
+            keep(i) = false;
+        else
+            if any(Bvars_diff_index == av.orig_index)
+                keep(i) = false;
+            else
+                Bvars_diff_index(i) = av.orig_index;
+            end
+        end
+    end
+end
+Bvars = Bvars(keep);
+Bvars_diff_index = Bvars_diff_index(keep);
+oo_.var.(var_model_name).ecm_idx = Bvars;
+
+% AR
+narvars = length(M_.var.(var_model_name).lhs);
+nothvars = length(Bvars);
+for i = 1:maxlag + 1
+    oo_.var.(var_model_name).ar{i} = zeros(narvars, narvars);
+    oo_.var.(var_model_name).ecm{i} = zeros(narvars, nothvars);
+end
+
+ecm_assigned = false;
 for i = 1:2
     if i == 1
         baselag = 2;
@@ -89,17 +138,40 @@ for i = 1:2
                 av = M_.aux_vars([M_.aux_vars.endo_index] == j);
                 assert(~isempty(av));
                 if av.type == 8
-                    orig_diff_var_vec = M_.var.(var_model_name).orig_diff_var(M_.var.(var_model_name).diff);
-                    assert(any(orig_diff_var_vec == av.orig_index));
                     col = M_.var.(var_model_name).orig_diff_var == av.orig_index;
+                    if any(col)
+                        assert(any(orig_diff_var_vec == av.orig_index));
+                        oo_.var.(var_model_name).ar{(av.orig_lead_lag * - 1) + baselag}(:, col) = ...
+                            g1(:, M_.lead_lag_incidence(i, j));
+                    else
+                        col = Bvars_diff_index == av.orig_index;
+                        ecm_assigned = true;
+                        oo_.var.(var_model_name).ecm{(av.orig_lead_lag * - 1) + baselag}(:, col) = ...
+                            g1(:, M_.lead_lag_incidence(i, j));
+                    end
                 else
                     col = M_.var.(var_model_name).lhs == av.orig_index;
+                    if any(col)
+                        oo_.var.(var_model_name).ar{(av.orig_lead_lag * - 1) + baselag}(:, col) = ...
+                            g1(:, M_.lead_lag_incidence(i, j));
+                    else
+                        col = Bvars == av.orig_index;
+                        ecm_assigned = true;
+                        oo_.var.(var_model_name).ecm{(av.orig_lead_lag * - 1) + baselag}(:, col) = ...
+                            g1(:, M_.lead_lag_incidence(i, j));
+                    end
                 end
-                oo_.var.(var_model_name).ar{(av.orig_lead_lag * - 1) + baselag}(:, col) = ...
-                    g1(:, M_.lead_lag_incidence(i, j));
             else
-                oo_.var.(var_model_name).ar{baselag}(:, M_.var.(var_model_name).lhs == j) = ...
-                    g1(:, M_.lead_lag_incidence(i, j));
+                col = M_.var.(var_model_name).lhs == j;
+                if ~any(col)
+                    col = Bvars == j;
+                    ecm_assigned = true;
+                    oo_.var.(var_model_name).ecm{baselag}(:, col) = ...
+                        g1(:, M_.lead_lag_incidence(i, j));
+                else
+                    oo_.var.(var_model_name).ar{baselag}(:, col) = ...
+                        g1(:, M_.lead_lag_incidence(i, j));
+                end
             end
         end
     end
@@ -114,5 +186,12 @@ if any(oo_.var.(var_model_name).ar{1}(:))
     error('This is not a VAR model! Contemporaneous endogenous variables are not allowed.')
 end
 
-% Remove first matrix
+% Remove time t matrix for autoregressive part
 oo_.var.(var_model_name).ar = oo_.var.(var_model_name).ar(2:end);
+
+% Remove error correction matrices if never assigned
+if ~ecm_assigned
+    oo_.var.(var_model_name) = rmfield(oo_.var.(var_model_name), 'ecm');
+    oo_.var.(var_model_name) = rmfield(oo_.var.(var_model_name), 'ecm_idx');
+end
+end
