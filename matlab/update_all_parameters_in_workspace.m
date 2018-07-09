e50d4fd41cec281000dd7a4f2101ce01be1f330d
Stéphane Adjemian(Charybdis) <stephane.adjemian@univ-lemans.fr>
2018-07-09 18:42:03 +0200

    Added routine to update parameter values in base workspace.

---
+++ matlab/update_all_parameters_in_workspace.m
@@ -0,0 +1,24 @@
+function update_all_parameters_in_workspace(DynareModel)
+
+% Updates all parameter values in Matlab/Octave base workspace.
+
+% Copyright (C) 2018 Dynare Team
+%
+% This file is part of Dynare.
+%
+% Dynare is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Dynare is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
+
+for i=1:length(DynareModel.params)
+    assignin('base', DynareModel.param_names{i}, DynareModel.params(i));
+end
