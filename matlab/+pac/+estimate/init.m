gboehl/dynare

commit 42bba088877ba59d963065c1014dfa2a06ca1f90

Make the code robust to the order of declaration of the estimated parameters.

matlab/+pac/+estimate/init.m
@@ -1,4 +1,4 @@
-function [pacmodl, lhs, rhs, pnames, enames, xnames, pid, eid, xid, pnames_, ipnames_, data, islaggedvariables] = ...
+function [pacmodl, lhs, rhs, pnames, enames, xnames, pid, eid, xid, pnames_, ipnames_, params, data, islaggedvariables] = ...
     init(M_, oo_, eqname, params, data, range)
 
 % Copyright (C) 2018 Dynare Team
@@ -60,6 +60,25 @@ ipnames_ = zeros(size(pnames_));
 for i=1:length(ipnames_)
     ipnames_(i) = strmatch(pnames_{i}, M_.param_names, 'exact');
 end
+
+% Ensure that the error correction parameter comes first, followed by the
+% autoregressive parameters (in increasing order w.r.t. the lags).
+ipnames__ = ipnames_;                                                  % The user provided order.
+ipnames_  = [M_.pac.(pacmodl).ec.params; M_.pac.(pacmodl).ar.params']; % The correct order.
+for i=1:length(ipnames_)
+    if ~ismember(ipnames_(i), ipnames__)
+        % This parameter is not estimated.
+        ipnames_(i) = NaN;
+    end
+end
+
+% Remove calibrated parameters (if any).
+ipnames_(find(isnan(ipnames_))) = [];
+
+% Reorder params if needed.
+[~, permutation] = ismember(ipnames__, ipnames_);
+pnames_ = pnames_(permutation);
+params = orderfields(params, permutation);
 
 % Add the auxiliary variables in the dataset.
 data = feval([M_.fname '.dynamic_set_auxiliary_series'], data, M_.params);

matlab/+pac/+estimate/iterative_ols.m
@@ -41,7 +41,7 @@ function iterative_ols(eqname, params, data, range)
 
 global M_ oo_
 
-[pacmodl, lhs, rhs, pnames, enames, xnames, pid, eid, xid, pnames_, ipnames_, data, islaggedvariables] = ...
+[pacmodl, lhs, rhs, pnames, enames, xnames, pid, eid, xid, pnames_, ipnames_, params, data, islaggedvariables] = ...
     pac.estimate.init(M_, oo_, eqname, params, data, range);
 
 % Set initial condition.

matlab/+pac/+estimate/nls.m
@@ -41,7 +41,7 @@ function nls(eqname, params, data, range)
 
 global M_ oo_
 
-[pacmodl, lhs, rhs, pnames, enames, xnames, pid, eid, xid, pnames_, ipnames_, data, islaggedvariables] = ...
+[pacmodl, lhs, rhs, pnames, enames, xnames, pid, eid, xid, pnames_, ipnames_, params, data, islaggedvariables] = ...
     pac.estimate.init(M_, oo_, eqname, params, data, range);
 
 % List of objects to be replaced

commit 7d6296f7a67a86124c34c3772eccefe764716fdb

Fixed bug introduced by 42bba088877ba59d963065c1014dfa2a06ca1f90.

matlab/+pac/+estimate/init.m
@@ -65,6 +65,9 @@ end
 % autoregressive parameters (in increasing order w.r.t. the lags).
 ipnames__ = ipnames_;                                                  % The user provided order.
 ipnames_  = [M_.pac.(pacmodl).ec.params; M_.pac.(pacmodl).ar.params']; % The correct order.
+if isfield(M_.pac.(pacmodl), 'share_of_optimizing_agents_index')
+    ipnames_ = [ipnames_; M_.pac.(pacmodl).share_of_optimizing_agents_index]
+end
 for i=1:length(ipnames_)
     if ~ismember(ipnames_(i), ipnames__)
         % This parameter is not estimated.