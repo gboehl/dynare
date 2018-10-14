a53c63d6d504480cd5afaaa1a29769470069428b
Stéphane Adjemia (Scylla) <stepan@dynare.org>
2018-10-14 16:48:29 +0200

    Added routines to print PAC_EXPECTATION and VAR_EXPECTATION in files.
    Each new routine pac.print() and var_expectation.print(), creates two files
    that can be included (using the macro language) in a mod file. The first
    file, {pac,var}expectationmodename-parameters.inc saved under
    {M_.fname}/model/{pac,var}expectationmodel, contains the declaration of the
    parameters created for the expanded (var or pac) expectation model, with
    calibrated values. The second file, {pac,var}expectationmodename-parameters.inc
    saved in the same subfolder, contains the expanded version of the (pac or var)
    expectation term as a linear combination of the variables in the companion
    representation of the expectatino model.

---
+++ matlab/+var_expectation/print.m
@@ -0,0 +1,43 @@
+function print(varexpectationmodelname, withcalibration)
+
+% Prints the exansion of the VAR_EXPECTATION term in files.
+%
+% INPUTS
+% - varepxpectationmodelname       [string]    Name of the expectation model.
+% - withcalibration                [logical]   Prints calibration if true.
+%
+% OUTPUTS
+% None
+%
+% REMARKS
+% The routine creates two text files
+%
+% - {varexpectationmodelname}-parameters.inc     which contains the declaration of the parameters specific to the expectation model kind term.
+% - {varexpectationmodelname}-expression.inc     which contains the expanded version of the expectation model kind term.
+%
+% These routines are saved under the {modfilename}/model/varexpectationmodel subfolder, and can be
+% used after in another mod file (ie included with the macro directive @#include).
+
+% Copyright (C) 2018 Dynare Team
+%
+% This file is part of Dynare.
+%
+% Dynare is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Dynare is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
+
+if nargin<2
+    % Print calibration by default.
+    withcalibration = true;
+end
+
+print_expectations(varexpectationmodelname, 'var-expectations', withcalibration);
