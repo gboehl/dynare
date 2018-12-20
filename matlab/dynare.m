gboehl/dynare

commit 0e77815ece62be21f99338d45f217a535eccb4c4

Options list at the top of the mod-file now parsed by the preprocessor
Closes #1630

matlab/dynare.m
@@ -207,25 +207,6 @@ else
     end
 end
 
-% Read options from the first line in mod/dyn file.
-fid = fopen(fname, 'r');
-firstline = fgetl(fid);
-fclose(fid);
-if regexp(firstline, '\s*\/\/', 'once') == 1
-    firstline = regexprep(firstline, '\s*\/\/', '');
-    if ~isempty(regexp(firstline, '(^\s*\-\-\+\s*options:\s*)', 'once')) ...
-        && ~isempty(regexp(firstline, '(\s*\+\-\-.*$)', 'once'))
-        firstline = regexprep(firstline, '(^\s*\-\-\+\s*options:\s*)', '');
-        firstline = regexprep(firstline, '(\s*\+\-\-.*$)', '');
-        dynoption = strsplit(firstline, {' ', ','});
-        if isequal(nargin, 1)
-            varargin = dynoption;
-        else
-            varargin = union(varargin, dynoption);
-        end
-    end
-end
-
 command = ['"' dynareroot 'preprocessor' arch_ext filesep 'dynare_m" ' fname] ;
 command = [ command ' mexext=' mexext ' "matlabroot=' matlabroot '"'];
 for i=1:length(varargin)

commit f363c3da6b03b6fcdccb2828067949f67ba180fe

Restore behavior of 'nolog' option
The option had been broken by 0e77815ece62be21f99338d45f217a535eccb4c4.

This option cannot be entirely handled at the preprocessor level, since the
main log file is created at the MATLAB/Octave level. Handle it via a dedicated
regular expression.

matlab/dynare.m
@@ -259,6 +259,12 @@ no_log = 0;
 for i=1:length(varargin)
     no_log = no_log || strcmp(varargin{i}, 'nolog');
 end
+fid = fopen(fname, 'r');
+firstline = fgetl(fid);
+fclose(fid);
+if ~isempty(regexp(firstline, '//\s*--\+\s*options:(|.*\s|.*,)nolog(|\s.*|,.*)\+--'))
+    no_log = 1;
+end
 if ~no_log
     logname = [fname(1:end-4) '.log'];
     fid = fopen(logname, 'w');