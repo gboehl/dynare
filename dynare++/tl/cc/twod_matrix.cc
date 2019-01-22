f4849421545ae5b9360bf62c876395f9e8b3a0c5
Sébastien Villemot <sebastien@dynare.org>
2019-01-22 18:10:16 +0100

    Update dseries submodule

--- dynare++/tl/cc/twod_matrix.cc
+++ dynare++/tl/cc/twod_matrix.cc
@@ -54,6 +54,13 @@ ConstTwoDMatrix::writeMat(mat_t *fd, const char *vname) const
   delete[] data;
 }
 
+TwoDMatrix &
+TwoDMatrix::operator=(const ConstTwoDMatrix &m)
+{
+  GeneralMatrix::operator=(m);
+  return *this;
+}
+
 void
 TwoDMatrix::copyRow(int from, int to)
 {
