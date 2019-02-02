gboehl/dynare

commit 752a02a36c16d1ebe3965101f10a8891bf65143e

Dynare++: use C++11 threads (instead of POSIX threads API)
On Windows, this means that a POSIX threads implementation is no longer needed,
since C++11 threads are implemented using native Windows threads.

On GNU/Linux and macOS, POSIX threads are still used under the hood.

A new m4 macro (AX_CXX11_THREAD) is used to add the proper compilation
flags (instead of AX_PTHREAD).

dynare++/tl/cc/sthread.cc
@@ -1,232 +1,70 @@
 // Copyright 2004, Ondra Kamenik
 
-/* We set the default values for
-   |max_parallel_threads| for both |posix| and |empty| implementation and
-   both joinable and detach group. For |posix| this defaults to
-   uniprocessor machine with hyper-threading, this is 2. */
 
-#include <cstring>
 #include "sthread.hh"
 
-#ifdef HAVE_PTHREAD
 namespace sthread
 {
-  template<>
-  int thread_group<posix>::max_parallel_threads = 2;
-  template<>
-  int detach_thread_group<posix>::max_parallel_threads = 2;
+  /* We set the default value for |max_parallel_threads| to 2, i.e.
+     uniprocessor machine with hyper-threading */
+  int detach_thread_group::max_parallel_threads = 2;
 
-  // POSIX specializations methods
-  void *posix_thread_function(void *c);
-  template <>
-  void
-  thread_traits<posix>::run(_Ctype *c)
+  /* The constructor acquires the mutex in the map. First it tries to
+     get an exclusive access to the map. Then it increases a number of
+     references of the mutex (if it does not exists, it inserts it). Then
+     unlocks the map, and finally tries to lock the mutex of the map. */
+  synchro::synchro(const void *c, std::string id)
+    : caller{c}, iden{std::move(id)}
   {
-    pthread_create(&(c->getThreadIden()), nullptr, posix_thread_function, (void *) c);
+    mutmap.lock_map();
+    if (!mutmap.check(caller, iden))
+      mutmap.insert(caller, iden);
+    mutmap.get(caller, iden).second++;
+    mutmap.unlock_map();
+    mutmap.get(caller, iden).first.lock();
   }
 
-  void *posix_detach_thread_function(void *c);
-
-  template <>
-  void
-  thread_traits<posix>::detach_run(_Dtype *c)
+  /* The destructor first locks the map. Then releases the lock,
+     and decreases a number of references. If it is zero, it removes the
+     mutex. */
+  synchro::~synchro()
   {
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-    pthread_create(&(c->getThreadIden()), &attr, posix_detach_thread_function, (void *) c);
-    pthread_attr_destroy(&attr);
-  }
-
-  template <>
-  void
-  thread_traits<posix>::exit()
-  {
-    pthread_exit(nullptr);
-  }
-
-  template <>
-  void
-  thread_traits<posix>::join(_Ctype *c)
-  {
-    pthread_join(c->getThreadIden(), nullptr);
-  }
-
-  template <>
-  void
-  mutex_traits<posix>::init(pthread_mutex_t &m)
-  {
-    pthread_mutex_init(&m, nullptr);
-  }
-
-  template <>
-  void
-  mutex_traits<posix>::lock(pthread_mutex_t &m)
-  {
-    pthread_mutex_lock(&m);
-  }
-
-  template <>
-  void
-  mutex_traits<posix>::unlock(pthread_mutex_t &m)
-  {
-    pthread_mutex_unlock(&m);
-  }
-
-  template <>
-  void
-  cond_traits<posix>::init(_Tcond &cond)
-  {
-    pthread_cond_init(&cond, nullptr);
-  }
-
-  template <>
-  void
-  cond_traits<posix>::broadcast(_Tcond &cond)
-  {
-    pthread_cond_broadcast(&cond);
-  }
-
-  template <>
-  void
-  cond_traits<posix>::wait(_Tcond &cond, _Tmutex &mutex)
-  {
-    pthread_cond_wait(&cond, &mutex);
-  }
-
-  template <>
-  void
-  cond_traits<posix>::destroy(_Tcond &cond)
-  {
-    pthread_cond_destroy(&cond);
-  }
-
-  /* Here we instantiate the static map, and construct |PosixSynchro|
-     using that map. */
-
-  static posix_synchro::mutex_map_t posix_mm;
-
-  PosixSynchro::PosixSynchro(const void *c, const char *id)
-    : posix_synchro(c, id, posix_mm)
-  {
-  }
-
-  /* This function is of the type |void* function(void*)| as required by
-     POSIX, but it typecasts its argument and runs |operator()()|. */
-
-  void *
-  posix_thread_function(void *c)
-  {
-    auto *ct
-      = (thread_traits<posix>::_Ctype *)c;
-    try
+    mutmap.lock_map();
+    if (mutmap.check(caller, iden))
       {
-        ct->operator()();
+        mutmap.get(caller, iden).first.unlock();
+        mutmap.get(caller, iden).second--;
+        if (mutmap.get(caller, iden).second == 0)
+          mutmap.remove(caller, iden);
       }
-    catch (...)
-      {
-        ct->exit();
-      }
-    return nullptr;
+    mutmap.unlock_map();
   }
 
-  void *
-  posix_detach_thread_function(void *c)
+  /* We cycle through all threads in the group, and in each cycle we wait
+     for the change in the |counter|. If the counter indicates less than
+     maximum parallel threads running, then a new thread is run, and the
+     iterator in the list is moved.
+
+     At the end we have to wait for all thread to finish. */
+  void
+  detach_thread_group::run()
   {
-    auto *ct
-      = (thread_traits<posix>::_Dtype *)c;
-    condition_counter<posix> *counter = ct->counter;
-    try
+    std::unique_lock<std::mutex> lk{m};
+    auto it = tlist.begin();
+    while (it != tlist.end())
       {
-        ct->operator()();
+        counter++;
+        std::thread th{[&, it] {
+            // The "it" variable is captured by value, because otherwise the iterator may move
+            (*it)->operator()();
+            std::unique_lock<std::mutex> lk2{m};
+            counter--;
+            std::notify_all_at_thread_exit(cv, std::move(lk2));
+          }};
+        th.detach();
+        ++it;
+        cv.wait(lk, [&] { return counter < max_parallel_threads; });
       }
-    catch (...)
-      {
-        ct->exit();
-      }
-    if (counter)
-      counter->decrease();
-    return nullptr;
+    cv.wait(lk, [&] { return counter == 0; });
   }
 }
-#else
-namespace sthread
-{
-  template<>
-  int thread_group<empty>::max_parallel_threads = 1;
-  template<>
-  int detach_thread_group<empty>::max_parallel_threads = 1;
-
-  // non-threading specialization methods
-  /* The only trait methods we need to work are |thread_traits::run| and
-     |thread_traits::detach_run|, which directly call
-     |operator()()|. Anything other is empty. */
-
-  template <>
-  void
-  thread_traits<empty>::run(_Ctype *c)
-  {
-    c->operator()();
-  }
-  template <>
-  void
-  thread_traits<empty>::detach_run(_Dtype *c)
-  {
-    c->operator()();
-  }
-
-  template <>
-  void
-  thread_traits<empty>::exit()
-  {
-  }
-
-  template <>
-  void
-  thread_traits<empty>::join(_Ctype *c)
-  {
-  }
-
-  template <>
-  void
-  mutex_traits<empty>::init(Empty &m)
-  {
-  }
-
-  template <>
-  void
-  mutex_traits<empty>::lock(Empty &m)
-  {
-  }
-
-  template <>
-  void
-  mutex_traits<empty>::unlock(Empty &m)
-  {
-  }
-
-  template <>
-  void
-  cond_traits<empty>::init(_Tcond &cond)
-  {
-  }
-
-  template <>
-  void
-  cond_traits<empty>::broadcast(_Tcond &cond)
-  {
-  }
-
-  template <>
-  void
-  cond_traits<empty>::wait(_Tcond &cond, _Tmutex &mutex)
-  {
-  }
-
-  template <>
-  void
-  cond_traits<empty>::destroy(_Tcond &cond)
-  {
-  }
-}
-#endif

configure.ac
@@ -79,6 +79,8 @@ CPPFLAGS="$CPPFLAGS_SAVED"
 # Don't use deprecated hash structures
 AC_DEFINE([BOOST_NO_HASH], [], [Don't use deprecated STL hash structures])
 
+AX_CXX11_THREAD
+
 # Check for libmatio, needed by Dynare++
 AX_MATIO
 AM_CONDITIONAL([HAVE_MATIO], [test "x$has_matio" = "xyes"])
@@ -118,8 +120,6 @@ if test "x$F77" != "x"; then
 fi
 AM_CONDITIONAL([HAVE_BLAS], [test x"$ax_blas_ok" = "xyes"])
 AM_CONDITIONAL([HAVE_LAPACK], [test x"$ax_lapack_ok" = "xyes"])
-
-AX_PTHREAD
 
 AC_CONFIG_FILES([Makefile
                  VERSION
@@ -181,11 +181,7 @@ AM_CONDITIONAL([ENABLE_ORG_EXPORT], [test "x$enable_org_export" != "x"])
 # Construct final output message
 
 if test "x$ax_blas_ok" = "xyes" -a "x$ax_lapack_ok" = "xyes" -a "x$has_matio" = "xyes"; then
-  if test x"$ax_pthread_ok" = "xyes"; then
-     BUILD_DYNAREPLUSPLUS="yes"
-  else
-     BUILD_DYNAREPLUSPLUS="yes (without POSIX threads)"
-  fi
+  BUILD_DYNAREPLUSPLUS="yes"
 else
   BUILD_DYNAREPLUSPLUS="no (missing one of: BLAS, LAPACK, MatIO)"
 fi

dynare++/integ/cc/Makefile.am
@@ -13,4 +13,4 @@ libinteg_a_SOURCES = \
 	vector_function.hh \
 	precalc_quadrature.hh
 libinteg_a_CPPFLAGS = -I../../sylv/cc -I../../tl/cc -I$(top_srcdir)/mex/sources
-libinteg_a_CXXFLAGS = $(AM_CXXFLAGS) $(PTHREAD_CFLAGS)
+libinteg_a_CXXFLAGS = $(AM_CXXFLAGS) $(THREAD_CXXFLAGS)

dynare++/integ/cc/quadrature.hh
@@ -96,7 +96,7 @@ template <typename _Tpit>
 class QuadratureImpl;
 
 template <typename _Tpit>
-class IntegrationWorker : public THREAD
+class IntegrationWorker : public sthread::detach_thread
 {
   const QuadratureImpl<_Tpit> &quad;
   VectorFunction &func;
@@ -138,7 +138,7 @@ public:
       }
 
     {
-      SYNCHRO syn(&outvec, "IntegrationWorker");
+      sthread::synchro syn(&outvec, "IntegrationWorker");
       outvec.add(1.0, tmpall);
     }
   }
@@ -168,7 +168,7 @@ public:
     // todo: out.length()==func.outdim()
     // todo: dim == func.indim()
     out.zeros();
-    THREAD_GROUP gr;
+    sthread::detach_thread_group gr;
     for (int ti = 0; ti < fs.getNum(); ti++)
       gr.insert(std::make_unique<IntegrationWorker<_Tpit>>(*this, fs.getFunc(ti),
                                                            level, ti, fs.getNum(), out));

dynare++/integ/src/Makefile.am
@@ -2,5 +2,5 @@ noinst_PROGRAMS = quadrature-points
 
 quadrature_points_SOURCES = quadrature-points.cc
 quadrature_points_CPPFLAGS = -I../.. -I../../sylv/cc -I../../integ/cc -I../../tl/cc
-quadrature_points_CXXFLAGS = $(AM_CXXFLAGS) $(PTHREAD_CFLAGS)
-quadrature_points_LDADD = ../cc/libinteg.a ../../tl/cc/libtl.a ../../parser/cc/libparser.a ../../sylv/cc/libsylv.a ../../utils/cc/libutils.a $(LAPACK_LIBS) $(BLAS_LIBS) $(LIBS) $(FLIBS) $(PTHREAD_LIBS)
+quadrature_points_CXXFLAGS = $(AM_CXXFLAGS) $(THREAD_CXXFLAGS)
+quadrature_points_LDADD = ../cc/libinteg.a ../../tl/cc/libtl.a ../../parser/cc/libparser.a ../../sylv/cc/libsylv.a ../../utils/cc/libutils.a $(LAPACK_LIBS) $(BLAS_LIBS) $(LIBS) $(FLIBS)

dynare++/integ/testing/Makefile.am
@@ -2,9 +2,9 @@ check_PROGRAMS = tests
 
 tests_SOURCES = tests.cc
 tests_CPPFLAGS = -I../cc -I../../tl/cc -I../../sylv/cc -I$(top_srcdir)/mex/sources
-tests_CXXFLAGS = $(AM_CXXFLAGS) $(PTHREAD_CFLAGS)
+tests_CXXFLAGS = $(AM_CXXFLAGS) $(THREAD_CXXFLAGS)
 tests_LDFLAGS = $(AM_LDFLAGS) $(LDFLAGS_MATIO)
-tests_LDADD = ../../tl/cc/libtl.a ../../sylv/cc/libsylv.a ../cc/libinteg.a $(LAPACK_LIBS) $(BLAS_LIBS) $(LIBS) $(FLIBS) $(PTHREAD_LIBS) $(LIBADD_MATIO)
+tests_LDADD = ../../tl/cc/libtl.a ../../sylv/cc/libsylv.a ../cc/libinteg.a $(LAPACK_LIBS) $(BLAS_LIBS) $(LIBS) $(FLIBS) $(LIBADD_MATIO)
 
 check-local:
 	./tests

dynare++/integ/testing/tests.cc
@@ -498,7 +498,7 @@ main()
         nvmax = test->nvar;
     }
   tls.init(dmax, nvmax); // initialize library
-  THREAD_GROUP::max_parallel_threads = num_threads;
+  sthread::detach_thread_group::max_parallel_threads = num_threads;
 
   // launch the tests
   int success = 0;

dynare++/kord/Makefile.am
@@ -27,15 +27,15 @@ libkord_a_SOURCES = \
 	random.hh
 
 libkord_a_CPPFLAGS = -I../sylv/cc -I../tl/cc -I../integ/cc -I$(top_srcdir)/mex/sources $(CPPFLAGS_MATIO)
-libkord_a_CXXFLAGS = $(AM_CXXFLAGS) $(PTHREAD_CFLAGS)
+libkord_a_CXXFLAGS = $(AM_CXXFLAGS) $(THREAD_CXXFLAGS)
 
 check_PROGRAMS = tests
 
 tests_SOURCES = tests.cc
 tests_CPPFLAGS = -I../sylv/cc -I../tl/cc -I../integ/cc -I$(top_srcdir)/mex/sources
-tests_CXXFLAGS = $(AM_CXXFLAGS) $(PTHREAD_CFLAGS)
+tests_CXXFLAGS = $(AM_CXXFLAGS) $(THREAD_CXXFLAGS)
 tests_LDFLAGS = $(AM_LDFLAGS) $(LDFLAGS_MATIO)
-tests_LDADD = libkord.a ../tl/cc/libtl.a ../sylv/cc/libsylv.a $(LAPACK_LIBS) $(BLAS_LIBS) $(LIBS) $(FLIBS) $(PTHREAD_LIBS) $(LIBADD_MATIO)
+tests_LDADD = libkord.a ../tl/cc/libtl.a ../sylv/cc/libsylv.a $(LAPACK_LIBS) $(BLAS_LIBS) $(LIBS) $(FLIBS) $(LIBADD_MATIO)
 
 check-local:
 	./tests

commit 3e5b4084b8863bdb5be2bb5eefb39eff2e3c1b3e

Dynare++: fix bug in new threading code
At the end of a thread, we must first notify the main thread waiting on the
condition variable, then unlock the mutex. We must do these two operations in
that order, otherwise there is a possibility of having the main process
destroying the condition variable before the thread tries to notify it (if all
other threads terminate at the same time and bring the counter down to zero).
For that reason, we cannot use std::notify_all_at_thread_exit().

Bug introduced in commit 752a02a36.

dynare++/tl/cc/sthread.cc
@@ -28,7 +28,15 @@ namespace sthread
             (*it)->operator()(mut_threads);
             std::unique_lock<std::mutex> lk2{mut_cv};
             counter--;
-            std::notify_all_at_thread_exit(cv, std::move(lk2));
+            /* First notify the thread waiting on the condition variable, then
+               unlock the mutex. We must do these two operations in that order,
+               otherwise there is a possibility of having the main process
+               destroying the condition variable before the thread tries to
+               notify it (if all other threads terminate at the same time and
+               bring the counter down to zero).
+               For that reason, we cannot use std::notify_all_at_thread_exit() */
+            cv.notify_one();
+            lk2.unlock();
           }};
         th.detach();
         ++it;