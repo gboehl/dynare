gboehl/dynare

commit 1f7d3beddc33ee9038028d86f7985f30bb53c9dd

Dynare++ tensor library: modernize the Symmetry class
We now use a initializer list constructor for creating symmetries of the form
$y^n$, $y^n u^m$, $y^nu^m\sigma^k$.

The constructor taking a single integer is used to initialize a symmetry of a
given length.

Similar changes are made to IntSequence.

This behavior is similar to std::vector.

mex/sources/k_order_perturbation/k_ord_dynare.cc
@@ -248,7 +248,7 @@ KordpDynare::populateDerivativesContainer(const TwoDMatrix &g, int ord, const st
     }
 
   // md container
-  md.remove(Symmetry(ord));
+  md.remove(Symmetry{ord});
   md.insert(mdTi);
   // No need to delete mdTi, it will be deleted by TensorContainer destructor
 }

dynare++/integ/cc/product.cc
@@ -10,7 +10,7 @@
 
 prodpit::prodpit(const ProductQuadrature &q, int j0, int l)
   : prodq(q), level(l), npoints(q.uquad.numPoints(l)),
-    jseq{q.dimen(), 0},
+    jseq(q.dimen(), 0),
     end_flag(false),
     sig{q.dimen()},
     p{q.dimen()}

dynare++/integ/cc/smolyak.cc
@@ -12,7 +12,7 @@
 
 smolpit::smolpit(const SmolyakQuadrature &q, unsigned int isum)
   : smolq(q), isummand(isum),
-    jseq{q.dimen(), 0},
+    jseq(q.dimen(), 0),
     sig{q.dimen()},
     p{q.dimen()}
 {

dynare++/integ/testing/tests.cc
@@ -256,7 +256,7 @@ TestRunnable::smolyak_normal_moments(const GeneralMatrix &m, int imom, int level
 
   // check against theoretical moments
   UNormalMoments moments(imom, msq);
-  smol_out.add(-1.0, (moments.get(Symmetry(imom)))->getData());
+  smol_out.add(-1.0, (moments.get(Symmetry{imom}))->getData());
   std::cout << "\tError:                         " << std::setw(16) << std::setprecision(12) << smol_out.getMax() << std::endl;
   return smol_out.getMax() < 1.e-7;
 }
@@ -285,7 +285,7 @@ TestRunnable::product_normal_moments(const GeneralMatrix &m, int imom, int level
 
   // check against theoretical moments
   UNormalMoments moments(imom, msq);
-  prod_out.add(-1.0, (moments.get(Symmetry(imom)))->getData());
+  prod_out.add(-1.0, (moments.get(Symmetry{imom}))->getData());
   std::cout << "\tError:                         " << std::setw(16) << std::setprecision(12) << prod_out.getMax() << std::endl;
   return prod_out.getMax() < 1.e-7;
 }

dynare++/kord/approximation.cc
@@ -80,7 +80,7 @@ Approximation::approxAtSteady()
 {
   model.calcDerivativesAtSteady();
   FirstOrder fo(model.nstat(), model.npred(), model.nboth(), model.nforw(),
-                model.nexog(), *(model.getModelDerivatives().get(Symmetry(1))),
+                model.nexog(), *(model.getModelDerivatives().get(Symmetry{1})),
                 journal, qz_criterium);
   KORD_RAISE_IF_X(!fo.isStable(),
                   "The model is not Blanchard-Kahn stable",
@@ -293,21 +293,21 @@ Approximation::calcStochShift(Vector &out, double at_sigma) const
     {
       if (KOrder::is_even(d))
         {
-          Symmetry sym(0, d, 0, 0);
+          Symmetry sym{0, d, 0, 0};
 
           // calculate $F_{u'^d}$ via |ZAuxContainer|
           FGSTensor *ten = new FGSTensor(ypart.ny(), TensorDimens(sym, nvs));
           ten->zeros();
           for (int l = 1; l <= d; l++)
             {
-              const FSSparseTensor *f = model.getModelDerivatives().get(Symmetry(l));
+              const FSSparseTensor *f = model.getModelDerivatives().get(Symmetry{l});
               zaux.multAndAdd(*f, *ten);
             }
 
           // multiply with shocks and add to result
-          FGSTensor *tmp = new FGSTensor(ypart.ny(), TensorDimens(Symmetry(0, 0, 0, 0), nvs));
+          FGSTensor *tmp = new FGSTensor(ypart.ny(), TensorDimens(Symmetry{0, 0, 0, 0}, nvs));
           tmp->zeros();
-          ten->contractAndAdd(1, *tmp, *(mom.get(Symmetry(d))));
+          ten->contractAndAdd(1, *tmp, *(mom.get(Symmetry{d})));
 
           out.add(pow(at_sigma, d)/dfac, tmp->getData());
           delete ten;
@@ -360,8 +360,8 @@ Approximation::check(double at_sigma) const
 TwoDMatrix *
 Approximation::calcYCov() const
 {
-  const TwoDMatrix &gy = *(rule_ders->get(Symmetry(1, 0, 0, 0)));
-  const TwoDMatrix &gu = *(rule_ders->get(Symmetry(0, 1, 0, 0)));
+  const TwoDMatrix &gy = *(rule_ders->get(Symmetry{1, 0, 0, 0}));
+  const TwoDMatrix &gu = *(rule_ders->get(Symmetry{0, 1, 0, 0}));
   TwoDMatrix G(model.numeq(), model.numeq());
   G.zeros();
   G.place(gy, 0, model.nstat());

dynare++/kord/decision_rule.hh
@@ -217,11 +217,11 @@ DecisionRuleImpl<t>::fillTensors(const _Tg &g, double sigma)
           int j = d-i;
           int kfact = 1;
           _Ttensor tmp(ypart.ny(),
-                       TensorDimens(Symmetry(i, j), tns));
+                       TensorDimens(Symmetry{i, j}, tns));
           tmp.zeros();
           for (int k = 0; k+d <= g.getMaxDim(); k++, kfact *= k)
             {
-              Symmetry sym(i, j, 0, k);
+              Symmetry sym{i, j, 0, k};
               if (g.check(sym))
                 {
                   double mult = pow(sigma, k)/dfact/kfact;
@@ -539,7 +539,7 @@ DRFixPoint<t>::DRFixPoint(const _Tg &g, const PartitionY &yp,
   fillTensors(g, sigma);
   _Tparent yspol(ypart.nstat, ypart.nys(), *this);
   bigf = new _Tparent((const _Tparent &) yspol);
-  _Ttensym *frst = bigf->get(Symmetry(1));
+  _Ttensym *frst = bigf->get(Symmetry{1});
   for (int i = 0; i < ypart.nys(); i++)
     frst->get(i, i) = frst->get(i, i) - 1;
   bigfder = new _Tparent(*bigf, 0);
@@ -572,9 +572,9 @@ DRFixPoint<t>::fillTensors(const _Tg &g, double sigma)
       int kfact = 1;
       for (int k = 0; d+k <= g.getMaxDim(); k++, kfact *= k)
         {
-          if (g.check(Symmetry(d, 0, 0, k)))
+          if (g.check(Symmetry{d, 0, 0, k}))
             {
-              const _Ttensor *ten = g.get(Symmetry(d, 0, 0, k));
+              const _Ttensor *ten = g.get(Symmetry{d, 0, 0, k});
               double mult = pow(sigma, k)/dfact/kfact;
               g_yd->add(mult, *ten);
             }

dynare++/kord/first_order.hh
@@ -72,10 +72,10 @@ public:
   {
     IntSequence nvs(4);
     nvs[0] = fo.ypart.nys(); nvs[1] = fo.nu; nvs[2] = fo.nu; nvs[3] = 1;
-    _Ttensor *ten = new _Ttensor(fo.ypart.ny(), TensorDimens(Symmetry(1, 0, 0, 0), nvs));
+    _Ttensor *ten = new _Ttensor(fo.ypart.ny(), TensorDimens(Symmetry{1, 0, 0, 0}, nvs));
     ten->zeros(); ten->add(1.0, fo.gy);
     this->insert(ten);
-    ten = new _Ttensor(fo.ypart.ny(), TensorDimens(Symmetry(0, 1, 0, 0), nvs));
+    ten = new _Ttensor(fo.ypart.ny(), TensorDimens(Symmetry{0, 1, 0, 0}, nvs));
     ten->zeros(); ten->add(1.0, fo.gu);
     this->insert(ten);
   }

dynare++/kord/global_check.cc
@@ -95,9 +95,9 @@ ResidFunction::setYU(const ConstVector &ys, const ConstVector &xx)
   hss = new FTensorPolynomial(dr_ss, ytmp_star);
   ConstVector ysteady_ss(dr.c->getSteady(), model->nstat()+model->npred(),
                          model->nboth()+model->nforw());
-  if (hss->check(Symmetry(0)))
+  if (hss->check(Symmetry{0}))
     {
-      hss->get(Symmetry(0))->getData().add(1.0, ysteady_ss);
+      hss->get(Symmetry{0})->getData().add(1.0, ysteady_ss);
     }
   else
     {

commit 9dfb5567409a697643f60fe1e703243225ea6869

k-order DLL: fix bug introduced in 1f7d3beddc
For the IntSequence class, the initialization with {} and () are no longer
equivalent.

mex/sources/k_order_perturbation/k_ord_dynare.cc
@@ -164,7 +164,7 @@ KordpDynare::populateDerivativesContainer(const TwoDMatrix &g, int ord, const st
   // model derivatives FSSparseTensor instance
   FSSparseTensor *mdTi = (new FSSparseTensor(ord, nJcols, nY));
 
-  IntSequence s{ord, 0};
+  IntSequence s(ord, 0);
 
   if (ord == 1)
     {