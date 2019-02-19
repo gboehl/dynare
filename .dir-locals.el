0f08486a12ba22991e1fb3c0ca51a5ca1010ecb5
Sébastien Villemot <sebastien@dynare.org>
2019-02-19 18:53:43 +0100

    Update emacs directory-local variables
    - fix C mode
    - add C++ mode
    - add Octave mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
-((nil .
-      ((c-file-style . "gnu")
-       (indent-tabs-mode . nil)
-       (fill-column . 79)))
- (c-mode . (c-basic-offset 2))
- (makefile-mode . ((indent-tabs-mode . t))))
+((nil . ((indent-tabs-mode . nil)
+         (fill-column . 79)))
+ (c-mode . ((c-file-style . "gnu")))
+ (c++-mode . ((c-file-style . "gnu")))
+ (makefile-mode . ((indent-tabs-mode . t)))
+ (octave-mode . ((octave-block-offset . 4))))
