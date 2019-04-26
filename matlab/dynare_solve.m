99edfc52ed9c65b065088d4c248490d76889c70d
Stéphane Adjemian (Charybdis) <stepan@dynare.org>
2019-04-26 15:55:06 +0200

    Removed unused routine.

--- matlab/dynare_solve.m
+++ matlab/dynare_solve.m
@@ -46,7 +46,7 @@ if isoctave
 else
     caller_file_name=stack(2).file;
 end
-if strcmp(caller_file_name,'simulation_core.m') || strcmp(caller_file_name,'solve_stacked_problem.m')
+if strcmp(caller_file_name,'solve_stacked_problem.m')
     tolf = options.dynatol.f;
 else
     tolf = options.solve_tolf;
