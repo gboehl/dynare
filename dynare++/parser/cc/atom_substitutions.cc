gboehl/dynare

commit 0b9b69f638d9efa385738abc981a47c1c63fe527

Dynare++: various simplifications

dynare++/parser/cc/atom_substitutions.cc
@@ -9,46 +9,24 @@ using namespace ogp;
 
 AtomSubstitutions::AtomSubstitutions(const AtomSubstitutions &as, const FineAtoms &oa,
                                      FineAtoms &na)
-  : old_atoms(oa), new_atoms(na)
+  : new2old(as.new2old), old2new(as.old2new), old_atoms(oa), new_atoms(na)
 {
-  const NameStorage &ns = na.get_name_storage();
-
-  // fill new2old
-  for (const auto & it : as.new2old)
-    new2old.emplace(ns.query(it.first),
-                    Tshiftname(ns.query(it.second.first),
-                               it.second.second));
-  // fill old2new
-  for (const auto & it : as.old2new)
-    {
-      Tshiftnameset sset;
-      for (const auto & itt : it.second)
-        sset.emplace(ns.query(itt.first), itt.second);
-      old2new.emplace(ns.query(it.first), sset);
-    }
 }
 
 void
-AtomSubstitutions::add_substitution(const char *newname, const char *oldname, int tshift)
+AtomSubstitutions::add_substitution(string newname, string oldname, int tshift)
 {
-  // make sure the storage is from the new_atoms
-  newname = new_atoms.get_name_storage().query(newname);
-  oldname = new_atoms.get_name_storage().query(oldname);
-  if (!newname || !oldname)
-    throw ogu::Exception(__FILE__, __LINE__,
-                         "Bad newname or oldname in AtomSubstitutions::add_substitution");
-
   // insert to new2old map
   new2old.emplace(newname, Tshiftname(oldname, tshift));
   // insert to old2new map
   auto it = old2new.find(oldname);
   if (it != old2new.end())
-    it->second.emplace(newname, -tshift);
+    it->second.emplace(std::move(newname), -tshift);
   else
     {
       Tshiftnameset snset;
-      snset.emplace(newname, -tshift);
-      old2new.emplace(oldname, snset);
+      snset.emplace(std::move(newname), -tshift);
+      old2new.emplace(std::move(oldname), snset);
     }
 
   // put to info
@@ -59,14 +37,14 @@ void
 AtomSubstitutions::substitutions_finished(VarOrdering::ord_type ot)
 {
   // create an external ordering of new_atoms from old_atoms
-  const vector<const char *> &oa_ext = old_atoms.get_allvar();
-  vector<const char *> na_ext;
-  for (auto oname : oa_ext)
+  const vector<string> &oa_ext = old_atoms.get_allvar();
+  vector<string> na_ext;
+  for (const auto &oname : oa_ext)
     {
       // add the old name itself
       na_ext.push_back(oname);
       // add all new names derived from the old name
-      Toldnamemap::const_iterator it = old2new.find(oname);
+      auto it = old2new.find(oname);
       if (it != old2new.end())
         for (const auto & itt : it->second)
           na_ext.push_back(itt.first);
@@ -76,8 +54,8 @@ AtomSubstitutions::substitutions_finished(VarOrdering::ord_type ot)
   new_atoms.parsing_finished(ot, na_ext);
 }
 
-const char *
-AtomSubstitutions::get_new4old(const char *oldname, int tshift) const
+string
+AtomSubstitutions::get_new4old(const string &oldname, int tshift) const
 {
   auto it = old2new.find(oldname);
   if (it != old2new.end())
@@ -87,7 +65,7 @@ AtomSubstitutions::get_new4old(const char *oldname, int tshift) const
         if (itt.second == -tshift)
           return itt.first;
     }
-  return nullptr;
+  return "";
 }
 
 void
@@ -106,25 +84,25 @@ AtomSubstitutions::print() const
 void
 SAtoms::substituteAllLagsAndLeads(FormulaParser &fp, AtomSubstitutions &as)
 {
-  const char *name;
+  string name;
 
   int mlead, mlag;
   endovarspan(mlead, mlag);
 
   // substitute all endo lagged more than 1
-  while (name = findEndoWithLeadInInterval(mlag, -2))
+  while (!(name = findEndoWithLeadInInterval(mlag, -2)).empty())
     makeAuxVariables(name, -1, -2, mlag, fp, as);
   // substitute all endo leaded more than 1
-  while (name = findEndoWithLeadInInterval(2, mlead))
+  while (!(name = findEndoWithLeadInInterval(2, mlead)).empty())
     makeAuxVariables(name, 1, 2, mlead, fp, as);
 
   exovarspan(mlead, mlag);
 
   // substitute all lagged exo
-  while (name = findExoWithLeadInInterval(mlag, -1))
+  while (!(name = findExoWithLeadInInterval(mlag, -1)).empty())
     makeAuxVariables(name, -1, -1, mlag, fp, as);
   // substitute all leaded exo
-  while (name = findExoWithLeadInInterval(1, mlead))
+  while (!(name = findExoWithLeadInInterval(1, mlead)).empty())
     makeAuxVariables(name, 1, 1, mlead, fp, as);
 
   // notify that substitution have been finished
@@ -134,30 +112,30 @@ SAtoms::substituteAllLagsAndLeads(FormulaParser &fp, AtomSubstitutions &as)
 void
 SAtoms::substituteAllLagsAndExo1Leads(FormulaParser &fp, AtomSubstitutions &as)
 {
-  const char *name;
+  string name;
 
   int mlead, mlag;
   endovarspan(mlead, mlag);
 
   // substitute all endo lagged more than 1
-  while (name = findEndoWithLeadInInterval(mlag, -2))
+  while (!(name = findEndoWithLeadInInterval(mlag, -2)).empty())
     makeAuxVariables(name, -1, -2, mlag, fp, as);
 
   exovarspan(mlead, mlag);
 
   // substitute all lagged exo
-  while (name = findExoWithLeadInInterval(mlag, -1))
+  while (!(name = findExoWithLeadInInterval(mlag, -1)).empty())
     makeAuxVariables(name, -1, -1, mlag, fp, as);
   // substitute all leaded exo by 1
-  while (name = findExoWithLeadInInterval(1, 1))
+  while (!(name = findExoWithLeadInInterval(1, 1)).empty())
     makeAuxVariables(name, 1, 1, 1, fp, as);
 
   // notify that substitution have been finished
   as.substitutions_finished(order_type);
 }
 
-const char *
-SAtoms::findNameWithLeadInInterval(const vector<const char *> &names,
+string
+SAtoms::findNameWithLeadInInterval(const vector<string> &names,
                                    int ll1, int ll2) const
 {
   for (auto name : names)
@@ -165,7 +143,7 @@ SAtoms::findNameWithLeadInInterval(const vector<const char *> &names,
       auto it = vars.find(name);
       if (it != vars.end())
         {
-          const DynamicAtoms::Tlagmap &lmap = (*it).second;
+          const DynamicAtoms::Tlagmap &lmap = it->second;
           for (auto itt : lmap)
             if (itt.first >= ll1 && itt.first <= ll2)
               return name;
@@ -173,29 +151,29 @@ SAtoms::findNameWithLeadInInterval(const vector<const char *> &names,
     }
 
   // nothing found
-  return nullptr;
+  return "";
 }
 
 void
-SAtoms::attemptAuxName(const char *str, int ll, string &out) const
+SAtoms::attemptAuxName(const string &str, int ll, string &out) const
 {
   char c = (ll >= 0) ? ((ll == 0) ? 'e' : 'p') : 'm';
   string absll = std::to_string(std::abs(ll));
   int iter = 1;
   do
     {
-      out = string(str) + '_';
+      out = str + '_';
       for (int i = 0; i < iter; i++)
         out += c;
       if (ll != 0)
         out += absll;
       iter++;
     }
-  while (varnames.query(out.c_str()));
+  while (varnames.query(out));
 }
 
 void
-SAtoms::makeAuxVariables(const char *name, int step, int start, int limit_lead,
+SAtoms::makeAuxVariables(const string &name, int step, int start, int limit_lead,
                          FormulaParser &fp, AtomSubstitutions &as)
 {
   if (!(step == 1 || step == -1))
@@ -220,10 +198,7 @@ SAtoms::makeAuxVariables(const char *name, int step, int start, int limit_lead,
   // index of atom "a(-2)"
   int tprev = index(name, start-step);
   if (tprev == -1)
-    {
-      string tmp = string{name} + '(' + std::to_string(start-step) + ')';
-      tprev = fp.add_nulary(tmp.c_str());
-    }
+    tprev = fp.add_nulary(name + '(' + std::to_string(start-step) + ')');
 
   int ll = start;
   do
@@ -234,17 +209,15 @@ SAtoms::makeAuxVariables(const char *name, int step, int start, int limit_lead,
       // check if "a_m2(0)" has not been already created (with
       // different step), in this case do not add equation "a_m2(0)
       // = a(-2)"
-      const char *newname;
-      string newname_str;
+      string newname, newname_str;
       int taux;
-      if (!(newname = as.get_new4old(name, ll-step)))
+      if ((newname = as.get_new4old(name, ll-step)).empty())
         {
           attemptAuxName(name, ll-step, newname_str);
-          newname = newname_str.c_str();
+          newname = newname_str;
           register_uniq_endo(newname);
           newname = varnames.query(newname);
-          string tmp = string{newname} + "(0)";
-          taux = fp.add_nulary(tmp.c_str());
+          taux = fp.add_nulary(newname + "(0)");
           // add to substitutions
           as.add_substitution(newname, name, ll-step);
 
@@ -269,8 +242,7 @@ SAtoms::makeAuxVariables(const char *name, int step, int start, int limit_lead,
       if (t == -1)
         {
           // no "a(-3)", make t <-> a_m2(-1)
-          string tmp = string{newname} + '(' + std::to_string(step) + ')';
-          t = fp.add_nulary(tmp.c_str());
+          t = fp.add_nulary(newname + '(' + std::to_string(step) + ')');
         }
       else
         {

dynare++/integ/src/quadrature-points.cc
@@ -29,7 +29,7 @@ struct QuadParams
   QuadParams(int argc, char **argv);
   void check_consistency() const;
 private:
-  enum {opt_max_level, opt_discard_weight, opt_vcov};
+  enum class opt {max_level, discard_weight, vcov};
 };
 
 QuadParams::QuadParams(int argc, char **argv)
@@ -45,9 +45,9 @@ QuadParams::QuadParams(int argc, char **argv)
   argc--;
 
   struct option const opts [] = {
-    {"max-level", required_argument, nullptr, opt_max_level},
-    {"discard-weight", required_argument, nullptr, opt_discard_weight},
-    {"vcov", required_argument, nullptr, opt_vcov},
+    {"max-level", required_argument, nullptr, static_cast<int>(opt::max_level)},
+    {"discard-weight", required_argument, nullptr, static_cast<int>(opt::discard_weight)},
+    {"vcov", required_argument, nullptr, static_cast<int>(opt::vcov)},
     {nullptr, 0, nullptr, 0}
   };
 
@@ -55,29 +55,35 @@ QuadParams::QuadParams(int argc, char **argv)
   int index;
   while (-1 != (ret = getopt_long(argc, argv, "", opts, &index)))
     {
-      switch (ret)
+      if (ret == '?')
         {
-        case opt_max_level:
+          std::cerr << "Unknown option, ignored\n";
+          continue;
+        }
+
+      switch (static_cast<opt>(ret))
+        {
+        case opt::max_level:
           try
             {
-              max_level = std::stoi(std::string{optarg});
+              max_level = std::stoi(optarg);
             }
           catch (const std::invalid_argument &e)
             {
               std::cerr << "Couldn't parse integer " << optarg << ", ignored" << std::endl;
             }
           break;
-        case opt_discard_weight:
+        case opt::discard_weight:
           try
             {
-              discard_weight = std::stod(std::string{optarg});
+              discard_weight = std::stod(optarg);
             }
           catch (const std::invalid_argument &e)
             {
               std::cerr << "Couldn't parse float " << optarg << ", ignored" << std::endl;
             }
           break;
-        case opt_vcov:
+        case opt::vcov:
           vcovname = optarg;
           break;
         }
@@ -124,7 +130,7 @@ main(int argc, char **argv)
 
       // parse the vcov matrix
       ogp::MatrixParser mp;
-      mp.parse(contents.length(), contents.c_str());
+      mp.parse(contents);
       if (mp.nrows() != mp.ncols())
         throw ogu::Exception(__FILE__, __LINE__,
                              "VCOV matrix not square");

dynare++/kord/decision_rule.cc
@@ -105,7 +105,7 @@ SimResults::writeMat(const std::string &base, const std::string &lname) const
   mat_t *matfd = Mat_Create(matfile_name.c_str(), nullptr);
   if (matfd)
     {
-      writeMat(matfd, lname.c_str());
+      writeMat(matfd, lname);
       Mat_Close(matfd);
     }
 }

dynare++/kord/journal.cc
@@ -186,8 +186,8 @@ Journal::printHeader()
         << "Dynare++ comes with ABSOLUTELY NO WARRANTY and is distributed under\n"
         << "GPL: modules integ, tl, kord, sylv, src, extern and documentation\n"
         << "LGPL: modules parser, utils\n"
-        << " for GPL  see http://www.gnu.org/licenses/gpl.html\n"
-        << " for LGPL see http://www.gnu.org/licenses/lgpl.html\n"
+        << " for GPL  see https://www.gnu.org/licenses/gpl.html\n"
+        << " for LGPL see https://www.gnu.org/licenses/lgpl.html\n"
         << "\n\n"
         << "System info: ";
 #ifndef _WIN32

dynare++/parser/cc/Makefile.am
@@ -1,16 +1,13 @@
 noinst_LIBRARIES = libparser.a
 
-GENERATED_FILES = assign_tab.cc csv_tab.cc formula_tab.cc matrix_tab.cc namelist_tab.cc assign_tab.hh csv_tab.hh formula_tab.hh matrix_tab.hh namelist_tab.hh assign_ll.cc csv_ll.cc formula_ll.cc matrix_ll.cc namelist_ll.cc
+GENERATED_FILES = assign_tab.cc formula_tab.cc matrix_tab.cc assign_tab.hh formula_tab.hh matrix_tab.hh assign_ll.cc formula_ll.cc matrix_ll.cc
 
 libparser_a_SOURCES = \
 	location.hh \
-	namelist.hh \
 	atom_assignings.cc \
 	atom_assignings.hh \
 	atom_substitutions.cc \
 	atom_substitutions.hh \
-	csv_parser.cc \
-	csv_parser.hh \
 	dynamic_atoms.cc \
 	dynamic_atoms.hh \
 	fine_atoms.cc \
@@ -33,7 +30,7 @@ libparser_a_CPPFLAGS = -I../.. $(BOOST_CPPFLAGS)
 
 BUILT_SOURCES = $(GENERATED_FILES)
 
-EXTRA_DIST = assign.yy csv.yy formula.yy matrix.yy namelist.yy assign.ll csv.ll formula.ll matrix.ll namelist.ll
+EXTRA_DIST = assign.yy formula.yy matrix.yy assign.ll formula.ll matrix.ll
 
 %_tab.cc %_tab.hh: %.yy
 	$(YACC) -W -o$*_tab.cc $<

dynare++/parser/cc/assign.yy
@@ -10,8 +10,9 @@
 %code
 {
 #include "atom_assignings.hh"
+#include <string>
 
-void asgn_error(const char*);
+void asgn_error(std::string);
 int asgn_lex();
 extern ogp::AtomAssignings* aparser;
 }
@@ -51,7 +52,7 @@ space : space BLANK | BLANK;
 %%
 
 void
-asgn_error(const char* mes)
+asgn_error(std::string mes)
 {
-  aparser->error(mes);
+  aparser->error(std::move(mes));
 }

dynare++/parser/cc/atom_assignings.cc
@@ -10,17 +10,15 @@
 
 #include <limits>
 #include <iostream>
-#include <algorithm>
+#include <sstream>
+#include <iomanip>
 
 using namespace ogp;
 
 AtomAssignings::AtomAssignings(const AtomAssignings &aa, ogp::StaticAtoms &a)
   : atoms(a), expr(aa.expr, atoms), left_names(aa.left_names),
-    order(aa.order)
+    lname2expr(aa.lname2expr), order(aa.order)
 {
-  // fill the lname2expr
-  for (auto it : aa.lname2expr)
-    lname2expr.emplace(left_names.query(it.first), it.second);
 }
 
 /** A global symbol for passing info to the AtomAssignings from
@@ -29,34 +27,30 @@ AtomAssignings *aparser;
 
 /** The declaration of functions defined in asgn_ll.cc and asgn_tab.cc
  * generated from assign.lex assign.y */
-void *asgn__scan_buffer(char *, size_t);
+void *asgn__scan_string(const char *);
 void asgn__destroy_buffer(void *);
 void asgn_parse();
 extern location_type asgn_lloc;
 
 void
-AtomAssignings::parse(int length, const char *stream)
+AtomAssignings::parse(const string &stream)
 {
-  auto buffer = std::make_unique<char[]>(length+2);
-  std::copy_n(stream, length, buffer.get());
-  buffer[length] = '\0';
-  buffer[length+1] = '\0';
   asgn_lloc.off = 0;
   asgn_lloc.ll = 0;
-  void *p = asgn__scan_buffer(buffer.get(), static_cast<unsigned int>(length)+2);
+  void *p = asgn__scan_string(stream.c_str());
   aparser = this;
   asgn_parse();
   asgn__destroy_buffer(p);
 }
 
 void
-AtomAssignings::error(const char *mes)
+AtomAssignings::error(string mes)
 {
-  throw ParserException(mes, asgn_lloc.off);
+  throw ParserException(std::move(mes), asgn_lloc.off);
 }
 
 void
-AtomAssignings::add_assignment_to_double(const char *name, double val)
+AtomAssignings::add_assignment_to_double(string name, double val)
 {
   // if left hand side is a registered atom, insert it to tree
   int t;
@@ -75,25 +69,26 @@ AtomAssignings::add_assignment_to_double(const char *name, double val)
   order.push_back(t);
 
   // add the double to the tree
-  char tmp[100];
-  sprintf(tmp, "%30.25g", val);
+  std::ostringstream buf;
+  buf << std::setprecision(std::numeric_limits<double>::max_digits10)
+      << val;
   try
     {
-      expr.parse(strlen(tmp), tmp);
+      expr.parse(buf.str());
     }
   catch (const ParserException &e)
     {
       // should never happen
-      throw ParserException(string("Error parsing double ")+tmp+": "+e.message(), 0);
+      throw ParserException(string("Error parsing double ")+buf.str()+": "+e.message(), 0);
     }
 
   // register name of the left hand side and put to lname2expr
-  const char *ss = left_names.insert(name);
-  lname2expr.emplace(ss, order.size()-1);
+  left_names.insert(name);
+  lname2expr.emplace(std::move(name), order.size()-1);
 }
 
 void
-AtomAssignings::add_assignment(int asgn_off, const char *str, int name_len,
+AtomAssignings::add_assignment(int asgn_off, const string &str, int name_len,
                                int right_off, int right_len)
 {
   // the order of doing things here is important: since the
@@ -102,23 +97,21 @@ AtomAssignings::add_assignment(int asgn_off, const char *str, int name_len,
   // nulary term for the left hand side, it must be inserted to the
   // expression tree before the expression is parsed.
 
-  // find the name in the atoms, make copy of name to be able to put
-  // '\0' at the end
-  auto *buf = new char[name_len+1];
-  strncpy(buf, str, name_len);
-  buf[name_len] = '\0';
+  // find the name in the atoms
+  string name = str.substr(0, name_len);
+
   // if left hand side is a registered atom, insert it to tree
   int t;
   try
     {
-      t = atoms.check(buf);
+      t = atoms.check(name);
       if (t == -1)
-        t = expr.add_nulary(buf);
+        t = expr.add_nulary(name);
     }
   catch (const ParserException &e)
     {
-      atoms.register_name(buf);
-      t = expr.add_nulary(buf);
+      atoms.register_name(name);
+      t = expr.add_nulary(name);
     }
   // register left hand side in order
   order.push_back(t);
@@ -126,7 +119,7 @@ AtomAssignings::add_assignment(int asgn_off, const char *str, int name_len,
   // parse expression on the right
   try
     {
-      expr.parse(right_len, str+right_off);
+      expr.parse(str.substr(right_off, right_len));
     }
   catch (const ParserException &e)
     {
@@ -134,17 +127,14 @@ AtomAssignings::add_assignment(int asgn_off, const char *str, int name_len,
     }
 
   // register name of the left hand side and put to lname2expr
-  const char *ss = left_names.insert(buf);
-  if (lname2expr.find(ss) != lname2expr.end())
+  left_names.insert(name);
+  if (lname2expr.find(name) != lname2expr.end())
     {
       // Prevent the occurrence of #415
-      std::cerr << "Changing the value of " << ss << " is not supported. Aborting." << std::endl;
+      std::cerr << "Changing the value of " << name << " is not supported. Aborting." << std::endl;
       exit(EXIT_FAILURE);
     }
-  lname2expr[ss] = order.size()-1;
-
-  // delete name
-  delete [] buf;
+  lname2expr[name] = order.size()-1;
 }
 
 void
@@ -154,7 +144,7 @@ AtomAssignings::apply_subst(const AtomSubstitutions::Toldnamemap &mm)
   // variables
   for (const auto & it : mm)
     {
-      const char *oldname = it.first;
+      const string &oldname = it.first;
       const AtomSubstitutions::Tshiftnameset &sset = it.second;
       if (!sset.empty())
         {
@@ -171,9 +161,9 @@ AtomAssignings::apply_subst(const AtomSubstitutions::Toldnamemap &mm)
           // reference to the newly added formula
           for (const auto & itt : sset)
             {
-              const char *newname = itt.first;
-              const char *nn = left_names.insert(newname);
-              lname2expr.emplace(nn, expr.nformulas()-1);
+              const string &newname = itt.first;
+              left_names.insert(newname);
+              lname2expr.emplace(newname, expr.nformulas()-1);
             }
         }
     }
@@ -199,7 +189,7 @@ AtomAsgnEvaluator::setValues(EvalTree &et) const
   double nan = std::numeric_limits<double>::quiet_NaN();
   for (int i = 0; i < aa.atoms.nvar(); i++)
     {
-      const char *ss = aa.atoms.name(i);
+      const string &ss = aa.atoms.name(i);
       int t = aa.atoms.index(ss);
       if (t >= 0)
         {
@@ -213,7 +203,7 @@ AtomAsgnEvaluator::setValues(EvalTree &et) const
 }
 
 void
-AtomAsgnEvaluator::set_user_value(const char *name, double val)
+AtomAsgnEvaluator::set_user_value(const string &name, double val)
 {
   int t = aa.atoms.index(name);
   if (t >= 0)
@@ -238,7 +228,7 @@ AtomAsgnEvaluator::load(int i, double res)
 }
 
 double
-AtomAsgnEvaluator::get_value(const char *name) const
+AtomAsgnEvaluator::get_value(const string &name) const
 {
   auto it = aa.lname2expr.find(name);
   if (it == aa.lname2expr.end())

dynare++/parser/cc/atom_assignings.hh
@@ -14,7 +14,6 @@
 
 namespace ogp
 {
-
   class AtomAsgnEvaluator;
 
   /** This class represents atom assignments used in parameters
@@ -26,7 +25,7 @@ namespace ogp
   {
     friend class AtomAsgnEvaluator;
   protected:
-    using Tvarintmap = std::map<const char *, int, ltstr>;
+    using Tvarintmap = std::map<string, int>;
     /** All atoms which should be sufficient for formulas at the
      * right hand sides. The atoms should be filled with names
      * (preregistered). This is a responsibility of the caller. */
@@ -53,14 +52,14 @@ namespace ogp
     AtomAssignings(const AtomAssignings &aa, StaticAtoms &a);
     virtual ~AtomAssignings() = default;
     /** Parse the assignments from the given string. */
-    void parse(int length, const char *stream);
+    void parse(const string &stream);
     /** Process a syntax error from bison. */
-    void error(const char *mes);
+    void error(string mes);
     /** Add an assignment of the given name to the given
      * double. Can be called by a user, anytime. */
-    void add_assignment_to_double(const char *name, double val);
+    void add_assignment_to_double(string name, double val);
     /** Add an assignment. Called from assign.y. */
-    void add_assignment(int asgn_off, const char *str, int name_len,
+    void add_assignment(int asgn_off, const string &str, int name_len,
                         int right_off, int right_len);
     /** This applies old2new map (possibly from atom
      * substitutions) to this object. It registers new variables
@@ -107,7 +106,7 @@ namespace ogp
      * parameters are known and should be set to their values. In
      * constrast endogenous variables are set initially to NaNs by
      * AtomValues::setValues. */
-    void set_user_value(const char *name, double val);
+    void set_user_value(const string &name, double val);
     /** This sets the result of i-th expression in aa to res, and
      * also checks whether the i-th expression is an atom. If so,
      * it sets the value of the atom in ogp::EvalTree
@@ -126,9 +125,8 @@ namespace ogp
     /** This returns a value for a given name. If the name is not
      * found among atoms, or there is no assignment for the atom,
      * NaN is returned. */
-    double get_value(const char *name) const;
+    double get_value(const string &name) const;
   };
-
 };
 
 #endif

commit 64b568001cf2b3ca79a70bc0805c802fe0fa106b

Dynare++: fix bug introduced in 0b9b69f638d9efa385738abc981a47c1c63fe527

dynare++/parser/cc/atom_substitutions.cc
@@ -216,7 +216,6 @@ SAtoms::makeAuxVariables(const string &name, int step, int start, int limit_lead
           attemptAuxName(name, ll-step, newname_str);
           newname = newname_str;
           register_uniq_endo(newname);
-          newname = varnames.query(newname);
           taux = fp.add_nulary(newname + "(0)");
           // add to substitutions
           as.add_substitution(newname, name, ll-step);