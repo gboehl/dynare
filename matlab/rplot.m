gboehl/dynare

commit 9deab17d7424b25395110cefa7c4531b8fcde638

clean up rplot

matlab/rplot.m
@@ -14,7 +14,7 @@ function rplot(s1)
 % SPECIAL REQUIREMENTS
 %    none
 
-% Copyright (C) 2001-2018 Dynare Team
+% Copyright (C) 2001-2019 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -33,8 +33,6 @@ function rplot(s1)
 
 global M_ oo_ options_
 
-rplottype = options_.rplottype;
-
 if isempty(oo_.endo_simul)
     error('rplot: oo_.endo_simul is empty.')
 end
@@ -47,43 +45,37 @@ if ~exist([M_.fname filesep 'graphs'],'dir')
     mkdir(M_.fname,'graphs');
 end
 
-col = ['y','c','r','g','b','w','m'] ;
-ix = [1 - M_.maximum_lag:size(oo_.endo_simul,2)-M_.maximum_lag]' ;
+ix = (1 - M_.maximum_lag:size(oo_.endo_simul,2)-M_.maximum_lag)';
 
 y = [];
 for k = 1:length(s1)
-    if isempty(strmatch(s1{k}, M_.endo_names, 'exact'))
-        if isempty(strmatch(s1{k}, M_.exo_names, 'exact'))
-            error (['rplot: One of the variables specified does not exist']) ;
+    if ~any(strcmp(s1{k}, M_.endo_names))
+        if ~any(strcmp(s1{k}, M_.exo_names))
+            error ('rplot: One of the variables specified does not exist') ;
         else
-            y = [y; oo_.exo_simul(:, strmatch(s1{k}, M_.exo_names,'exact'))'] ;
+            y = [y; oo_.exo_simul(:, strcmp(s1{k}, M_.exo_names))'] ;
         end
     else
-        y = [y; oo_.endo_simul(strmatch(s1{k}, M_.endo_names,'exact'),:)] ;
+        y = [y; oo_.endo_simul(strcmp(s1{k}, M_.endo_names), :)];
     end
 end
 
 if options_.smpl == 0
-    i = [max(1, M_.maximum_lag):size(oo_.endo_simul,2)]' ;
+    i = (max(1, M_.maximum_lag):size(oo_.endo_simul,2))';
 else
-    i = [options_.smpl(1)+M_.maximum_lag:options_.smpl(2)+M_.maximum_lag]' ;
+    i = (options_.smpl(1)+M_.maximum_lag:options_.smpl(2)+M_.maximum_lag)';
 end
 
 if options_.TeX && any(strcmp('eps',cellstr(options_.graph_format)))
-    fidTeX = fopen([M_.fname, filesep, 'graphs', filesep, M_.fname '_simulated_trajectories_', num2str(rplottype), '.tex'],'w');
+    fidTeX = fopen([M_.fname, filesep, 'graphs', filesep, M_.fname '_simulated_trajectories_', num2str(options_.rplottype), '.tex'],'w');
     fprintf(fidTeX,'%% TeX eps-loader file generated by rplot.m (Dynare).\n');
     fprintf(fidTeX,['%% ' datestr(now,0) '\n']);
 end
 
-
-t = ['Plot of '] ;
-if rplottype == 0
-    for j = 1:size(y,1)
-        t = [t s1{j} ' '] ;
-    end
-    hh=dyn_figure(options_.nodisplay,'Name',['Simulated Trajectory']);
+if options_.rplottype == 0
+    hh=dyn_figure(options_.nodisplay,'Name', 'Simulated Trajectory');
     plot(ix(i),y(:,i)) ;
-    title (t,'Interpreter','none') ;
+    title (['Plot of ' strjoin(s1, ' ')],'Interpreter','none') ;
     xlabel('Periods') ;
     xlim([min(ix(i)) max(ix(i))])
     if length(s1) > 1
@@ -96,32 +88,32 @@ if rplottype == 0
     end
     dyn_saveas(hh,[M_.fname, filesep, 'graphs', filesep, 'SimulatedTrajectory_' s1{1}],options_.nodisplay,options_.graph_format)
     if options_.TeX && any(strcmp('eps',cellstr(options_.graph_format)))
-        create_TeX_loader(fidTeX,options_,[M_.fname, '/graphs/', 'SimulatedTrajectory_' s1{1}],'Simulated trajectories','SimulatedTrajectory_',s1{1},1)
+        create_TeX_loader(fidTeX,[M_.fname, '/graphs/', 'SimulatedTrajectory_' s1{1}],'Simulated trajectories','SimulatedTrajectory_',s1{1},1)
     end
-elseif rplottype == 1
+elseif options_.rplottype == 1
     for j = 1:size(y,1)
-        hh=dyn_figure(options_.nodisplay,'Name',['Simulated Trajectory']);
+        hh=dyn_figure(options_.nodisplay,'Name', 'Simulated Trajectory');
         plot(ix(i),y(j,i)) ;
         xlim([min(ix(i)) max(ix(i))])
         title(['Plot of ' s1{j}],'Interpreter','none') ;
         xlabel('Periods') ;
         dyn_saveas(hh,[M_.fname, filesep, 'graphs', filesep, 'SimulatedTrajectory_' s1{j}],options_.nodisplay,options_.graph_format)
         if options_.TeX && any(strcmp('eps',cellstr(options_.graph_format)))
-            create_TeX_loader(fidTeX,options_,[M_.fname, '/graphs/', 'SimulatedTrajectory_' s1{j}],'Simulated trajectories','SimulatedTrajectory_',s1{j},1);
+            create_TeX_loader(fidTeX,[M_.fname, '/graphs/', 'SimulatedTrajectory_' s1{j}],'Simulated trajectories','SimulatedTrajectory_',s1{j},1);
         end
     end
-elseif rplottype == 2
-    hh=dyn_figure(options_.nodisplay,'Name',['Simulated Trajectory']);
+elseif options_.rplottype == 2
+    hh=dyn_figure(options_.nodisplay,'Name', 'Simulated Trajectory');
     nl = max(1,fix(size(y,1)/4)) ;
     nc = ceil(size(y,1)/nl) ;
     for j = 1:size(y,1)
         subplot(nl,nc,j) ;
         plot(ix(i),y(j,i)) ;
         hold on ;
-        if ~isempty(strmatch(s1{j},M_.endo_names,'exact'))
-            plot(ix(i),oo_.steady_state(strmatch(s1{j},M_.endo_names,'exact'))*ones(1,size(i,1)),'r:') ;
+        if any(strcmp(s1{j}, M_.endo_names))
+            plot(ix(i),oo_.steady_state(strcmp(s1{j}, M_.endo_names))*ones(1,size(i,1)),'r:') ;
         else
-            plot(ix(i),oo_.exo_steady_state(strmatch(s1{j},M_.exo_names,'exact'))*ones(1,size(i,1)),'r:') ;
+            plot(ix(i),oo_.exo_steady_state(strcmp(s1{j}, M_.exo_names))*ones(1,size(i,1)),'r:') ;
         end
         xlabel('Periods') ;
         ylabel([s1{j}],'Interpreter','none') ;
@@ -130,7 +122,7 @@ elseif rplottype == 2
     end
     dyn_saveas(hh,[M_.fname, filesep, 'graphs', filesep, 'SimulatedTrajectory_' s1{1}],options_.nodisplay,options_.graph_format)
     if options_.TeX && any(strcmp('eps',cellstr(options_.graph_format)))
-        create_TeX_loader(fidTeX,options_,[M_.fname, '/graphs/', 'SimulatedTrajectory_' s1{1}],'Simulated trajectories','SimulatedTrajectory_', s1{1},min(j/nc,1));
+        create_TeX_loader(fidTeX,[M_.fname, '/graphs/', 'SimulatedTrajectory_' s1{1}],'Simulated trajectories','SimulatedTrajectory_', s1{1},min(j/nc,1));
     end
 end
 
@@ -138,9 +130,10 @@ if options_.TeX && any(strcmp('eps',cellstr(options_.graph_format)))
     fprintf(fidTeX,'%% End Of TeX file. \n');
     fclose(fidTeX);
 end
+end
 
-function []=create_TeX_loader(fidTeX,options,figpath,caption,label_name,label_type,scale_factor)
-if nargin<6
+function create_TeX_loader(fidTeX,figpath,caption,label_name,label_type,scale_factor)
+if nargin<5
     scale_factor=1;
 end
 fprintf(fidTeX,' \n');
@@ -150,8 +143,4 @@ fprintf(fidTeX,'\\includegraphics[width=%2.2f\\textwidth]{%s}\n',0.8*scale_facto
 fprintf(fidTeX,'\\caption{%s.}',caption);
 fprintf(fidTeX,'\\label{Fig:%s:%s}\n',label_name,label_type);
 fprintf(fidTeX,'\\end{figure}\n\n');
-
-% 02/28/01 MJ replaced bseastr by MATLAB's strmatch
-% 06/19/01 MJ added 'exact' to strmatch calls
-% 06/25/03 MJ correction when options_.smpl ~= 0
-% 03/18/13 JP bugfix for rplottype>0; added figure names
+end

commit 2e1f1897249c3a0eea2889e46360bc1a80378747

compatibility problem introduced in 9deab17d7424b25395110cefa7c4531b8fcde638: strjoin was introduced in R2013a

matlab/rplot.m
@@ -72,10 +72,14 @@ if options_.TeX && any(strcmp('eps',cellstr(options_.graph_format)))
     fprintf(fidTeX,['%% ' datestr(now,0) '\n']);
 end
 
+t = ['Plot of '] ;
 if options_.rplottype == 0
+    for j = 1:size(y,1)
+        t = [t s1{j} ' '] ;
+    end
     hh=dyn_figure(options_.nodisplay,'Name', 'Simulated Trajectory');
     plot(ix(i),y(:,i)) ;
-    title (['Plot of ' strjoin(s1, ' ')],'Interpreter','none') ;
+    title (t,'Interpreter','none') ;
     xlabel('Periods') ;
     xlim([min(ix(i)) max(ix(i))])
     if length(s1) > 1