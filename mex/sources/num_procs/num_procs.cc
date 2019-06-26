9557061fa305f02484ce66f904fc6cdb244f1e2a
Sébastien Villemot <sebastien@dynare.org>
2019-06-26 17:06:52 +0200

    Add new num_procs MEX file for getting number of available processing units

---
+++ mex/sources/num_procs/num_procs.cc
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2019 Dynare Team
+ *
+ * This file is part of Dynare.
+ *
+ * Dynare is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Dynare is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <thread>
+
+#include <dynmex.h>
+
+void
+mexFunction(int nlhs, mxArray *plhs[],
+            int nrhs, const mxArray *prhs[])
+{
+  if (nrhs != 0 || nlhs != 1)
+    mexErrMsgTxt("Must have zero input argument and one output argument");
+
+  plhs[0] = mxCreateDoubleScalar(std::thread::hardware_concurrency());
+}
