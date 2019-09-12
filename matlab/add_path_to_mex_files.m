de8152c0838090328990b2d6db11aded6b1da349
Sébastien Villemot <sebastien@dynare.org>
2019-09-12 12:48:32 +0200

    Fix MATLAB R2019b support
    Some changes were missing in a1a4168c3
    
    (cherry picked from commit c9798a96e0f33c9a737b6d3736cc548aaeceea80)

--- matlab/add_path_to_mex_files.m
+++ matlab/add_path_to_mex_files.m
@@ -60,7 +60,7 @@ else
                 end
             end
         else
-            tmp = [dynareroot '../mex/matlab/win64-9.4-9.5/'];
+            tmp = [dynareroot '../mex/matlab/win64-9.4-9.7/'];
             if exist(tmp, 'dir')
                 mexpath = tmp;
                 if modifypath
