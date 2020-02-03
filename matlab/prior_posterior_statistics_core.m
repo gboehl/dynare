1aa0df4b248f310d7ef9d442679cb1c6e1f76846
Sébastien Villemot <sebastien@dynare.org>
2020-02-03 18:17:21 +0100

    Implement estimation under discretionary optimal policy
    Also add a test case.
    
    For the time being, estimating parameters that appear in the discount factor is
    not supported.
    
    Ref. #1173
    
    (cherry picked from commit 177c5c359fb997b06ae7c6cbed2994b2e6e66c0b)

--- matlab/prior_posterior_statistics_core.m
+++ matlab/prior_posterior_statistics_core.m
@@ -30,7 +30,7 @@ function myoutput=prior_posterior_statistics_core(myinputs,fpar,B,whoiam, ThisMa
 % SPECIAL REQUIREMENTS.
 %   None.
 
-% Copyright (C) 2005-2017 Dynare Team
+% Copyright (C) 2005-2020 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -203,7 +203,11 @@ for b=fpar:B
     M_ = set_all_parameters(deep,estim_params_,M_);
 
     if run_smoother
-        [dr,info,M_,options_,oo_] = resol(0,M_,options_,oo_);
+        if options_.discretionary_policy
+            [dr,info,M_,options_,oo_] = discretionary_policy_1(options_.instruments,M_,options_,oo_);
+        else
+            [dr,info,M_,options_,oo_] = resol(0,M_,options_,oo_);
+        end
         [alphahat,etahat,epsilonhat,alphatilde,SteadyState,trend_coeff,aK,~,~,P,~,~,trend_addition,state_uncertainty,M_,oo_,options_,bayestopt_] = ...
             DsgeSmoother(deep,gend,Y,data_index,missing_value,M_,oo_,options_,bayestopt_,estim_params_);
 
