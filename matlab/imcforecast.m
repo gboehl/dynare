gboehl/dynare

commit 228b2a532f4bfc06f5eadff801ba966e0f7031d6

conditional forecasts: add check whether simulation was successful

matlab/imcforecast.m
@@ -204,9 +204,6 @@ if ~estimated_model
     trend = repmat(ys(oo_.dr.order_var,:),1,options_cond_fcst.periods+1); %trend needs to contain correct steady state
 end
 
-
-
-
 NumberOfStates = length(InitState);
 FORCS1 = zeros(NumberOfStates,options_cond_fcst.periods+1,options_cond_fcst.replic);
 
@@ -252,7 +249,11 @@ for b=1:options_cond_fcst.replic %conditional forecast using cL set to constrain
     [FORCS1(:,:,b), FORCS1_shocks(:,:,b)] = mcforecast3(cL,options_cond_fcst.periods,constrained_paths,shocks,FORCS1(:,:,b),T,R,mv, mu);
     FORCS1(:,:,b)=FORCS1(:,:,b)+trend; %add trend
 end
-
+if max(max(max(abs(FORCS1(constrained_vars,1:cL,:)-constrained_paths))))>1e-4
+    fprintf('\nconditional_forecasts: controlling of variables was not successful.\n')
+    fprintf('This can be due to numerical imprecision (e.g. explosive simulations)\n')
+    fprintf('or because the instrument(s) do not allow controlling the variable(s).\n')
+end
 mFORCS1 = mean(FORCS1,3);
 mFORCS1_shocks = mean(FORCS1_shocks,3);
 

matlab/mcforecast3.m
@@ -1,5 +1,5 @@
 function [forcs, e]= mcforecast3(cL,H,mcValue,shocks,forcs,T,R,mv,mu)
-% forcs = mcforecast3(cL,H,mcValue,shocks,forcs,T,R,mv,mu)
+% [forcs, e] = mcforecast3(cL,H,mcValue,shocks,forcs,T,R,mv,mu)
 % Computes the shock values for constrained forecasts necessary to keep
 % endogenous variables at their constrained paths
 %
@@ -7,13 +7,15 @@ function [forcs, e]= mcforecast3(cL,H,mcValue,shocks,forcs,T,R,mv,mu)
 %  o cL             [scalar]                            number of controlled periods
 %  o H              [scalar]                            number of forecast periods
 %  o mcValue        [n_controlled_vars by cL double]    paths for constrained variables
-%  o shocks         [nexo by H double]      shock values draws (with zeros for controlled_varexo)
-%  o forcs
-%  o T              [n_endovars by n_endovars double]       transition matrix of the state equation.
-%  o R              [n_endovars by n_exo double]           matrix relating the endogenous variables to the innovations in the state equation.
-%  o mv             [n_controlled_exo by n_endovars boolean]        indicator vector  selecting constrained endogenous variables
-%  o mu             [n_controlled_vars by nexo boolean]             indicator vector
-%                                                   selecting controlled exogenous variables
+%  o shocks         [nexo by H double]                  shock values draws (with zeros for controlled_varexo)
+%  o forcs          [n_endovars by H+1 double]          matrix of endogenous variables storing the inital condition
+%  o T              [n_endovars by n_endovars double]   transition matrix of the state equation.
+%  o R              [n_endovars by n_exo double]        matrix relating the endogenous variables to the innovations in the state equation.
+%  o mv             [n_controlled_exo by n_endovars boolean]   indicator vector  selecting constrained endogenous variables
+%  o mu             [n_controlled_vars by nexo boolean]        indicator vector selecting controlled exogenous variables
+% OUTPUTS
+%  o forcs          [n_endovars by H+1 double]          matrix of forecasted endogenous variables
+%  o e              [nexo by H double]                  matrix of exogenous variables
 %
 % Algorithm:
 %   Relies on state-space form:

commit 133b77c061a2e1937e38348a169b4c7d2954f2bf

Conditional forecasting: compatibility fix with MATLAB < R2016b
Implicit expansion (a.k.a. automatic broadcasting) was introduced in MATLAB
R2016b (and it has been present in Octave for quite some time).

Hence use bsxfun() instead.

The problem had been introduced in 228b2a532.

matlab/imcforecast.m
@@ -28,7 +28,7 @@ function imcforecast(constrained_paths, constrained_vars, options_cond_fcst)
 % [1] Results are stored in oo_.conditional_forecast.
 % [2] Use the function plot_icforecast to plot the results.
 
-% Copyright (C) 2006-2019 Dynare Team
+% Copyright (C) 2006-2020 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -249,7 +249,7 @@ for b=1:options_cond_fcst.replic %conditional forecast using cL set to constrain
     [FORCS1(:,:,b), FORCS1_shocks(:,:,b)] = mcforecast3(cL,options_cond_fcst.periods,constrained_paths,shocks,FORCS1(:,:,b),T,R,mv, mu);
     FORCS1(:,:,b)=FORCS1(:,:,b)+trend; %add trend
 end
-if max(max(max(abs(FORCS1(constrained_vars,1:cL,:)-constrained_paths))))>1e-4
+if max(max(max(abs(bsxfun(@minus,FORCS1(constrained_vars,1:cL,:),constrained_paths)))))>1e-4
     fprintf('\nconditional_forecasts: controlling of variables was not successful.\n')
     fprintf('This can be due to numerical imprecision (e.g. explosive simulations)\n')
     fprintf('or because the instrument(s) do not allow controlling the variable(s).\n')