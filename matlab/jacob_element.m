25a977f4bee1193f200d7848ebc80a0a8bd622fe
Houtan Bastani <houtan@dynare.org>
2020-05-04 12:12:41 -0400

    jacob_element & hess_element: remove unnecessary loops

--- matlab/jacob_element.m
+++ matlab/jacob_element.m
@@ -13,7 +13,7 @@ function d=jacob_element(func,element,args)
 % SPECIAL REQUIREMENTS
 %    none
 
-% Copyright (C) 2010-2011 Dynare Team
+% Copyright (C) 2010-2020 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -30,17 +30,15 @@ function d=jacob_element(func,element,args)
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
 
+assert(element <= length(args));
+
 func = str2func(func);
 
 h=10e-6;
-pargs=args;
 margs=args;
-% length(args) is used instead of size(args, 2) to avoid to transpose column vectors
-for i=1:length(args)
-    if i==element
-        pargs{i} = pargs{i} + h;
-        margs{i} = margs{i} - h;
-    end
+
+args{element} = args{element} + h;
+margs{element} = margs{element} - h;
+
+d=(func(args{:})-func(margs{:}))/(2*h);
 end
-d=(func(pargs{:})...
-   -func(margs{:}))/(2*h);
