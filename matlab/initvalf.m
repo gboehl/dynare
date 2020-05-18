gboehl/dynare

commit 179d57f13813b6cf3b6053dd4b94b623fe09e287

Fixed path issue.

matlab/initvalf.m
@@ -51,7 +51,7 @@ if isempty(extension)
     end
 end
 
-fullname = [basename extension];
+fullname = [directory '/' basename extension];
 
 if ~exist(fullname, 'file')
     error(['Can''t find datafile: ' fullname ]);

commit a6ea878627098549acd01ac3a44b9cb205abef2f

Fixed bug introduced in 179d57f13.
initvalf was failing if the datafile was in the current folder.

matlab/initvalf.m
@@ -51,6 +51,10 @@ if isempty(extension)
     end
 end
 
+if isempty(directory)
+    directory = '.';
+end
+
 fullname = [directory '/' basename extension];
 
 if ~exist(fullname, 'file')