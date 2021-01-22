5bee25c06ec67df6550e68888aced023187da7c0
Johannes Pfeifer <jpfeifer@gmx.de>
2021-01-22 15:46:50 +0100

    weibull prior: fix second derivative output and suppress display of output

--- matlab/priordens.m
+++ matlab/priordens.m
@@ -181,9 +181,9 @@ if tt8
         return
     end
     if nargout==2
-        [tmp, dlprior(id8)] = lpdfgweibull(x(id8),p6(id8),p7(id8))
+        [tmp, dlprior(id8)] = lpdfgweibull(x(id8),p6(id8),p7(id8));
     elseif nargout==3
-        [tmp, dlprior(id8), ds2lprior(id8)] = lpdfgweibull(x(id8),p6(id8),p7(id8))
+        [tmp, dlprior(id8), d2lprior(id8)] = lpdfgweibull(x(id8),p6(id8),p7(id8));
     end
 end
 
