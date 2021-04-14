279446968ca494f946cae337ed67cb2783151e88
Sébastien Villemot <sebastien@dynare.org>
2021-04-14 14:43:25 +0200

    Windows package: bump dependencies

--- windows/deps/versions.mk
+++ windows/deps/versions.mk
@@ -18,7 +18,7 @@ MATLAB64_VERSION = 20200930
 ## Build dependencies
 
 # pacman -Ss mingw-w64-x86_64-boost
-MINGW64_BOOST_VERSION = 1.75.0-2
+MINGW64_BOOST_VERSION = 1.75.0-3
 
 # pacman -Ss mingw-w64-x86_64-gsl
 MINGW64_GSL_VERSION = 2.6-1
@@ -27,7 +27,7 @@ MINGW64_GSL_VERSION = 2.6-1
 MINGW64_OPENBLAS_VERSION = 0.3.13-2
 
 # pacman -Ss mingw-w64-x86_64-matio
-MINGW64_MATIO_VERSION = 1.5.19-1
+MINGW64_MATIO_VERSION = 1.5.21-1
 
 # Dependency of matio (and of the MinGW compiler)
 # pacman -Ss mingw-w64-x86_64-zlib
@@ -35,7 +35,7 @@ MINGW64_ZLIB_VERSION = 1.2.11-9
 
 # Dependency of matio
 # pacman -Ss mingw-w64-x86_64-hdf5
-MINGW64_HDF5_VERSION = 1.12.0-2
+MINGW64_HDF5_VERSION = 1.12.0-3
 
 # Dependency of HDF5
 # pacman -Ss mingw-w64-x86_64-szip
@@ -44,22 +44,22 @@ MINGW64_SZIP_VERSION = 2.1.1-2
 ## MinGW packages for the embedded compiler
 
 # pacman -Ss mingw-w64-x86_64-gcc$
-MINGW64_GCC_VERSION = 10.2.0-9
+MINGW64_GCC_VERSION = 10.2.0-10
 
 # pacman -Ss mingw-w64-x86_64-gmp
-MINGW64_GMP_VERSION = 6.2.1-1
+MINGW64_GMP_VERSION = 6.2.1-2
 
 # pacman -Ss mingw-w64-x86_64-binutils
-MINGW64_BINUTILS_VERSION = 2.36.1-1
+MINGW64_BINUTILS_VERSION = 2.36.1-3
 
 # pacman -Ss mingw-w64-x86_64-headers-git
-MINGW64_HEADERS_VERSION = 9.0.0.6128.07922837-2
+MINGW64_HEADERS_VERSION = 9.0.0.6158.1c773877-1
 
 # pacman -Ss mingw-w64-x86_64-crt-git
-MINGW64_CRT_VERSION = 9.0.0.6128.07922837-1
+MINGW64_CRT_VERSION = 9.0.0.6158.1c773877-1
 
 # pacman -Ss mingw-w64-x86_64-winpthreads-git
-MINGW64_WINPTHREADS_VERSION = 9.0.0.6128.07922837-2
+MINGW64_WINPTHREADS_VERSION = 9.0.0.6158.1c773877-1
 
 # pacman -Ss mingw-w64-x86_64-zstd
-MINGW64_ZSTD_VERSION = 1.4.8-2
+MINGW64_ZSTD_VERSION = 1.4.9-1
