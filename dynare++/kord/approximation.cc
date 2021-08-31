gboehl/dynare

commit 6d99b6d1208580c2b91e8f00a51da7d139c93e2e

Dynare++ kord library: various modernizations and simplifications

dynare++/kord/approximation.cc
@@ -10,8 +10,7 @@
 ZAuxContainer::ZAuxContainer(const _Ctype *gss, int ngss, int ng, int ny, int nu)
   : StackContainer<FGSTensor>(4, 1)
 {
-  stack_sizes[0] = ngss; stack_sizes[1] = ng;
-  stack_sizes[2] = ny; stack_sizes[3] = nu;
+  stack_sizes = { ngss, ng, ny, nu };
   conts[0] = gss;
   calculateOffsets();
 }
@@ -32,34 +31,21 @@ ZAuxContainer::getType(int i, const Symmetry &s) const
 }
 
 Approximation::Approximation(DynamicModel &m, Journal &j, int ns, bool dr_centr, double qz_crit)
-  : model(m), journal(j), rule_ders(nullptr), rule_ders_ss(nullptr), fdr(nullptr), udr(nullptr),
+  : model(m), journal(j),
     ypart(model.nstat(), model.npred(), model.nboth(), model.nforw()),
-    mom(UNormalMoments(model.order(), model.getVcov())), nvs(4), steps(ns),
+    mom(UNormalMoments(model.order(), model.getVcov())),
+    nvs{ypart.nys(), model.nexog(), model.nexog(), 1 },
+    steps(ns),
     dr_centralize(dr_centr), qz_criterium(qz_crit), ss(ypart.ny(), steps+1)
 {
-  nvs[0] = ypart.nys(); nvs[1] = model.nexog();
-  nvs[2] = model.nexog(); nvs[3] = 1;
-
   ss.nans();
-}
-
-Approximation::~Approximation()
-{
-  if (rule_ders_ss)
-    delete rule_ders_ss;
-  if (rule_ders)
-    delete rule_ders;
-  if (fdr)
-    delete fdr;
-  if (udr)
-    delete udr;
 }
 
 /* This just returns |fdr| with a check that it is created. */
 const FoldDecisionRule &
 Approximation::getFoldDecisionRule() const
 {
-  KORD_RAISE_IF(fdr == nullptr,
+  KORD_RAISE_IF(!fdr,
                 "Folded decision rule has not been created in Approximation::getFoldDecisionRule");
   return *fdr;
 }
@@ -68,7 +54,7 @@ Approximation::getFoldDecisionRule() const
 const UnfoldDecisionRule &
 Approximation::getUnfoldDecisionRule() const
 {
-  KORD_RAISE_IF(udr == nullptr,
+  KORD_RAISE_IF(!udr,
                 "Unfolded decision rule has not been created in Approximation::getUnfoldDecisionRule");
   return *udr;
 }
@@ -147,7 +133,7 @@ Approximation::walkStochSteady()
   model.solveDeterministicSteady();
   approxAtSteady();
   Vector steady0{ss.getCol(0)};
-  steady0 = (const Vector &) model.getSteady();
+  steady0 = model.getSteady();
 
   double sigma_so_far = 0.0;
   double dsigma = (steps == 0) ? 0.0 : 1.0/steps;
@@ -156,14 +142,14 @@ Approximation::walkStochSteady()
       JournalRecordPair pa(journal);
       pa << "Approximation about stochastic steady for sigma=" << sigma_so_far+dsigma << endrec;
 
-      Vector last_steady((const Vector &)model.getSteady());
+      Vector last_steady(model.getSteady());
 
       // calculate fix-point of the last rule for |dsigma|
       /* We form the |DRFixPoint| object from the last rule with
          $\sigma=dsigma$. Then we save the steady state to |ss|. The new steady
          is also put to |model.getSteady()|. */
       DRFixPoint<KOrder::fold> fp(*rule_ders, ypart, model.getSteady(), dsigma);
-      bool converged = fp.calcFixPoint(DecisionRule::horner, model.getSteady());
+      bool converged = fp.calcFixPoint(DecisionRule::emethod::horner, model.getSteady());
       JournalRecord rec(journal);
       rec << "Fix point calcs: iter=" << fp.getNumIter() << ", newton_iter="
           << fp.getNewtonTotalIter() << ", last_newton_iter=" << fp.getNewtonLastIter() << ".";
@@ -175,14 +161,14 @@ Approximation::walkStochSteady()
           KORD_RAISE_X("Fix point calculation not converged", KORD_FP_NOT_CONV);
         }
       Vector steadyi{ss.getCol(i)};
-      steadyi = (const Vector &) model.getSteady();
+      steadyi = model.getSteady();
 
       // calculate |hh| as expectations of the last $g^{**}$
       /* We form the steady state shift |dy|, which is the new steady state
          minus the old steady state. Then we create |StochForwardDerivs|
          object, which calculates the derivatives of $g^{**}$ expectations at
          new sigma and new steady. */
-      Vector dy((const Vector &)model.getSteady());
+      Vector dy(model.getSteady());
       dy.add(-1.0, last_steady);
 
       StochForwardDerivs<KOrder::fold> hh(ypart, model.nexog(), *rule_ders_ss, mom, dy,
@@ -197,9 +183,8 @@ Approximation::walkStochSteady()
       KOrderStoch korder_stoch(ypart, model.nexog(), model.getModelDerivatives(),
                                hh, journal);
       for (int d = 1; d <= model.order(); d++)
-        {
-          korder_stoch.performStep<KOrder::fold>(d);
-        }
+        korder_stoch.performStep<KOrder::fold>(d);
+
       saveRuleDerivs(korder_stoch.getFoldDers());
 
       check(sigma_so_far+dsigma);
@@ -207,24 +192,14 @@ Approximation::walkStochSteady()
     }
 
   // construct the resulting decision rules
-  if (fdr)
-    {
-      delete fdr;
-      fdr = nullptr;
-    }
-  if (udr)
-    {
-      delete udr;
-      udr = nullptr;
-    }
-
-  fdr = new FoldDecisionRule(*rule_ders, ypart, model.nexog(),
-                             model.getSteady(), 1.0-sigma_so_far);
+  udr.reset();
+  fdr = std::make_unique<FoldDecisionRule>(*rule_ders, ypart, model.nexog(),
+                                           model.getSteady(), 1.0-sigma_so_far);
   if (steps == 0 && dr_centralize)
     {
       // centralize decision rule for zero steps
       DRFixPoint<KOrder::fold> fp(*rule_ders, ypart, model.getSteady(), 1.0);
-      bool converged = fp.calcFixPoint(DecisionRule::horner, model.getSteady());
+      bool converged = fp.calcFixPoint(DecisionRule::emethod::horner, model.getSteady());
       JournalRecord rec(journal);
       rec << "Fix point calcs: iter=" << fp.getNumIter() << ", newton_iter="
           << fp.getNewtonTotalIter() << ", last_newton_iter=" << fp.getNewtonLastIter() << ".";
@@ -239,9 +214,7 @@ Approximation::walkStochSteady()
       {
         JournalRecordPair recp(journal);
         recp << "Centralizing about fix-point." << endrec;
-        FoldDecisionRule *dr_backup = fdr;
-        fdr = new FoldDecisionRule(*dr_backup, model.getSteady());
-        delete dr_backup;
+        fdr = std::make_unique<FoldDecisionRule>(*fdr, model.getSteady());
       }
     }
 }
@@ -254,14 +227,9 @@ Approximation::walkStochSteady()
 void
 Approximation::saveRuleDerivs(const FGSContainer &g)
 {
-  if (rule_ders)
-    {
-      delete rule_ders;
-      delete rule_ders_ss;
-    }
-  rule_ders = new FGSContainer(g);
-  rule_ders_ss = new FGSContainer(4);
-  for (auto & run : (*rule_ders))
+  rule_ders = std::make_unique<FGSContainer>(g);
+  rule_ders_ss = std::make_unique<FGSContainer>(4);
+  for (auto &run : *rule_ders)
     {
       auto ten = std::make_unique<FGSTensor>(ypart.nstat+ypart.npred, ypart.nyss(), *(run.second));
       rule_ders_ss->insert(std::move(ten));
@@ -287,7 +255,7 @@ Approximation::calcStochShift(Vector &out, double at_sigma) const
                 "Wrong length of output vector for Approximation::calcStochShift");
   out.zeros();
 
-  ZAuxContainer zaux(rule_ders_ss, ypart.nyss(), ypart.ny(),
+  ZAuxContainer zaux(rule_ders_ss.get(), ypart.nyss(), ypart.ny(),
                      ypart.nys(), model.nexog());
 
   int dfac = 1;
@@ -298,7 +266,7 @@ Approximation::calcStochShift(Vector &out, double at_sigma) const
           Symmetry sym{0, d, 0, 0};
 
           // calculate $F_{u'^d}$ via |ZAuxContainer|
-          FGSTensor *ten = new FGSTensor(ypart.ny(), TensorDimens(sym, nvs));
+          auto ten = std::make_unique<FGSTensor>(ypart.ny(), TensorDimens(sym, nvs));
           ten->zeros();
           for (int l = 1; l <= d; l++)
             {
@@ -307,13 +275,11 @@ Approximation::calcStochShift(Vector &out, double at_sigma) const
             }
 
           // multiply with shocks and add to result
-          FGSTensor *tmp = new FGSTensor(ypart.ny(), TensorDimens(Symmetry{0, 0, 0, 0}, nvs));
+          auto tmp = std::make_unique<FGSTensor>(ypart.ny(), TensorDimens(Symmetry{0, 0, 0, 0}, nvs));
           tmp->zeros();
           ten->contractAndAdd(1, *tmp, mom.get(Symmetry{d}));
 
           out.add(pow(at_sigma, d)/dfac, tmp->getData());
-          delete ten;
-          delete tmp;
         }
     }
 }
@@ -359,7 +325,7 @@ Approximation::check(double at_sigma) const
    So we invoke the Sylvester solver for the first dimension with $A=I$,
    $B=-G$, $C=G^T$ and $D=g_u\Sigma g_u^T$. */
 
-TwoDMatrix *
+TwoDMatrix
 Approximation::calcYCov() const
 {
   const TwoDMatrix &gy = rule_ders->get(Symmetry{1, 0, 0, 0});
@@ -367,7 +333,7 @@ Approximation::calcYCov() const
   TwoDMatrix G(model.numeq(), model.numeq());
   G.zeros();
   G.place(gy, 0, model.nstat());
-  TwoDMatrix B((const TwoDMatrix &)G);
+  TwoDMatrix B(const_cast<const TwoDMatrix &>(G));
   B.mult(-1.0);
   TwoDMatrix C(transpose(G));
   TwoDMatrix A(model.numeq(), model.numeq());
@@ -375,10 +341,10 @@ Approximation::calcYCov() const
   for (int i = 0; i < model.numeq(); i++)
     A.get(i, i) = 1.0;
 
-  TwoDMatrix *X = new TwoDMatrix((gu * model.getVcov()) * transpose(gu));
+  TwoDMatrix X((gu * model.getVcov()) * transpose(gu));
 
   GeneralSylvester gs(1, model.numeq(), model.numeq(), 0,
-                      A.getData(), B.getData(), C.getData(), X->getData());
+                      A.getData(), B.getData(), C.getData(), X.getData());
   gs.solve();
 
   return X;

dynare++/extern/matlab/dynare_simul.cc
@@ -113,12 +113,10 @@ extern "C" {
         ConstTwoDMatrix vcov_mat(nexog, nexog, ConstVector{vcov});
         GenShockRealization sr(vcov_mat, shocks_mat, seed);
         // simulate and copy the results
-        TwoDMatrix *res_mat
-          = dr.simulate(DecisionRule::horner, nper,
-                        ConstVector{ystart}, sr);
+        TwoDMatrix res_mat{dr.simulate(DecisionRule::emethod::horner, nper,
+                                       ConstVector{ystart}, sr)};
         TwoDMatrix res_tmp_mat{ny, nper, Vector{res}};
-        res_tmp_mat = (const TwoDMatrix &) (*res_mat);
-        delete res_mat;
+        res_tmp_mat = const_cast<const TwoDMatrix &>(res_mat);
         plhs[1] = res;
       }
     catch (const KordException &e)

dynare++/kord/approximation.hh
@@ -55,6 +55,8 @@
 #include "korder.hh"
 #include "journal.hh"
 
+#include <memory>
+
 /* This class is used to calculate derivatives by faa Di Bruno of the
    $$f(g^{**}(g^*(y^*,u,\sigma),u',\sigma),g(y^*,u,\sigma),y^*,u)$$ with
    respect $u'$. In order to keep it as simple as possible, the class
@@ -108,10 +110,10 @@ class Approximation
 {
   DynamicModel &model;
   Journal &journal;
-  FGSContainer *rule_ders;
-  FGSContainer *rule_ders_ss;
-  FoldDecisionRule *fdr;
-  UnfoldDecisionRule *udr;
+  std::unique_ptr<FGSContainer> rule_ders;
+  std::unique_ptr<FGSContainer> rule_ders_ss;
+  std::unique_ptr<FoldDecisionRule> fdr;
+  std::unique_ptr<UnfoldDecisionRule> udr;
   const PartitionY ypart;
   const FNormalMoments mom;
   IntSequence nvs;
@@ -121,11 +123,9 @@ class Approximation
   TwoDMatrix ss;
 public:
   Approximation(DynamicModel &m, Journal &j, int ns, bool dr_centr, double qz_crit);
-  virtual
-  ~Approximation();
 
-  const FoldDecisionRule&getFoldDecisionRule() const;
-  const UnfoldDecisionRule&getUnfoldDecisionRule() const;
+  const FoldDecisionRule &getFoldDecisionRule() const;
+  const UnfoldDecisionRule &getUnfoldDecisionRule() const;
   const TwoDMatrix &
   getSS() const
   {
@@ -138,16 +138,16 @@ public:
   }
 
   void walkStochSteady();
-  TwoDMatrix *calcYCov() const;
-  const FGSContainer *
+  TwoDMatrix calcYCov() const;
+  const FGSContainer &
   get_rule_ders() const
   {
-    return rule_ders;
+    return *rule_ders;
   }
-  const FGSContainer *
+  const FGSContainer &
   get_rule_ders_ss() const
   {
-    return rule_ders;
+    return *rule_ders_ss;
   }
 protected:
   void approxAtSteady();

dynare++/kord/decision_rule.cc
@@ -13,29 +13,12 @@
 #include <utility>
 #include <memory>
 
-template <>
-int DRFixPoint<KOrder::fold>::max_iter = 10000;
-template <>
-int DRFixPoint<KOrder::unfold>::max_iter = 10000;
-template <>
-double DRFixPoint<KOrder::fold>::tol = 1.e-10;
-template <>
-double DRFixPoint<KOrder::unfold>::tol = 1.e-10;
-template <>
-int DRFixPoint<KOrder::fold>::max_newton_iter = 50;
-template <>
-int DRFixPoint<KOrder::unfold>::max_newton_iter = 50;
-template <>
-int DRFixPoint<KOrder::fold>::newton_pause = 100;
-template <>
-int DRFixPoint<KOrder::unfold>::newton_pause = 100;
-
 // |FoldDecisionRule| conversion from |UnfoldDecisionRule|
 FoldDecisionRule::FoldDecisionRule(const UnfoldDecisionRule &udr)
   : DecisionRuleImpl<KOrder::fold>(ctraits<KOrder::fold>::Tpol(udr.nrows(), udr.nvars()),
                                    udr.ypart, udr.nu, udr.ysteady)
 {
-  for (const auto & it : udr)
+  for (const auto &it : udr)
     insert(std::make_unique<ctraits<KOrder::fold>::Ttensym>(*(it.second)));
 }
 
@@ -44,17 +27,8 @@ UnfoldDecisionRule::UnfoldDecisionRule(const FoldDecisionRule &fdr)
   : DecisionRuleImpl<KOrder::unfold>(ctraits<KOrder::unfold>::Tpol(fdr.nrows(), fdr.nvars()),
                                      fdr.ypart, fdr.nu, fdr.ysteady)
 {
-  for (const auto & it : fdr)
+  for (const auto &it : fdr)
     insert(std::make_unique<ctraits<KOrder::unfold>::Ttensym>(*(it.second)));
-}
-
-SimResults::~SimResults()
-{
-  for (int i = 0; i < getNumSets(); i++)
-    {
-      delete data[i];
-      delete shocks[i];
-    }
 }
 
 /* This runs simulations with an output to journal file. Note that we
@@ -92,7 +66,7 @@ SimResults::simulate(int num_sim, const DecisionRule &dr, const Vector &start,
     {
       RandomShockRealization sr(vcov, seed_generator::get_new_seed());
       rsrs.push_back(sr);
-      gr.insert(std::make_unique<SimulationWorker>(*this, dr, DecisionRule::horner,
+      gr.insert(std::make_unique<SimulationWorker>(*this, dr, DecisionRule::emethod::horner,
                                                    num_per+num_burn, start, rsrs.back()));
     }
   gr.run();
@@ -103,39 +77,35 @@ SimResults::simulate(int num_sim, const DecisionRule &dr, const Vector &start,
    and shocks are thrown away. */
 
 bool
-SimResults::addDataSet(TwoDMatrix *d, ExplicitShockRealization *sr, const ConstVector &st)
+SimResults::addDataSet(const TwoDMatrix &d, const ExplicitShockRealization &sr, const ConstVector &st)
 {
-  KORD_RAISE_IF(d->nrows() != num_y,
+  KORD_RAISE_IF(d.nrows() != num_y,
                 "Incompatible number of rows for SimResults::addDataSets");
-  KORD_RAISE_IF(d->ncols() != num_per+num_burn,
+  KORD_RAISE_IF(d.ncols() != num_per+num_burn,
                 "Incompatible number of cols for SimResults::addDataSets");
   bool ret = false;
-  if (d->isFinite())
+  if (d.isFinite())
     {
-      data.push_back(new TwoDMatrix((const TwoDMatrix &) (*d), num_burn, num_per));
-      shocks.push_back(new ExplicitShockRealization(
-                                                    ConstTwoDMatrix(sr->getShocks(), num_burn, num_per)));
+      data.emplace_back(d, num_burn, num_per);
+      shocks.emplace_back(ConstTwoDMatrix(sr.getShocks(), num_burn, num_per));
       if (num_burn == 0)
         start.emplace_back(st);
       else
-        start.emplace_back(d->getCol(num_burn-1));
+        start.emplace_back(d.getCol(num_burn-1));
       ret = true;
     }
 
-  delete d;
-  delete sr;
   return ret;
 }
 
 void
-SimResults::writeMat(const char *base, const char *lname) const
+SimResults::writeMat(const std::string &base, const std::string &lname) const
 {
-  char matfile_name[100];
-  sprintf(matfile_name, "%s.mat", base);
-  mat_t *matfd = Mat_Create(matfile_name, nullptr);
-  if (matfd != nullptr)
+  std::string matfile_name = base + ".mat";
+  mat_t *matfd = Mat_Create(matfile_name.c_str(), nullptr);
+  if (matfd)
     {
-      writeMat(matfd, lname);
+      writeMat(matfd, lname.c_str());
       Mat_Close(matfd);
     }
 }
@@ -144,17 +114,14 @@ SimResults::writeMat(const char *base, const char *lname) const
    appended. If there is only one matrix, the index is not appended. */
 
 void
-SimResults::writeMat(mat_t *fd, const char *lname) const
+SimResults::writeMat(mat_t *fd, const std::string &lname) const
 {
-  char tmp[100];
   for (int i = 0; i < getNumSets(); i++)
     {
+      std::string tmp = lname + "_data";
       if (getNumSets() > 1)
-        sprintf(tmp, "%s_data%d", lname, i+1);
-      else
-        sprintf(tmp, "%s_data", lname);
-      ConstTwoDMatrix m(*(data[i]));
-      m.writeMat(fd, tmp);
+        tmp += std::to_string(i+1);
+      data[i].writeMat(fd, tmp);
     }
 }
 
@@ -178,14 +145,10 @@ SimResultsStats::simulate(int num_sim, const DecisionRule &dr,
 
 /* Here we do not save the data itself, we save only mean and vcov. */
 void
-SimResultsStats::writeMat(mat_t *fd, const char *lname) const
+SimResultsStats::writeMat(mat_t *fd, const std::string &lname) const
 {
-  char tmp[100];
-  sprintf(tmp, "%s_mean", lname);
-  ConstTwoDMatrix m(num_y, 1, mean);
-  m.writeMat(fd, tmp);
-  sprintf(tmp, "%s_vcov", lname);
-  ConstTwoDMatrix(vcov).writeMat(fd, tmp);
+  ConstTwoDMatrix(num_y, 1, mean).writeMat(fd, lname + "_mean");;
+  vcov.writeMat(fd, lname + "_vcov");
 }
 
 void
@@ -195,11 +158,11 @@ SimResultsStats::calcMean()
   if (data.size()*num_per > 0)
     {
       double mult = 1.0/data.size()/num_per;
-      for (auto & i : data)
+      for (const auto &i : data)
         {
           for (int j = 0; j < num_per; j++)
             {
-              ConstVector col{i->getCol(j)};
+              ConstVector col{i.getCol(j)};
               mean.add(mult, col);
             }
         }
@@ -213,28 +176,19 @@ SimResultsStats::calcVcov()
     {
       vcov.zeros();
       double mult = 1.0/(data.size()*num_per - 1);
-      for (auto & i : data)
-        {
-          const TwoDMatrix &d = *i;
-          for (int j = 0; j < num_per; j++)
-            {
-              for (int m = 0; m < num_y; m++)
-                {
-                  for (int n = m; n < num_y; n++)
-                    {
-                      double s = (d.get(m, j)-mean[m])*(d.get(n, j)-mean[n]);
-                      vcov.get(m, n) += mult*s;
-                      if (m != n)
-                        vcov.get(n, m) += mult*s;
-                    }
-                }
-            }
-        }
+      for (const auto &d : data)
+        for (int j = 0; j < num_per; j++)
+          for (int m = 0; m < num_y; m++)
+            for (int n = m; n < num_y; n++)
+              {
+                double s = (d.get(m, j)-mean[m])*(d.get(n, j)-mean[n]);
+                vcov.get(m, n) += mult*s;
+                if (m != n)
+                  vcov.get(n, m) += mult*s;
+              }
     }
   else
-    {
-      vcov.infs();
-    }
+    vcov.infs();
 }
 
 void
@@ -256,13 +210,10 @@ SimResultsDynamicStats::simulate(int num_sim, const DecisionRule &dr,
 }
 
 void
-SimResultsDynamicStats::writeMat(mat_t *fd, const char *lname) const
+SimResultsDynamicStats::writeMat(mat_t *fd, const std::string &lname) const
 {
-  char tmp[100];
-  sprintf(tmp, "%s_cond_mean", lname);
-  ConstTwoDMatrix(mean).writeMat(fd, tmp);
-  sprintf(tmp, "%s_cond_variance", lname);
-  ConstTwoDMatrix(variance).writeMat(fd, tmp);
+  mean.writeMat(fd, lname + "_cond_mean");
+  variance.writeMat(fd, lname + "_cond_variance");
 }
 
 void
@@ -275,9 +226,9 @@ SimResultsDynamicStats::calcMean()
       for (int j = 0; j < num_per; j++)
         {
           Vector meanj{mean.getCol(j)};
-          for (auto & i : data)
+          for (const auto &i : data)
             {
-              ConstVector col{i->getCol(j)};
+              ConstVector col{i.getCol(j)};
               meanj.add(mult, col);
             }
         }
@@ -295,9 +246,9 @@ SimResultsDynamicStats::calcVariance()
         {
           ConstVector meanj{mean.getCol(j)};
           Vector varj{variance.getCol(j)};
-          for (auto & i : data)
+          for (const auto &i : data)
             {
-              Vector col{i->getCol(j)};
+              Vector col{i.getCol(j)};
               col.add(-1.0, meanj);
               for (int k = 0; k < col.length(); k++)
                 col[k] = col[k]*col[k];
@@ -306,9 +257,7 @@ SimResultsDynamicStats::calcVariance()
         }
     }
   else
-    {
-      variance.infs();
-    }
+    variance.infs();
 }
 
 void
@@ -334,7 +283,7 @@ SimResultsIRF::simulate(const DecisionRule &dr)
 {
   sthread::detach_thread_group gr;
   for (int idata = 0; idata < control.getNumSets(); idata++)
-    gr.insert(std::make_unique<SimulationIRFWorker>(*this, dr, DecisionRule::horner,
+    gr.insert(std::make_unique<SimulationIRFWorker>(*this, dr, DecisionRule::emethod::horner,
                                                     num_per, idata, ishock, imp));
   gr.run();
 }
@@ -345,8 +294,8 @@ SimResultsIRF::calcMeans()
   means.zeros();
   if (data.size() > 0)
     {
-      for (auto & i : data)
-        means.add(1.0, *i);
+      for (const auto &i : data)
+        means.add(1.0, i);
       means.mult(1.0/data.size());
     }
 }
@@ -357,9 +306,9 @@ SimResultsIRF::calcVariances()
   if (data.size() > 1)
     {
       variances.zeros();
-      for (auto & i : data)
+      for (const auto &i : data)
         {
-          TwoDMatrix d((const TwoDMatrix &)(*i));
+          TwoDMatrix d(i);
           d.add(-1.0, means);
           for (int j = 0; j < d.nrows(); j++)
             for (int k = 0; k < d.ncols(); k++)
@@ -368,19 +317,14 @@ SimResultsIRF::calcVariances()
         }
     }
   else
-    {
-      variances.infs();
-    }
+    variances.infs();
 }
 
 void
-SimResultsIRF::writeMat(mat_t *fd, const char *lname) const
+SimResultsIRF::writeMat(mat_t *fd, const std::string &lname) const
 {
-  char tmp[100];
-  sprintf(tmp, "%s_mean", lname);
-  means.writeMat(fd, tmp);
-  sprintf(tmp, "%s_var", lname);
-  variances.writeMat(fd, tmp);
+  means.writeMat(fd, lname + "_mean");
+  variances.writeMat(fd, lname + "_var");
 }
 
 void
@@ -416,21 +360,17 @@ RTSimResultsStats::simulate(int num_sim, const DecisionRule &dr, const Vector &s
     {
       RandomShockRealization sr(vcov, seed_generator::get_new_seed());
       rsrs.push_back(sr);
-      gr.insert(std::make_unique<RTSimulationWorker>(*this, dr, DecisionRule::horner,
+      gr.insert(std::make_unique<RTSimulationWorker>(*this, dr, DecisionRule::emethod::horner,
                                                      num_per, start, rsrs.back()));
     }
   gr.run();
 }
 
 void
-RTSimResultsStats::writeMat(mat_t *fd, const char *lname)
+RTSimResultsStats::writeMat(mat_t *fd, const std::string &lname)
 {
-  char tmp[100];
-  sprintf(tmp, "%s_rt_mean", lname);
-  ConstTwoDMatrix m(nc.getDim(), 1, mean);
-  m.writeMat(fd, tmp);
-  sprintf(tmp, "%s_rt_vcov", lname);
-  ConstTwoDMatrix(vcov).writeMat(fd, tmp);
+  ConstTwoDMatrix(nc.getDim(), 1, mean).writeMat(fd, lname + "_rt_mean");
+  vcov.writeMat(fd, lname + "_rt_vcov");
 }
 
 IRFResults::IRFResults(const DynamicModel &mod, const DecisionRule &dr,
@@ -440,51 +380,42 @@ IRFResults::IRFResults(const DynamicModel &mod, const DecisionRule &dr,
 {
   int num_per = control.getNumPer();
   JournalRecordPair pa(journal);
-  pa << "Calculating IRFs against control for " << (int) irf_list_ind.size() << " shocks and for "
+  pa << "Calculating IRFs against control for " << static_cast<int>(irf_list_ind.size()) << " shocks and for "
      << num_per << " periods" << endrec;
   const TwoDMatrix &vcov = mod.getVcov();
   for (int ishock : irf_list_ind)
     {
       double stderror = sqrt(vcov.get(ishock, ishock));
-      irf_res.push_back(new SimResultsIRF(control, model.numeq(), num_per,
-                                          ishock, stderror));
-      irf_res.push_back(new SimResultsIRF(control, model.numeq(), num_per,
-                                          ishock, -stderror));
+      irf_res.emplace_back(control, model.numeq(), num_per,
+                           ishock, stderror);
+      irf_res.emplace_back(control, model.numeq(), num_per,
+                           ishock, -stderror);
     }
 
   for (unsigned int ii = 0; ii < irf_list_ind.size(); ii++)
     {
-      irf_res[2*ii]->simulate(dr, journal);
-      irf_res[2*ii+1]->simulate(dr, journal);
+      irf_res[2*ii].simulate(dr, journal);
+      irf_res[2*ii+1].simulate(dr, journal);
     }
 }
 
-IRFResults::~IRFResults()
-{
-  for (auto & irf_re : irf_res)
-    delete irf_re;
-}
-
 void
-IRFResults::writeMat(mat_t *fd, const char *prefix) const
+IRFResults::writeMat(mat_t *fd, const std::string &prefix) const
 {
   for (unsigned int i = 0; i < irf_list_ind.size(); i++)
     {
-      char tmp[100];
       int ishock = irf_list_ind[i];
-      const char *shockname = model.getExogNames().getName(ishock);
-      sprintf(tmp, "%s_irfp_%s", prefix, shockname);
-      irf_res[2*i]->writeMat(fd, tmp);
-      sprintf(tmp, "%s_irfm_%s", prefix, shockname);
-      irf_res[2*i+1]->writeMat(fd, tmp);
+      auto shockname = model.getExogNames().getName(ishock);
+      irf_res[2*i].writeMat(fd, prefix + "_irfp_" + shockname);
+      irf_res[2*i+1].writeMat(fd, prefix + "_irfm_" + shockname);
     }
 }
 
 void
 SimulationWorker::operator()(std::mutex &mut)
 {
-  auto *esr = new ExplicitShockRealization(sr, np);
-  TwoDMatrix *m = dr.simulate(em, np, st, *esr);
+  ExplicitShockRealization esr(sr, np);
+  TwoDMatrix m{dr.simulate(em, np, st, esr)};
   {
     std::unique_lock<std::mutex> lk{mut};
     res.addDataSet(m, esr, st);
@@ -497,11 +428,10 @@ SimulationWorker::operator()(std::mutex &mut)
 void
 SimulationIRFWorker::operator()(std::mutex &mut)
 {
-  auto *esr
-    = new ExplicitShockRealization(res.control.getShocks(idata));
-  esr->addToShock(ishock, 0, imp);
-  TwoDMatrix *m = dr.simulate(em, np, res.control.getStart(idata), *esr);
-  m->add(-1.0, res.control.getData(idata));
+  ExplicitShockRealization esr(res.control.getShocks(idata));
+  esr.addToShock(ishock, 0, imp);
+  TwoDMatrix m{dr.simulate(em, np, res.control.getStart(idata), esr)};
+  m.add(-1.0, res.control.getData(idata));
   {
     std::unique_lock<std::mutex> lk{mut};
     res.addDataSet(m, esr, res.control.getStart(idata));
@@ -579,8 +509,7 @@ RandomShockRealization::choleskyFactor(const ConstTwoDMatrix &v)
 void
 RandomShockRealization::schurFactor(const ConstTwoDMatrix &v)
 {
-  SymSchurDecomp ssd(v);
-  ssd.getFactor(factor);
+  SymSchurDecomp(v).getFactor(factor);
 }
 
 void

commit 5eb769008b32157d31929157ee95c8f955452ecf

Dynare++: fix bug in the --steps option
This option could crash or give wrong results.

Bug introduced in 6d99b6d1208580c2b91e8f00a51da7d139c93e2e.
The Vector(Vector &) and Vector(const Vector &) constructors are not
equivalent.

Closes: #1804

dynare++/kord/approximation.cc
@@ -154,7 +154,7 @@ Approximation::walkStochSteady()
       JournalRecordPair pa(journal);
       pa << "Approximation about stochastic steady for sigma=" << sigma_so_far+dsigma << endrec;
 
-      Vector last_steady(model.getSteady());
+      Vector last_steady(const_cast<const Vector &>(model.getSteady()));
 
       // calculate fix-point of the last rule for ‘dsigma’
       /* We form the DRFixPoint object from the last rule with σ=dsigma. Then
@@ -180,7 +180,7 @@ Approximation::walkStochSteady()
          minus the old steady state. Then we create StochForwardDerivs object,
          which calculates the derivatives of g** expectations at new sigma and
          new steady. */
-      Vector dy(model.getSteady());
+      Vector dy(const_cast<const Vector &>(model.getSteady()));
       dy.add(-1.0, last_steady);
 
       StochForwardDerivs<Storage::fold> hh(ypart, model.nexog(), *rule_ders_ss, mom, dy,