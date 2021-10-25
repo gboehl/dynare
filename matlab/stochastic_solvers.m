gboehl/dynare

commit 57bbec1f727a19c42549bd09c4a2727731fcbc7a

stochastic_solvers.m: set dr.state_var for purely backwards models at order=1
Subsequent codes may need it. Closes https://git.dynare.org/Dynare/dynare/-/issues/1703

matlab/stochastic_solvers.m
@@ -216,6 +216,7 @@ if M_.maximum_endo_lead == 0
     % backward models: simplified code exist only at order == 1
     if local_order == 1
         [k1,~,k2] = find(kstate(:,4));
+        dr.state_var = k2;
         dr.ghx(:,k1) = -b\jacobia_(:,k2);
         if M_.exo_nbr
             dr.ghu =  -b\jacobia_(:,nz+1:end);

commit 17a58ccec60774ebf33c8cf3fc1ec4769ecf17e2

stochastic_solvers.m: fix bug introduced in 57bbec1f
Rely on lead_lag_incidence instead of kstate to identify states

matlab/stochastic_solvers.m
@@ -216,7 +216,11 @@ if M_.maximum_endo_lead == 0
     % backward models: simplified code exist only at order == 1
     if local_order == 1
         [k1,~,k2] = find(kstate(:,4));
-        dr.state_var = k2;
+        if M_.maximum_endo_lag
+            dr.state_var = find(M_.lead_lag_incidence(1,:));
+        else
+            dr.state_var = [];
+        end
         dr.ghx(:,k1) = -b\jacobia_(:,k2);
         if M_.exo_nbr
             dr.ghu =  -b\jacobia_(:,nz+1:end);