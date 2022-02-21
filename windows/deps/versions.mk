0b053f1ebf3470fe582bb4ac8f3943388b4fa2d6
Sébastien Villemot <sebastien@dynare.org>
2022-02-21 16:02:04 +0100

    Windows package: bump dependencies

--- windows/deps/versions.mk
+++ windows/deps/versions.mk
@@ -18,7 +18,7 @@ MATLAB64_VERSION = 20200930
 ## Build dependencies
 
 # pacman -Ss mingw-w64-x86_64-boost
-MINGW64_BOOST_VERSION = 1.78.0-1
+MINGW64_BOOST_VERSION = 1.78.0-3
 
 # pacman -Ss mingw-w64-x86_64-gsl
 MINGW64_GSL_VERSION = 2.7.1-1
@@ -39,27 +39,27 @@ MINGW64_HDF5_VERSION = 1.12.1-2
 
 # Dependency of HDF5 (provides szip library)
 # pacman -Ss mingw-w64-x86_64-libaec
-MINGW64_LIBAEC_VERSION = 1.0.6-1
+MINGW64_LIBAEC_VERSION = 1.0.6-2
 
 ## MinGW packages for the embedded compiler
 
 # pacman -Ss mingw-w64-x86_64-gcc$
-MINGW64_GCC_VERSION = 11.2.0-6
+MINGW64_GCC_VERSION = 11.2.0-9
 
 # pacman -Ss mingw-w64-x86_64-gmp
 MINGW64_GMP_VERSION = 6.2.1-3
 
 # pacman -Ss mingw-w64-x86_64-binutils
-MINGW64_BINUTILS_VERSION = 2.37-4
+MINGW64_BINUTILS_VERSION = 2.38-1
 
 # pacman -Ss mingw-w64-x86_64-headers-git
-MINGW64_HEADERS_VERSION = 9.0.0.6373.5be8fcd83-1
+MINGW64_HEADERS_VERSION = 9.0.0.6373.5be8fcd83-2
 
 # pacman -Ss mingw-w64-x86_64-crt-git
-MINGW64_CRT_VERSION = 9.0.0.6373.5be8fcd83-1
+MINGW64_CRT_VERSION = 9.0.0.6373.5be8fcd83-2
 
 # pacman -Ss mingw-w64-x86_64-winpthreads-git
-MINGW64_WINPTHREADS_VERSION = 9.0.0.6373.5be8fcd83-1
+MINGW64_WINPTHREADS_VERSION = 9.0.0.6373.5be8fcd83-2
 
 # pacman -Ss mingw-w64-x86_64-zstd
-MINGW64_ZSTD_VERSION = 1.5.1-1
+MINGW64_ZSTD_VERSION = 1.5.2-1
