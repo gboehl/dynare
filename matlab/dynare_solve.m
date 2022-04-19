gboehl/dynare

commit ef2bb4e669230762dfc7a57dfe1e4551ade95a0c

Return and use errorcode from dynare_solve.
Note that the interpretation of the error code depends on the nonlinear solver.

matlab/dynare_solve.m
@@ -1,4 +1,4 @@
-function [x, errorflag, fvec, fjac, exitflag] = dynare_solve(f, x, options, varargin)
+function [x, errorflag, fvec, fjac, errorcode] = dynare_solve(f, x, options, varargin)
 
 % Solves a nonlinear system of equations, f(x) = 0 with n unknowns
 % and n equations.
@@ -14,7 +14,13 @@ function [x, errorflag, fvec, fjac, exitflag] = dynare_solve(f, x, options, vara
 % - errorflag    [logical]          scalar, true iff the model can not be solved.
 % - fvec         [double]           n×1 vector, function value at x (f(x), used for debugging when errorflag is true).
 % - fjac         [double]           n×n matrix, Jacobian value at x (J(x), used for debugging when errorflag is true).
-% - exitflag     [integer]          scalar,
+% - errorcode    [integer]          scalar.
+%
+% REMARKS
+% Interpretation of the error code depends on the algorithm, except if value of errorcode is
+%
+%        -10  -> System of equation ill-behaved at the initial guess (Inf, Nans or complex numbers).
+%        -11  -> Initial guess is a solution of the system of equations.
 
 % Copyright © 2001-2022 Dynare Team
 %
@@ -33,8 +39,6 @@ function [x, errorflag, fvec, fjac, exitflag] = dynare_solve(f, x, options, vara
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <https://www.gnu.org/licenses/>.
 
-exitflag = nan;
-
 jacobian_flag = options.jacobian_flag; % true iff Jacobian is returned by f routine (as a second output argument).
 
 % Set tolerance parameter depending the the caller function.
@@ -49,7 +53,7 @@ else
         caller_file_name=stack(1).file;
     end
 end
-if strcmp(caller_file_name, 'solve_stacked_problem.m') || strcmp(caller_file_name, 'sim1_purely_backward.m') 
+if strcmp(caller_file_name, 'solve_stacked_problem.m') || strcmp(caller_file_name, 'sim1_purely_backward.m')
     tolf = options.dynatol.f;
     tolx = options.dynatol.x;
 else
@@ -63,7 +67,7 @@ else
     maxit = options.steady.maxit;
 end
 
-errorflag = false;
+errorflag = false; % Let's be optimistic!
 nn = size(x,1);
 
 % Keep a copy of the initial guess.
@@ -98,7 +102,7 @@ if jacobian_flag
     if ~all(isfinite(fvec)) || any(isinf(fjac(:))) || any(isnan((fjac(:)))) || any(~isreal(fvec)) || any(~isreal(fjac(:)))
         if ~ismember(options.solve_algo,[10,11]) && max(abs(fvec))< tolf
             % return if initial value solves the problem except if a mixed complementarity problem is to be solved (complementarity conditions may not be satisfied)
-            exitflag = -1;
+            errorcode = -11;
             return;
         end
         disp_verbose('Randomize initial guess...', options.verbosity)
@@ -134,7 +138,7 @@ else
     fjac = zeros(nn, nn);
     if ~ismember(options.solve_algo,[10,11]) && max(abs(fvec)) < tolf
         % return if initial value solves the problem except if a mixed complementarity problem is to be solved (complementarity conditions may not be satisfied)
-        exitflag = -1;
+        errorcode = -11;
         return;
     end
     wrong_initial_guess_flag = false;
@@ -170,6 +174,7 @@ end
 
 % Exit with error if no initial guess has been found.
 if wrong_initial_guess_flag
+    errorcode = -10;
     errorflag = true;
     x = x0;
     return
@@ -196,7 +201,7 @@ if options.solve_algo == 0
         options4fsolve.Jacobian = 'off';
     end
     if ~isoctave
-        [x, ~, exitflag] = fsolve(f, x, options4fsolve, arguments{:});
+        [x, ~, errorcode] = fsolve(f, x, options4fsolve, arguments{:});
     else
         % Under Octave, use a wrapper, since fsolve() does not have a 4th arg
         if ischar(f)
@@ -205,19 +210,20 @@ if options.solve_algo == 0
             f2 = f;
         end
         f = @(x) f2(x, arguments{:});
-        [x, ~, exitflag] = fsolve(f, x, options4fsolve);
+        [x, ~, errorcode] = fsolve(f, x, options4fsolve);
     end
-    if exitflag == 1
+    if errorcode==1
         errorflag = false;
-    elseif exitflag > 1
-        if ischar(f)
-            f2 = str2func(f);
-        else
-            f2 = f;
+    elseif errorcode>1
+        if ~isoctave
+            if ischar(f)
+                f2 = str2func(f);
+            else
+                f2 = f;
+            end
+            f = @(x) f2(x, arguments{:});
         end
-        f = @(x) f2(x, arguments{:});
-        fvec = feval(f, x);
-        if max(abs(fvec)) >= tolf
+        if max(abs(fvec)) > tolf
             errorflag = true;
         else
             errorflag = false;
@@ -225,10 +231,13 @@ if options.solve_algo == 0
     else
         errorflag = true;
     end
+    [fvec, fjac] = feval(f, x, arguments{:});
 elseif options.solve_algo==1
-    [x, errorflag, exitflag] = solve1(f, x, 1:nn, 1:nn, jacobian_flag, options.gstep, tolf, tolx, maxit, [], options.debug, arguments{:});
+    [x, errorflag, errorcode] = solve1(f, x, 1:nn, 1:nn, jacobian_flag, options.gstep, tolf, tolx, maxit, [], options.debug, arguments{:});
+    [fvec, fjac] = feval(f, x, arguments{:});
 elseif options.solve_algo==9
-    [x, errorflag, exitflag] = trust_region(f, x, 1:nn, 1:nn, jacobian_flag, options.gstep, tolf, tolx, maxit, options.trust_region_initial_step_bound_factor, options.debug, arguments{:});
+    [x, errorflag, errorcode] = trust_region(f, x, 1:nn, 1:nn, jacobian_flag, options.gstep, tolf, tolx, maxit, options.trust_region_initial_step_bound_factor, options.debug, arguments{:});
+    [fvec, fjac] = feval(f, x, arguments{:});
 elseif ismember(options.solve_algo, [2, 12, 4])
     if ismember(options.solve_algo, [2, 12])
         solver = @solve1;
@@ -304,11 +313,11 @@ elseif ismember(options.solve_algo, [2, 12, 4])
                 dprintf('DYNARE_SOLVE (solve_algo=2|4|12): solving block %u with trust_region routine.', i);
             end
         end
-        [x, errorflag, exitflag] = solver(f, x, j1(j), j2(j), jacobian_flag, ...
-                                          options.gstep, ...
-                                          tolf, options.solve_tolx, maxit, ...
-                                          options.trust_region_initial_step_bound_factor, ...
-                                          options.debug, arguments{:});
+        [x, errorflag, errorcode] = solver(f, x, j1(j), j2(j), jacobian_flag, ...
+                                           options.gstep, ...
+                                           tolf, options.solve_tolx, maxit, ...
+                                           options.trust_region_initial_step_bound_factor, ...
+                                           options.debug, arguments{:});
         fre = true;
         if errorflag
             return
@@ -317,27 +326,34 @@ elseif ismember(options.solve_algo, [2, 12, 4])
     fvec = feval(f, x, arguments{:});
     if max(abs(fvec))>tolf
         disp_verbose('Call solver on the full nonlinear problem.',options.verbosity)
-        [x, errorflag, exitflag] = solver(f, x, 1:nn, 1:nn, jacobian_flag, ...
-                                          options.gstep, tolf, options.solve_tolx, maxit, ...
-                                          options.trust_region_initial_step_bound_factor, ...
-                                          options.debug, arguments{:});
+        [x, errorflag, errorcode] = solver(f, x, 1:nn, 1:nn, jacobian_flag, ...
+                                           options.gstep, tolf, options.solve_tolx, maxit, ...
+                                           options.trust_region_initial_step_bound_factor, ...
+                                           options.debug, arguments{:});
     end
+    [fvec, fjac] = feval(f, x, arguments{:});
 elseif options.solve_algo==3
     if jacobian_flag
-        [x, errorflag] = csolve(f, x, f, tolf, maxit, arguments{:});
+        [x, errorcode] = csolve(f, x, f, tolf, maxit, arguments{:});
     else
-        [x, errorflag] = csolve(f, x, [], tolf, maxit, arguments{:});
+        [x, errorcode] = csolve(f, x, [], tolf, maxit, arguments{:});
+    end
+    if errorcode==0
+        errorflag = false;
+    else
+        errorflag = true;
     end
     [fvec, fjac] = feval(f, x, arguments{:});
 elseif options.solve_algo==10
     % LMMCP
     olmmcp = options.lmmcp;
-    [x, ~, exitflag] = lmmcp(f, x, olmmcp.lb, olmmcp.ub, olmmcp, arguments{:});
-    if exitflag==1
+    [x, fvec, errorcode, ~, fjac] = lmmcp(f, x, olmmcp.lb, olmmcp.ub, olmmcp, arguments{:});
+    if errorcode==1
         errorflag = false;
     else
         errorflag = true;
     end
+    [fvec, fjac] = feval(f, x, arguments{:});
 elseif options.solve_algo == 11
     % PATH mixed complementary problem
     % PATH linear mixed complementary problem
@@ -355,6 +371,8 @@ elseif options.solve_algo == 11
     catch
         errorflag = true;
     end
+    errorcode = nan; % There is no error code for this algorithm, as PATH is closed source it is unlikely we can fix that.
+    [fvec, fjac] = feval(f, x, arguments{:});
 elseif ismember(options.solve_algo, [13, 14])
     if ~jacobian_flag
         error('DYNARE_SOLVE: option solve_algo=13|14 needs computed Jacobian')
@@ -366,7 +384,7 @@ elseif ismember(options.solve_algo, [13, 14])
         auxstruct.isloggedlhs = isloggedlhs;
         auxstruct.isauxdiffloggedrhs = isauxdiffloggedrhs;
     end
-    [x, errorflag, exitflag] = block_trust_region(f, x, tolf, options.solve_tolx, maxit, options.trust_region_initial_step_bound_factor, options.debug, auxstruct, arguments{:});
+    [x, errorflag, errorcode] = block_trust_region(f, x, tolf, options.solve_tolx, maxit, options.trust_region_initial_step_bound_factor, options.debug, auxstruct, arguments{:});
     [fvec, fjac] = feval(f, x, arguments{:});
 else
     error('DYNARE_SOLVE: option solve_algo must be one of [0,1,2,3,4,9,10,11,12,13,14]')

matlab/backward/backward_model_inversion.m
@@ -14,7 +14,7 @@ function [endogenousvariables, exogenousvariables] = backward_model_inversion(co
 %
 % REMARKS
 
-% Copyright (C) 2017 Dynare Team
+% Copyright © 2017-2022 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -108,9 +108,9 @@ for t = 1:nobs(constraints)
     % values) and the free exogenous variables (initialized with 0).
     z = [Y(freeendogenousvariables_id,ity-1); zeros(nxfree, 1)];
     % Solves for z.
-    [z, failed] = dynare_solve(model_dtransf, z, DynareOptions, model_dynamic, ylag, ycur, X, DynareModel.params, DynareOutput.steady_state, itx, ModelInversion);
+    [z, failed, ~, ~, errorcode] = dynare_solve(model_dtransf, z, DynareOptions, model_dynamic, ylag, ycur, X, DynareModel.params, DynareOutput.steady_state, itx, ModelInversion);
     if failed
-        error('Enable to solve the system of equations!')
+        error('Nonlinear solver failed with errorcode=%i', errorcode)
     end
     % Update the matrix of exogenous variables.
     X(itx,freeinnovations_id) = z(nyfree+(1:nxfree));

matlab/backward/simul_backward_nonlinear_model_.m
@@ -56,17 +56,17 @@ for it = initialconditions.nobs+(1:samplesize)
     y = y_;                            % A good guess for the initial conditions is the previous values for the endogenous variables.
     try
         if ismember(DynareOptions.solve_algo, [12,14])
-            [DynareOutput.endo_simul(:,it), errorflag, ~, ~, exitflag] = ...
+            [DynareOutput.endo_simul(:,it), errorflag, ~, ~, errorcode] = ...
                 dynare_solve(model_dynamic_s, y, DynareOptions, ...
                              DynareModel.isloggedlhs, DynareModel.isauxdiffloggedrhs, DynareModel.endo_names, DynareModel.lhs, ...
                              model_dynamic, ylag, DynareOutput.exo_simul, DynareModel.params, DynareOutput.steady_state, it);
         else
-            [DynareOutput.endo_simul(:,it), errorflag, ~, ~, exitflag] = ...
+            [DynareOutput.endo_simul(:,it), errorflag, ~, ~, errorcode] = ...
                 dynare_solve(model_dynamic_s, y, DynareOptions, ...
                              model_dynamic, ylag, DynareOutput.exo_simul, DynareModel.params, DynareOutput.steady_state, it);
         end
         if errorflag
-            error()
+            error('Nonlinear solver routine failed with errorcode=%i in period %i.', errorcode, it)
         end
     catch
         DynareOutput.endo_simul = DynareOutput.endo_simul(:, 1:it-1);
@@ -108,7 +108,7 @@ for it = initialconditions.nobs+(1:samplesize)
         %
         % Evaluate and check the residuals
         %
-        r = feval(model_dynamic, [ytm; ytm(iy1)], DynareOutput.exo_simul, DynareModel.params, DynareOutput.steady_state, it);
+        [r, J] = feval(model_dynamic, [ytm; ytm(iy1)], DynareOutput.exo_simul, DynareModel.params, DynareOutput.steady_state, it);
         residuals_evaluating_to_nan = isnan(r);
         residuals_evaluating_to_inf = isinf(r);
         residuals_evaluating_to_complex = ~isreal(r);
@@ -129,34 +129,6 @@ for it = initialconditions.nobs+(1:samplesize)
             skipline()
             display_names_of_problematic_equations(DynareModel, residuals_evaluating_to_nan);
             skipline()
-        end
-        %
-        % Display value of exitflag if available (fsolve, solve_algo=0, only)
-        %
-        if ~isnan(exitflag)
-            skipline()
-            switch exitflag
-              case 0
-                disp('Returned value for exitflag is 0 (maximum number of iterations or evaluation reached).')
-              case -1
-                if options.solve_algo==0
-                    disp('Returned value for exitflag is -1 (objective function stopped algorithm).')
-                elseif options.solve_algo==1
-                    disp('Returned value for exitflag is -1 (objective function cannot be evaluated at the initial guess).')
-                end
-              case -3
-                if options.solve_algo==0
-                    disp('Returned value for exitflag is -3 (Trust region radius became too small, trust-region-dogleg algorithm).')
-                elseif options.solve_algo==1
-                    disp('Returned value for exitflag is -3 (Convergence too slow).')
-                end
-              case -2
-                if options.solve_algo==0
-                    disp('Returned value for exitflag is -2.')
-                elseif options.solve_algo==1
-                    disp('Returned value for exitflag is -2 (Spurious convergence).')
-                end
-            end
         end
         break
         % TODO Implement same checks with the jacobian matrix.

matlab/ep/solve_stochastic_perfect_foresight_model_1.m
@@ -1,6 +1,6 @@
-function [flag,endo_simul,err,y] = solve_stochastic_perfect_foresight_model_1(endo_simul,exo_simul,Options,pfm,order,varargin)
+function [errorflag, endo_simul, errorcode, y] = solve_stochastic_perfect_foresight_model_1(endo_simul, exo_simul, Options, pfm, order, varargin)
 
-% Copyright (C) 2012-2017 Dynare Team
+% Copyright © 2012-2022 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -22,9 +22,9 @@ if nargin < 6
 else
     homotopy_parameter = varargin{1};
 end
+
 flag = 0;
 err = 0;
-stop = 0;
 
 EpOptions = Options.ep;
 
@@ -143,7 +143,7 @@ Options.steady.maxit = 100;
 y = repmat(steady_state,block_nbr,1);
 Options.solve_algo = Options.ep.solve_algo;
 Options.steady.maxit = Options.ep.maxit;
-[y,info] = dynare_solve(@ep_problem_2,y,Options,exo_simul,pfm);
+[y, errorflag, ~, ~, errorcode] = dynare_solve(@ep_problem_2,y,Options,exo_simul,pfm);
 if info
     flag = 1;
     err = info;

matlab/evaluate_steady_state.m
@@ -22,7 +22,7 @@ function [ys,params,info] = evaluate_steady_state(ys_init,M,options,oo,steadysta
 % SPECIAL REQUIREMENTS
 %   none
 
-% Copyright (C) 2001-2021 Dynare Team
+% Copyright © 2001-2022 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -262,17 +262,19 @@ elseif ~options.bytecode && ~options.block
     if ~options.linear
         % non linear model
         static_model = str2func([M.fname '.static']);
-        [ys,check] = dynare_solve(@static_problem,...
-                                  ys_init,...
-                                  options, exo_ss, params,...
-                                  M.endo_nbr,...
-                                  static_model);
+        [ys, check] = dynare_solve(@static_problem, ...
+                                   ys_init, ...
+                                   options, exo_ss, params, ...
+                                   M.endo_nbr, ...
+                                   static_model);
         if check && options.debug
-            [ys,check,fvec,fjac] = dynare_solve(@static_problem,...
-                                                ys_init,...
-                                                options, exo_ss, params,...
-                                                M.endo_nbr,...
-                                                static_model);
+            [ys, check, fvec, fjac, errorcode] = dynare_solve(@static_problem, ...
+                                                              ys_init, ...
+                                                              options, exo_ss, params, ...
+                                                              M.endo_nbr, ...
+                                                              static_model);
+            dprintf('Nonlinear solver routine returned errorcode=%i.', errorcode)
+            skipline()
             [infrow,infcol]=find(isinf(fjac) | isnan(fjac));
             if ~isempty(infrow)
                 fprintf('\nSTEADY:  The Jacobian at the initial values contains Inf or NaN. The problem arises from: \n')

matlab/evaluate_steady_state_file.m
@@ -19,7 +19,7 @@ function [ys,params,info] = evaluate_steady_state_file(ys_init,exo_ss,M,options,
 % SPECIAL REQUIREMENTS
 %   none
 
-% Copyright (C) 2001-2019 Dynare Team
+% Copyright © 2001-2022 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -127,5 +127,5 @@ elseif ~isempty(options.steadystate_partial)
     for i = 1:nov
         indv(i) = strmatch(ssvar(i), M.endo_names, 'exact');
     end
-    [ys,~] = dynare_solve('restricted_steadystate', ys(indv), options, exo_ss,indv);
+    ys = dynare_solve('restricted_steadystate', ys(indv), options, exo_ss,indv);
 end

matlab/perfect-foresight-models/sim1_purely_backward.m
@@ -2,7 +2,7 @@ function [endogenousvariables, info] = sim1_purely_backward(endogenousvariables,
 
 % Performs deterministic simulation of a purely backward model
 
-% Copyright © 2012-2021 Dynare Team
+% Copyright © 2012-2022 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -39,14 +39,17 @@ for it = M.maximum_lag + (1:options.periods)
     y = endogenousvariables(:,it-1);        % Values at previous period, also used as guess value for current period
     ylag = y(iyb);
     if ismember(options.solve_algo, [12,14])
-        [tmp, check] = dynare_solve(dynamicmodel_s, y, options, M.isloggedlhs, M.isauxdiffloggedrhs, M.endo_names, M.lhs, ...
-                                    dynamicmodel, ylag, exogenousvariables, M.params, steadystate, it);
+        [tmp, check, ~, ~, errorcode] = dynare_solve(dynamicmodel_s, y, options, M.isloggedlhs, M.isauxdiffloggedrhs, M.endo_names, M.lhs, ...
+                                                     dynamicmodel, ylag, exogenousvariables, M.params, steadystate, it);
     else
-        [tmp, check] = dynare_solve(dynamicmodel_s, y, options, ...
-                                    dynamicmodel, ylag, exogenousvariables, M.params, steadystate, it);
+        [tmp, check, ~, ~, errorcode] = dynare_solve(dynamicmodel_s, y, options, ...
+                                                     dynamicmodel, ylag, exogenousvariables, M.params, steadystate, it);
     end
     if check
         info.status = false;
+        if options.debug
+            dprintf('sim1_purely_backward: Nonlinear solver routine failed with errorcode=%i. in period %i', errorcode, it)
+        end
     end
     endogenousvariables(:,it) = tmp;
 end

matlab/perfect-foresight-models/sim1_purely_static.m
@@ -2,7 +2,7 @@ function [endogenousvariables, info] = sim1_purely_static(endogenousvariables, e
 
 % Performs deterministic simulation of a purely static model
 
-% Copyright © 2021 Dynare Team
+% Copyright © 2021-2022 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -36,14 +36,17 @@ y = endogenousvariables(:,1);
 
 for it = 1:options.periods
     if ismember(options.solve_algo, [12,14])
-        [tmp, check] = dynare_solve(dynamicmodel_s, y, options, M.isloggedlhs, M.isauxdiffloggedrhs, M.endo_names, M.lhs, ...
-                                    dynamicmodel, exogenousvariables, M.params, steadystate, it);
+        [tmp, check, ~, ~, errorcode] = dynare_solve(dynamicmodel_s, y, options, M.isloggedlhs, M.isauxdiffloggedrhs, M.endo_names, M.lhs, ...
+                                                     dynamicmodel, exogenousvariables, M.params, steadystate, it);
     else
-        [tmp, check] = dynare_solve(dynamicmodel_s, y, options, ...
-                                    dynamicmodel, exogenousvariables, M.params, steadystate, it);
+        [tmp, check, ~, ~, errorcode] = dynare_solve(dynamicmodel_s, y, options, ...
+                                                     dynamicmodel, exogenousvariables, M.params, steadystate, it);
     end
     if check
         info.status = false;
+        if options.debug
+            dprintf('sim1_purely_static: Nonlinear solver routine failed with errorcode=%i in period %i.', errorcode, it)
+        end
     end
     endogenousvariables(:,it) = tmp;
     y = endogenousvariables(:,it);

commit ce7540747bff541989624ee6a7e92b9c32ba9e92

dynare_solve: fix bug introduced in ef2bb4e669230762dfc7a57dfe1e4551ade95a0c
The feval(f, x, args{:}) call would fail under Octave because the contents of
args{:} would be appended twice (hence too many input arguments error).

The same bug would materialize under MATLAB for errorcode>1.

matlab/dynare_solve.m
@@ -184,20 +184,11 @@ if options.solve_algo == 0
         else
             f2 = f;
         end
-        f = @(x) f2(x, args{:});
-        [x, ~, errorcode] = fsolve(f, x, options4fsolve);
+        [x, ~, errorcode] = fsolve(@(x) f2(x, args{:}), x, options4fsolve);
     end
     if errorcode==1
         errorflag = false;
     elseif errorcode>1
-        if ~isoctave
-            if ischar(f)
-                f2 = str2func(f);
-            else
-                f2 = f;
-            end
-            f = @(x) f2(x, args{:});
-        end
         if max(abs(fvec)) > tolf
             errorflag = true;
         else