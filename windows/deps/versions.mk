e6c35f205d316b01c2a52e9ef60c0e4e6c18446a
Sébastien Villemot <sebastien@dynare.org>
2022-06-09 11:54:30 +0200

    Windows package: bump dependencies
    In particular, upgrade the embedded compiler to GCC 12.

--- windows/deps/versions.mk
+++ windows/deps/versions.mk
@@ -18,16 +18,16 @@ MATLAB64_VERSION = 20200930
 ## Build dependencies
 
 # pacman -Ss mingw-w64-x86_64-boost
-MINGW64_BOOST_VERSION = 1.78.0-3
+MINGW64_BOOST_VERSION = 1.79.0-3
 
 # pacman -Ss mingw-w64-x86_64-gsl
 MINGW64_GSL_VERSION = 2.7.1-1
 
 # pacman -Ss mingw-w64-x86_64-openblas
-MINGW64_OPENBLAS_VERSION = 0.3.20-2
+MINGW64_OPENBLAS_VERSION = 0.3.20-3
 
 # pacman -Ss mingw-w64-x86_64-matio
-MINGW64_MATIO_VERSION = 1.5.21-2
+MINGW64_MATIO_VERSION = 1.5.23-1
 
 # Dependency of matio (and of the MinGW compiler)
 # pacman -Ss mingw-w64-x86_64-zlib
@@ -35,7 +35,7 @@ MINGW64_ZLIB_VERSION = 1.2.12-1
 
 # Dependency of matio
 # pacman -Ss mingw-w64-x86_64-hdf5
-MINGW64_HDF5_VERSION = 1.12.1-2
+MINGW64_HDF5_VERSION = 1.12.2-1
 
 # Dependency of HDF5 (provides szip library)
 # pacman -Ss mingw-w64-x86_64-libaec
@@ -44,22 +44,22 @@ MINGW64_LIBAEC_VERSION = 1.0.6-2
 ## MinGW packages for the embedded compiler
 
 # pacman -Ss mingw-w64-x86_64-gcc$
-MINGW64_GCC_VERSION = 11.2.0-10
+MINGW64_GCC_VERSION = 12.1.0-2
 
 # pacman -Ss mingw-w64-x86_64-gmp
 MINGW64_GMP_VERSION = 6.2.1-3
 
 # pacman -Ss mingw-w64-x86_64-binutils
-MINGW64_BINUTILS_VERSION = 2.38-2
+MINGW64_BINUTILS_VERSION = 2.38-3
 
 # pacman -Ss mingw-w64-x86_64-headers-git
-MINGW64_HEADERS_VERSION = 9.0.0.6454.b4445ee52-1
+MINGW64_HEADERS_VERSION = 10.0.0.r32.g89bacd2be-1
 
 # pacman -Ss mingw-w64-x86_64-crt-git
-MINGW64_CRT_VERSION = 9.0.0.6454.b4445ee52-1
+MINGW64_CRT_VERSION = 10.0.0.r32.g89bacd2be-1
 
 # pacman -Ss mingw-w64-x86_64-winpthreads-git
-MINGW64_WINPTHREADS_VERSION = 9.0.0.6454.b4445ee52-1
+MINGW64_WINPTHREADS_VERSION = 10.0.0.r32.g89bacd2be-1
 
 # pacman -Ss mingw-w64-x86_64-zstd
 MINGW64_ZSTD_VERSION = 1.5.2-2
