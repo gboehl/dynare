0a9dc0137a69cca71a254b9dd11c7e35ba1e7cf5
Sébastien Villemot <sebastien@dynare.org>
2022-07-05 11:53:36 +0200

    k_order_welfare MEX: fix documentation of output arguments

--- matlab/mex/k_order_welfare.m
+++ matlab/mex/k_order_welfare.m
@@ -1,4 +1,4 @@
-% [unc_welfare, U_dynpp_derivs, W_dynpp_derivs, U_dyn_derivs, W_dyn_derivs] = k_order_welfare(dr, DynareModel, DynareOptions)
+% W = k_order_welfare(dr, DynareModel, DynareOptions)
 % computes a k-th order approximation of welfare
 %
 % INPUTS
@@ -8,41 +8,16 @@
 %
 % OUTPUTS
 %
-% unc_welfare      double   approximation of conditional welfare from the non-stochastic steady state and allowing stochastic shocks
-%
-% U_dynpp_derivs   struct   Derivatives of the felicity function in Dynare++ format.
-%                         The tensors are folded and the Taylor coefficients (1/n!) are
-%                         included.
-%                         Fieldnames are U_0, U_1, U_2, …
-% W_dynpp_derivs   struct   Derivatives of the welfare function in Dynare++ format.
+% W                struct   Derivatives of the welfare function in Dynare++ format.
 %                         The tensors are folded and the Taylor coefficients (1/n!) are
 %                         included.
 %                         Fieldnames are W_0, W_1, W_2, …
-% U_dyn_derivs     struct   Derivatives of the felicity function in Dynare format, up to third order.
-%                         Matrices are dense and unfolded. The Taylor coefficients (1/2
-%                         and 1/6) aren't included.
-%                         The derivatives w.r.t. different categories of variables
-%                         are separated.
-%                         Fieldnames are:
-%                          + Uy, Uu
-%                          + if order ≥ 2: Uyy, Uyu, Uuu, Uss
-%                          + if order ≥ 3: Uyyy, Uyyu, Uyuu, Uuuu, Uyss, Uuss
-%
-% W_dyn_derivs     struct   Derivatives of the welfare function in Dynare format, up to third order.
-%                         Matrices are dense and unfolded. The Taylor coefficients (1/2
-%                         and 1/6) aren't included.
-%                         The derivatives w.r.t. different categories of variables
-%                         are separated.
-%                         Fieldnames are:
-%                          + Wy, Wu
-%                          + if order ≥ 2: Wyy, Wyu, Wuu, Wss
-%                          + if order ≥ 3: Wyyy, Wyyu, Wyuu, Wuuu, Wyss, Wuss
 %
 % k_order_welfare is a compiled MEX function. Its source code is in
 % dynare/mex/sources/k_order_welfare/k_order_welfare.cc and it uses code provided by
 % dynare++
 
-% Copyright © 2021 Dynare Team
+% Copyright © 2021-2022 Dynare Team
 %
 % This file is part of Dynare.
 %
