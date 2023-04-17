cbef92f495a55aef943a9262098645d20c13e74c
Sébastien Villemot <sebastien@dynare.org>
2023-04-17 13:39:42 +0200

    Octave compatibility fix: the 3rd output argument to fminsearch was added in Octave 4.4
    (cherry picked from commit e4136962772903c0d89cf74cbdee7c9fa316c856)

--- matlab/optimization/dynare_minimize_objective.m
+++ matlab/optimization/dynare_minimize_objective.m
@@ -353,9 +353,9 @@ switch minimizer_algorithm
         [opt_par_values,fval,exitflag] = fminsearch(objective_function,start_par_value,optim_options,varargin{:});
     else
         % Under Octave, use a wrapper, since fminsearch() does not have a
-        % 4th arg, and only has two output args
+        % 4th arg.
         func = @(x) objective_function(x,varargin{:});
-        [opt_par_values,fval] = fminsearch(func,start_par_value,optim_options);
+        [opt_par_values,fval,exitflag] = fminsearch(func,start_par_value,optim_options);
     end
   case 8
     % Dynare implementation of the simplex algorithm.
