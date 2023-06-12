gboehl/dynare

commit 7722e8e36b1318584dd5fb44846d5617e7278dda

Perfect foresight: inner functions no longer return a modified oo_
They now only return what’s really their output (simulated paths, maximum
residual error…). This is a move towards a more functional programming style.

matlab/perfect-foresight-models/perfect_foresight_solver_core.m
@@ -1,4 +1,4 @@
-function [oo_, maxerror] = perfect_foresight_solver_core(M_, options_, oo_)
+function [y, success, maxerror, iter, per_block_status] = perfect_foresight_solver_core(M_, options_, oo_)
 
 % Core function calling solvers for perfect foresight model
 %
@@ -8,8 +8,11 @@ function [oo_, maxerror] = perfect_foresight_solver_core(M_, options_, oo_)
 % - oo_                 [struct] contains results
 %
 % OUTPUTS
-% - oo_                 [struct] contains results
+% - y                   [double array] path for the endogenous variables (solution)
+% - success             [logical] Whether a solution was found
 % - maxerror            [double] contains the maximum absolute error
+% - iter                [integer] Number of iterations of the underlying nonlinear solver (empty for non-iterative methods)
+% - per_block_status    [struct] In the case of block decomposition, provides per-block solver status information (empty if no block decomposition)
 
 % Copyright © 2015-2023 Dynare Team
 %
@@ -49,62 +52,60 @@ if options_.linear && (isequal(options_.stack_solve_algo, 0) || isequal(options_
     options_.linear_approximation = true;
 end
 
+maxerror = [];
+iter = [];
+per_block_status = [];
+
 if options_.block
     if M_.block_structure.time_recursive
         error('Internal error: can''t perform stacked perfect foresight simulation with time-recursive block decomposition')
     end
     if options_.bytecode
         try
-            oo_.endo_simul = bytecode('dynamic', 'block_decomposed', oo_.endo_simul, oo_.exo_simul, M_.params, repmat(oo_.steady_state,1, periods+2), periods);
-            oo_.deterministic_simulation.status = true;
+            y = bytecode('dynamic', 'block_decomposed', oo_.endo_simul, oo_.exo_simul, M_.params, repmat(oo_.steady_state,1, periods+2), periods);
+            success = true;
         catch ME
             disp(ME.message)
             if options_.no_homotopy
                 error('Error in bytecode')
             end
-            oo_.deterministic_simulation.status = false;
+            success = false;
         end
     else
-        oo_ = solve_block_decomposed_problem(options_, M_, oo_);
+        [y, success, maxerror, per_block_status] = solve_block_decomposed_problem(options_, M_, oo_);
     end
 else
     if options_.bytecode
         try
-            oo_.endo_simul = bytecode('dynamic', oo_.endo_simul, oo_.exo_simul, M_.params, repmat(oo_.steady_state, 1, periods+2), periods);
-            oo_.deterministic_simulation.status = true;
+            y = bytecode('dynamic', oo_.endo_simul, oo_.exo_simul, M_.params, repmat(oo_.steady_state, 1, periods+2), periods);
+            success = true;
         catch ME
             disp(ME.message)
             if options_.no_homotopy
                 error('Error in bytecode')
             end
-            oo_.deterministic_simulation.status = false;
+            success = false;
         end
     else
         if M_.maximum_endo_lead == 0 && M_.maximum_endo_lag>0 && ~options_.lmmcp.status % Purely backward model
-            [oo_.endo_simul, oo_.deterministic_simulation] = ...
-                sim1_purely_backward(oo_.endo_simul, oo_.exo_simul, oo_.steady_state, M_, options_);
+            [y, success] = sim1_purely_backward(oo_.endo_simul, oo_.exo_simul, oo_.steady_state, M_, options_);
         elseif M_.maximum_endo_lag == 0 && M_.maximum_endo_lead>0 && ~options_.lmmcp.status % Purely forward model
-            [oo_.endo_simul, oo_.deterministic_simulation] = ...
-                sim1_purely_forward(oo_.endo_simul, oo_.exo_simul, oo_.steady_state, M_, options_);
+            [y, success] = sim1_purely_forward(oo_.endo_simul, oo_.exo_simul, oo_.steady_state, M_, options_);
         elseif M_.maximum_endo_lag == 0 && M_.maximum_endo_lead == 0 && ~options_.lmmcp.status % Purely static model
-            [oo_.endo_simul, oo_.deterministic_simulation] = ...
-                sim1_purely_static(oo_.endo_simul, oo_.exo_simul, oo_.steady_state, M_, options_);
+            [y, success] = sim1_purely_static(oo_.endo_simul, oo_.exo_simul, oo_.steady_state, M_, options_);
         else % General case
             switch options_.stack_solve_algo
               case 0
                 if options_.linear_approximation
-                    [oo_.endo_simul, oo_.deterministic_simulation] = ...
-                        sim1_linear(oo_.endo_simul, oo_.exo_simul, oo_.steady_state, oo_.exo_steady_state, M_, options_);
+                    [y, success, maxerror] = sim1_linear(oo_.endo_simul, oo_.exo_simul, oo_.steady_state, oo_.exo_steady_state, M_, options_);
                 else
-                    [oo_.endo_simul, oo_.deterministic_simulation] = ...
-                        sim1(oo_.endo_simul, oo_.exo_simul, oo_.steady_state, M_, options_);
+                    [y, success, maxerror, iter] = sim1(oo_.endo_simul, oo_.exo_simul, oo_.steady_state, M_, options_);
                 end
               case {1 6}
                 if options_.linear_approximation
                     error('Invalid value of stack_solve_algo option!')
                 end
-                [oo_.endo_simul, oo_.deterministic_simulation] = ...
-                    sim1_lbj(oo_.endo_simul, oo_.exo_simul, oo_.steady_state, M_, options_);
+                [y, success, maxerror, iter] = sim1_lbj(oo_.endo_simul, oo_.exo_simul, oo_.steady_state, M_, options_);
               case 7
                 if options_.linear_approximation
                     if isequal(options_.solve_algo, 10) 
@@ -116,11 +117,9 @@ else
                             warning('It would be more efficient to set option solve_algo equal to 0!')
                         end
                     end
-                    [oo_.endo_simul, oo_.deterministic_simulation] = ...
-                        solve_stacked_linear_problem(oo_.endo_simul, oo_.exo_simul, oo_.steady_state, oo_.exo_steady_state, M_, options_);
+                    [y, success] = solve_stacked_linear_problem(oo_.endo_simul, oo_.exo_simul, oo_.steady_state, oo_.exo_steady_state, M_, options_);
                 else
-                    [oo_.endo_simul, oo_.deterministic_simulation, residuals] = ...
-                        solve_stacked_problem(oo_.endo_simul, oo_.exo_simul, oo_.steady_state, M_, options_);
+                    [y, success, maxerror] = solve_stacked_problem(oo_.endo_simul, oo_.exo_simul, oo_.steady_state, M_, options_);
                 end
               otherwise
                 error('Invalid value of stack_solve_algo option!')
@@ -129,29 +128,21 @@ else
     end
 end
 
-if nargout>1
-    if options_.lmmcp.status
-        maxerror = max(max(abs(residuals)));
-    elseif options_.block && ~options_.bytecode
-        maxerror = oo_.deterministic_simulation.error;
+% Some solvers do not compute the maximum error, so do it here if needed
+if nargout > 2 && isempty(maxerror)
+    ny = size(oo_.endo_simul, 1);
+    if M_.maximum_lag > 0
+        y0 = y(:, M_.maximum_lag);
     else
-        if options_.bytecode
-            residuals = bytecode('dynamic','evaluate', oo_.endo_simul, oo_.exo_simul, M_.params, oo_.steady_state, 1);
-        else
-            if M_.maximum_lag > 0
-                y0 = oo_.endo_simul(:, M_.maximum_lag);
-            else
-                y0 = NaN(ny, 1);
-            end
-            if M_.maximum_lead > 0
-                yT = oo_.endo_simul(:, M_.maximum_lag+periods+1);
-            else
-                yT = NaN(ny, 1);
-            end
-            yy = oo_.endo_simul(:,M_.maximum_lag+(1:periods));
-
-            residuals = perfect_foresight_problem(yy(:), y0, yT, oo_.exo_simul, M_.params, oo_.steady_state, periods, M_, options_);
-        end
-        maxerror = max(max(abs(residuals)));
+        y0 = NaN(ny, 1);
     end
+    if M_.maximum_lead > 0
+        yT = y(:, M_.maximum_lag+periods+1);
+    else
+        yT = NaN(ny, 1);
+    end
+    yy = y(:,M_.maximum_lag+(1:periods));
+
+    residuals = perfect_foresight_problem(yy(:), y0, yT, oo_.exo_simul, M_.params, oo_.steady_state, periods, M_, options_);
+    maxerror = max(max(abs(residuals)));
 end

matlab/ep/extended_path_core.m
@@ -4,7 +4,7 @@ function [y, info_convergence, endogenousvariablespaths] = extended_path_core(pe
                                                   debug,order,M,pfm,algo,solve_algo,stack_solve_algo,...
                                                   olmmcp,options,oo,initialguess)
 
-% Copyright © 2016-2020 Dynare Team
+% Copyright © 2016-2023 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -56,32 +56,25 @@ if order == 0
     options.lmmcp = olmmcp;
     options.solve_algo = solve_algo;
     options.stack_solve_algo = stack_solve_algo;
-    tmp = perfect_foresight_solver_core(M, options, oo);
-    if ~tmp.deterministic_simulation.status
-        info_convergence = false;
-    else
-        info_convergence = true;
-    end
+    [endogenousvariablespaths, info_convergence] = perfect_foresight_solver_core(M, options, oo);
 else
     switch(algo)
         case 0
-            [flag, tmp.endo_simul] = ...
+            [flag, endogenousvariablespaths] = ...
             solve_stochastic_perfect_foresight_model(endo_simul, exo_simul, pfm, ep.stochastic.quadrature.nodes, ep.stochastic.order);
         case 1
-            [flag, tmp.endo_simul] = ...
+            [flag, endogenousvariablespaths] = ...
             solve_stochastic_perfect_foresight_model_1(endo_simul, exo_simul, options, pfm, ep.stochastic.order);
     end
     info_convergence = ~flag;
 end
 
 if ~info_convergence && ~options.no_homotopy
-    [info_convergence, tmp.endo_simul] = extended_path_homotopy(endo_simul, exo_simul, M, options, oo, pfm, ep, order, algo, 2, debug);
+    [info_convergence, endogenousvariablespaths] = extended_path_homotopy(endo_simul, exo_simul, M, options, oo, pfm, ep, order, algo, 2, debug);
 end
 
 if info_convergence
-    y = tmp.endo_simul(:,2);
+    y = endogenousvariablespaths(:,2);
 else
     y = NaN(size(endo_nbr,1));
 end
-
-endogenousvariablespaths = tmp.endo_simul;

matlab/ep/extended_path_homotopy.m
@@ -35,20 +35,20 @@ if ismember(method, [1, 2])
         oo.endo_simul(:,1) = oo.steady_state + weight*(endo_simul0(:,1) - oo.steady_state);
         oo.exo_simul = bsxfun(@plus, weight*exo_simul, (1-weight)*transpose(oo.exo_steady_state));
         if order==0
-            tmp = perfect_foresight_solver_core(M, options, oo);
+            [endo_simul_new, success] = perfect_foresight_solver_core(M, options, oo);
         else
             switch(algo)
               case 0
-                [flag, tmp.endo_simul] = ...
+                [flag, endo_simul_new] = ...
                     solve_stochastic_perfect_foresight_model(endo_simul, exo_simul, pfm, ep.stochastic.quadrature.nodes, ep.stochastic.order);
               case 1
-                [flag, tmp.endo_simul] = ...
+                [flag, endo_simul_new] = ...
                     solve_stochastic_perfect_foresight_model_1(endo_simul, exo_simul, options, pfm, ep.stochastic.order);
             end
         end
         if isequal(order, 0)
             % Logical variable flag is false iff the solver fails.
-            flag = tmp.deterministic_simulation.status;
+            flag = success;
         else
             % Fix convention issue on the value of flag.
             flag = ~flag;
@@ -70,7 +70,7 @@ if ismember(method, [1, 2])
             oldweight = weight;
             weight = min(weight*increase_factor, 1);
             increase_flag = true;
-            endo_simul = tmp.endo_simul;
+            endo_simul = endo_simul_new;
         else
             if increase_flag
                 weight = oldweight + (weight-oldweight)/100;
@@ -102,20 +102,20 @@ if isequal(method, 3) || (isequal(method, 2) && noconvergence)
         oo.endo_simul = endo_simul;
         oo.exo_simul = bsxfun(@plus, weight*exo_simul, (1-weight)*transpose(oo.exo_steady_state));
         if order==0
-            tmp = perfect_foresight_solver_core(M, options, oo);
+            [endo_simul_new, success] = perfect_foresight_solver_core(M, options, oo);
         else
             switch(algo)
               case 0
-                [flag, tmp.endo_simul] = ...
+                [flag, endo_simul_new] = ...
                     solve_stochastic_perfect_foresight_model(endo_simul, exo_simul, pfm, ep.stochastic.quadrature.nodes, ep.stochastic.order);
               case 1
-                [flag, tmp.endo_simul] = ...
+                [flag, endo_simul_new] = ...
                     solve_stochastic_perfect_foresight_model_1(endo_simul, exo_simul, options, pfm, ep.stochastic.order);
             end
         end
         if isequal(order, 0)
             % Logical variable flag is false iff the solver fails.
-            flag = tmp.deterministic_simulation.status;
+            flag = success;
         else
             % Fix convention issue on the value of flag.
             flag = ~flag;
@@ -130,7 +130,7 @@ if isequal(method, 3) || (isequal(method, 2) && noconvergence)
                 continue
             end
             index = index+1;
-            endo_simul = tmp.endo_simul;
+            endo_simul = endo_simul_new;
         else
             break
         end

matlab/evaluate_steady_state.m
@@ -370,11 +370,11 @@ elseif ~options.bytecode && options.block
                 end
             else
                 nze = length(M.block_structure_stat.block(b).g1_sparse_rowval);
-                [ys, T, ~, info2] = solve_one_boundary(fh_static, ys, exo_ss, ...
-                                                       params, [], T, mfs_idx, nze, 1, false, b, 0, options.simul.maxit, ...
-                                                       options.solve_tolf, ...
-                                                       0, options.solve_algo, true, false, false, M, options, []);
-                if info2
+                [ys, T, success] = solve_one_boundary(fh_static, ys, exo_ss, ...
+                                                      params, [], T, mfs_idx, nze, 1, false, b, 0, options.simul.maxit, ...
+                                                      options.solve_tolf, ...
+                                                      0, options.solve_algo, true, false, false, M, options);
+                if ~success
                     check = 1;
                     break
                 end

matlab/perfect-foresight-models/perfect_foresight_solver.m
@@ -54,10 +54,10 @@ end
 initperiods = 1:M_.maximum_lag;
 lastperiods = (M_.maximum_lag+periods+1):(M_.maximum_lag+periods+M_.maximum_lead);
 
-oo_ = perfect_foresight_solver_core(M_,options_,oo_);
+[oo_.endo_simul, success, maxerror, solver_iter, per_block_status] = perfect_foresight_solver_core(M_,options_,oo_);
 
 % If simulation failed try homotopy.
-if ~oo_.deterministic_simulation.status && ~options_.no_homotopy
+if ~success && ~options_.no_homotopy
 
     if ~options_.noprint
         fprintf('\nSimulation of the perfect foresight model failed!\n')
@@ -131,13 +131,13 @@ if ~oo_.deterministic_simulation.status && ~options_.no_homotopy
         saved_endo_simul = oo_.endo_simul;
 
         % Solve for the paths of the endogenous variables.
-        [oo_,me] = perfect_foresight_solver_core(M_,options_,oo_);
+        [oo_.endo_simul, success, maxerror, solver_iter, per_block_status] = perfect_foresight_solver_core(M_,options_,oo_);
 
-        if oo_.deterministic_simulation.status
+        if success
             current_weight = new_weight;
             if current_weight >= 1
                 if ~options_.noprint
-                    fprintf('%i \t | %1.5f \t | %s \t | %e\n', iteration, new_weight, 'succeeded', me)
+                    fprintf('%i \t | %1.5f \t | %s \t | %e\n', iteration, new_weight, 'succeeded', maxerror)
                 end
                 break
             end
@@ -147,7 +147,7 @@ if ~oo_.deterministic_simulation.status && ~options_.no_homotopy
                 step = step * 2;
             end
             if ~options_.noprint
-                fprintf('%i \t | %1.5f \t | %s \t | %e\n', iteration, new_weight, 'succeeded', me)
+                fprintf('%i \t | %1.5f \t | %s \t | %e\n', iteration, new_weight, 'succeeded', maxerror)
             end
         else
             % If solver failed, then go back.
@@ -155,8 +155,8 @@ if ~oo_.deterministic_simulation.status && ~options_.no_homotopy
             success_counter = 0;
             step = step / 2;
             if ~options_.noprint
-                if isreal(me)
-                    fprintf('%i \t | %1.5f \t | %s \t | %e\n', iteration, new_weight, 'failed', me)
+                if isreal(maxerror)
+                    fprintf('%i \t | %1.5f \t | %s \t | %e\n', iteration, new_weight, 'failed', maxerror)
                 else
                     fprintf('%i \t | %1.5f \t | %s \t | %s\n', iteration, new_weight, 'failed', 'Complex')
                 end
@@ -186,21 +186,32 @@ if ~isreal(oo_.endo_simul(:)) % cannot happen with bytecode or the perfect_fores
     yy = real(oo_.endo_simul(:,M_.maximum_lag+(1:periods)));
     residuals = perfect_foresight_problem(yy(:), y0, yT, oo_.exo_simul, M_.params, oo_.steady_state, periods, M_, options_);
 
-    if max(abs(residuals))< options_.dynatol.f
-        oo_.deterministic_simulation.status = true;
+    maxerror = max(max(abs(residuals)))
+    if maxerror < options_.dynatol.f
+        success = true;
         oo_.endo_simul=real(oo_.endo_simul);
     else
-        oo_.deterministic_simulation.status = false;
+        success = false;
         disp('Simulation terminated with imaginary parts in the residuals or endogenous variables.')
     end
 end
 
-if oo_.deterministic_simulation.status
+if success
     if ~options_.noprint
         fprintf('Perfect foresight solution found.\n\n')
     end
 else
     fprintf('Failed to solve perfect foresight model\n\n')
+end
+
+% Put solver status information in oo_
+oo_.deterministic_simulation.status = success;
+oo_.deterministic_simulation.error = maxerror;
+if ~isempty(solver_iter)
+    oo_.deterministic_simulation.iterations = solver_iter;
+end
+if ~isempty(per_block_status)
+    oo_.deterministic_simulation.block = per_block_status;
 end
 
 dyn2vec(M_, oo_, options_);
@@ -222,6 +233,6 @@ end
 
 assignin('base', 'Simulated_time_series', ts);
 
-if oo_.deterministic_simulation.status
+if success
     oo_.gui.ran_perfect_foresight = true;
 end

matlab/perfect-foresight-models/sim1.m
@@ -1,4 +1,4 @@
-function [endogenousvariables, info] = sim1(endogenousvariables, exogenousvariables, steadystate, M, options)
+function [endogenousvariables, success, err, iter] = sim1(endogenousvariables, exogenousvariables, steadystate, M, options)
 % Performs deterministic simulations with lead or lag of one period, using
 % a basic Newton solver on sparse matrices.
 % Uses perfect_foresight_problem DLL to construct the stacked problem.
@@ -11,9 +11,11 @@ function [endogenousvariables, info] = sim1(endogenousvariables, exogenousvariab
 %   - options             [struct] contains various options.
 % OUTPUTS
 %   - endogenousvariables [double] N*(T+M.maximum_lag+M.maximum_lead) array, paths for the endogenous variables (solution of the perfect foresight model).
-%   - info                [struct] contains informations about the results.
+%   - success             [logical] Whether a solution was found
+%   - err                 [double] ∞-norm of the residual
+%   - iter                [integer] Number of iterations
 
-% Copyright © 1996-2021 Dynare Team
+% Copyright © 1996-2023 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -145,10 +147,7 @@ if stop
     % initial or terminal observations may contain
     % harmless NaN or Inf. We test only values computed above
     if any(any(isnan(y))) || any(any(isinf(y)))
-        info.status = false;% NaN or Inf occurred
-        info.error = err;
-        info.iterations = iter;
-        info.periods = vperiods(1:iter);
+        success = false; % NaN or Inf occurred
         if verbose
             skipline()
             fprintf('Total time of simulation: %g.\n', etime(clock,h1))
@@ -163,10 +162,7 @@ if stop
             fprintf('Total time of simulation: %g.\n', etime(clock,h1))
             printline(56)
         end
-        info.status = true;% Convergency obtained.
-        info.error = err;
-        info.iterations = iter;
-        info.periods = vperiods(1:iter);
+        success = true; % Convergency obtained.
     end
 elseif ~stop
     if verbose
@@ -175,10 +171,7 @@ elseif ~stop
         disp('Maximum number of iterations is reached (modify option maxit).')
         printline(62)
     end
-    info.status = false;% more iterations are needed.
-    info.error = err;
-    info.periods = vperiods(1:iter);
-    info.iterations = options.simul.maxit;
+    success = false; % more iterations are needed.
 end
 
 if verbose

matlab/perfect-foresight-models/sim1_lbj.m
@@ -1,4 +1,4 @@
-function [endogenousvariables, info] = sim1_lbj(endogenousvariables, exogenousvariables, steadystate, M, options)
+function [endogenousvariables, success, err, iter] = sim1_lbj(endogenousvariables, exogenousvariables, steadystate, M, options)
 
 % Performs deterministic simulations with lead or lag on one period using the historical LBJ algorithm
 %
@@ -94,9 +94,7 @@ for iter = 1:options.simul.maxit
             skipline()
             disp(sprintf('Total time of simulation: %s', num2str(etime(clock,h1))))
         end
-        info.status = 1;% Convergency obtained.
-        info.error = err;
-        info.iterations = iter;
+        success = true; % Convergency obtained.
         break
     end
 end
@@ -106,10 +104,7 @@ if ~stop
         disp(sprintf('Total time of simulation: %s.', num2str(etime(clock,h1))))
         disp('Maximum number of iterations is reached (modify option maxit).')
     end
-    info.status = 0;% more iterations are needed.
-    info.error = err;
-    info.errors = c/abs(err);
-    info.iterations = options.simul.maxit;
+    success = false; % More iterations are needed.
 end
 
 if verbose

matlab/perfect-foresight-models/sim1_linear.m
@@ -1,4 +1,4 @@
-function [endogenousvariables, info] = sim1_linear(endogenousvariables, exogenousvariables, steadystate_y, steadystate_x, M, options)
+function [endogenousvariables, success, ERR] = sim1_linear(endogenousvariables, exogenousvariables, steadystate_y, steadystate_x, M, options)
 
 % Solves a linear approximation of a perfect foresight model using sparse matrix.
 %
@@ -12,7 +12,8 @@ function [endogenousvariables, info] = sim1_linear(endogenousvariables, exogenou
 %
 % OUTPUTS
 % - endogenousvariables [double] N*T array, paths for the endogenous variables (solution of the perfect foresight model).
-% - info                [struct] contains informations about the results.
+% - success             [logical] Whether a solution was found
+% - ERR                 [double] ∞-norm of the residual
 %
 % NOTATIONS
 % - N is the number of endogenous variables.
@@ -38,7 +39,7 @@ function [endogenousvariables, info] = sim1_linear(endogenousvariables, exogenou
 % to center the variables around the deterministic steady state to solve the
 % perfect foresight model.
 
-% Copyright © 2015-2020 Dynare Team
+% Copyright © 2015-2023 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -209,9 +210,8 @@ try
 catch
     % Normally, because the model is linear, the solution of the perfect foresight model should
     % be obtained in one Newton step. This is not the case if the model is singular.
-    info.status = false;
-    info.error = NaN;
-    info.iterations = 1;
+    success = false;
+    ERR = [];
     if verbose
         skipline()
         disp('Singularity problem! The jacobian matrix of the stacked model cannot be inverted.')
@@ -238,9 +238,7 @@ if verbose
 end
 
 if any(isnan(res)) || any(isinf(res)) || any(isnan(Y)) || any(isinf(Y)) || ~isreal(res) || ~isreal(Y)
-    info.status = false;% NaN or Inf occurred
-    info.error = ERR;
-    info.iterations = 1;
+    success = false; % NaN or Inf occurred
     endogenousvariables = reshape(Y, ny, periods+maximum_lag+M.maximum_lead);
     if verbose
         skipline()
@@ -252,9 +250,7 @@ if any(isnan(res)) || any(isinf(res)) || any(isnan(Y)) || any(isinf(Y)) || ~isre
         disp('There is most likely something wrong with your model. Try model_diagnostics or another simulation method.')
     end
 else
-    info.status = true;% Convergency obtained.
-    info.error = ERR;
-    info.iterations = 1;
+    success = true; % Convergency obtained.
     endogenousvariables = bsxfun(@plus, reshape(Y, ny, periods+maximum_lag+M.maximum_lead), steadystate_y);
 end
 

commit d81e3992c3ca8987d5f10756d887fad4ecc31cde

perfect_foresight_solver_core: fix crash when bytecode does not converge
The bug was introduced in commit 7722e8e36b1318584dd5fb44846d5617e7278dda

matlab/perfect-foresight-models/perfect_foresight_solver_core.m
@@ -69,6 +69,7 @@ if options_.block
             if options_.no_homotopy
                 error('Error in bytecode')
             end
+            y = oo_.endo_simul; % Set something for y, need for computing maxerror
             success = false;
         end
     else
@@ -84,6 +85,7 @@ else
             if options_.no_homotopy
                 error('Error in bytecode')
             end
+            y = oo_.endo_simul; % Set something for y, need for computing maxerror
             success = false;
         end
     else