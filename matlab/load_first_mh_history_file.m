d2fecdc5d32d1dee1c1d9acd446dd2c311558fc2
Johannes Pfeifer <jpfeifer@gmx.de>
2023-07-18 10:01:47 -0400

    load_*_mh_history_file: remove assignin statements

--- matlab/load_first_mh_history_file.m
+++ matlab/load_first_mh_history_file.m
@@ -1,8 +1,8 @@
-function info = load_first_mh_history_file(MetropolisFolder, ModelName)
+function record = load_first_mh_history_file(MetropolisFolder, ModelName)
 
 % This routine requires that the MCMC draws were obtained with a dynare version greater than 4.3.3.
 
-% Copyright © 2013-2017 Dynare Team
+% Copyright © 2013-2023 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -19,9 +19,6 @@ function info = load_first_mh_history_file(MetropolisFolder, ModelName)
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <https://www.gnu.org/licenses/>.
 
-% record is also a Matlab function.
-record = 0;
-
 % Get the list of all the mh_history files.
 BaseName = [MetropolisFolder filesep ModelName];
 mh_history_files = dir([BaseName '_mh_history_*.mat']);
@@ -30,10 +27,4 @@ if isequal(length(mh_history_files),0)
     error(['Estimation::load_mh_file: I cannot find any mh-history file in ' MetropolisFolder '!'])
 end
 
-load([BaseName '_mh_history_0.mat']);
-
-if isequal(nargout,0)
-    assignin('caller', 'record', record);
-else
-    info = record;
-end
+load([BaseName '_mh_history_0.mat'],'record');
