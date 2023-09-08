2b240210d0e01f39bc7d8bfb115d693aa1cfb301
Johannes Pfeifer <jpfeifer@gmx.de>
2023-09-08 11:55:15 +0200

    OSR: Remove globals and wrapper layer, move to +subfolder

--- matlab/+osr/get_variance_of_endogenous_variables.m
+++ matlab/+osr/get_variance_of_endogenous_variables.m
@@ -1,19 +1,21 @@
-function vx1 = get_variance_of_endogenous_variables(dr,i_var)
-
-% function vx1 = get_variance_of_endogenous_variables(dr,i_var)
+function vx1 = get_variance_of_endogenous_variables(M_,options_,dr,i_var)
+% vx1 = get_variance_of_endogenous_variables(dr,i_var)
 % Gets the variance of a variables subset
 %
 % INPUTS
-%    dr:        structure of decisions rules for stochastic simulations
-%    i_var:     indices of a variables list
+%   M_                        [structure]       Dynare's model structure
+%   oo_                       [structure]       Dynare's results structure
+%   options_                  [structure]       Dynare's options structure
+%   dr:                       [structure]       structure of decisions rules for stochastic simulations
+%   i_var:                    [integer]         indices of a variables list
 %
 % OUTPUTS
-%    vx1:       variance-covariance matrix
+%    vx1:                     [double]          variance-covariance matrix
 %
 % SPECIAL REQUIREMENTS
 %    none
 
-% Copyright © 2003-2017 Dynare Team
+% Copyright © 2003-2023 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -30,22 +32,14 @@ function vx1 = get_variance_of_endogenous_variables(dr,i_var)
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <https://www.gnu.org/licenses/>.
 
-global M_ options_
-
-endo_nbr = M_.endo_nbr;
-
-Sigma_e = M_.Sigma_e;
-
-nstatic = M_.nstatic;
-nspred = M_.nspred;
 ghx = dr.ghx(i_var,:);
 ghu = dr.ghu(i_var,:);
 nc = size(ghx,2);
 n = length(i_var);
 
-[A,B] = kalman_transition_matrix(dr,nstatic+(1:nspred),1:nc);
+[A,B] = kalman_transition_matrix(dr,M_.nstatic+(1:M_.nspred),1:nc);
 
-[vx,u] = lyapunov_symm(A,B*Sigma_e*B',options_.lyapunov_fixed_point_tol,options_.qz_criterium,options_.lyapunov_complex_threshold, [], options_.debug);
+[vx,u] = lyapunov_symm(A,B*M_.Sigma_e*B',options_.lyapunov_fixed_point_tol,options_.qz_criterium,options_.lyapunov_complex_threshold, [], options_.debug);
 
 if size(u,2) > 0
     i_stat = find(any(abs(ghx*u) < options_.schur_vec_tol,2)); %only set those variances of objective function for which variance is finite
@@ -56,4 +50,4 @@ else
 end
 
 vx1 = Inf*ones(n,n);
-vx1(i_stat,i_stat) = ghx*vx*ghx'+ghu*Sigma_e*ghu';
+vx1(i_stat,i_stat) = ghx*vx*ghx'+ghu*M_.Sigma_e*ghu';
