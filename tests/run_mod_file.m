8a798991895918a24ed829e9b00e815f4c3048d1
Sébastien Villemot <sebastien@dynare.org>
2023-09-12 17:10:17 +0200

    Meson build system: implement most of the testsuite
    The tests that are individually listed in {M,O}_TRS_FILES of tests/Makefile.am
    are still missing.

---
+++ tests/run_mod_file.m
@@ -0,0 +1,47 @@
+% Copyright © 2011-2023 Dynare Team
+%
+% This file is part of Dynare.
+%
+% Dynare is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Dynare is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Dynare.  If not, see <https://www.gnu.org/licenses/>.
+
+source_dir = getenv('source_root');
+addpath([source_dir filesep 'tests' filesep 'utils']);
+addpath([source_dir filesep 'matlab']);
+
+if isoctave
+    load_octave_packages
+end
+
+fprintf('\n*** TESTING: %s ***\n\n', getenv('mod_file'));
+
+tic;
+
+% NB: all variables will be cleared by the call to Dynare
+try
+    dynare(getenv('mod_file'), 'console')
+    testFailed = false;
+catch exception
+    if isoctave
+        printMakeCheckOctaveErrMsg(getenv('mod_file'), exception);
+    else
+        printMakeCheckMatlabErrMsg(getenv('mod_file'), exception);
+    end
+  testFailed = true;
+end
+
+
+fprintf('\n*** Elapsed time (in seconds): %.1f\n\n', toc);
+
+% Ensure proper termination and exit code
+quit(testFailed)
