gboehl/dynare

commit 4598dba40c959509b3157e258ef33b87cad1f41f

display_static_residuals.m: remove globals

matlab/display_static_residuals.m
@@ -1,10 +1,13 @@
-function z = display_static_residuals(options_resid_)
-% function z = display_static_residuals(options_resid_)
+function z = display_static_residuals(M_, options_, oo_,options_resid_)
+% function z = display_static_residuals(M_, options_, oo_,options_resid_)
 %
 % Computes static residuals associated with the guess values.
 %
 % INPUTS
-%    options_resid_:   options to resid
+%   M:              [structure] storing the model information
+%   options:        [structure] storing the options
+%   oo:             [structure] storing the results
+%   options_resid_:             options to resid
 %
 % OUTPUTS
 %    z:      residuals
@@ -29,11 +32,7 @@ function z = display_static_residuals(options_resid_)
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <https://www.gnu.org/licenses/>.
 
-global M_ options_ oo_
-
-% Properly handle the case where no input argument is given, e.g. when
-% writing “z = resid;” in a .mod file (hence not using the preprocessor syntax).
-non_zero = nargin > 0 && isfield(options_resid_, 'non_zero') && options_resid_.non_zero;
+non_zero = (isfield(options_resid_, 'non_zero') && options_resid_.non_zero);
 
 tags  = M_.equations_tags;
 istag = 0;
@@ -41,15 +40,6 @@ if ~isempty(tags)
     istag = 1;
 end
 
-steady_state_old = oo_.steady_state;
-
-% Keep of a copy of M_.Sigma_e
-Sigma_e = M_.Sigma_e;
-
-% Set M_.Sigma_e=0 (we evaluate the *deterministic* static model)
-M_.Sigma_e = zeros(size(Sigma_e));
-
-info = 0;
 if any(imag(oo_.steady_state))
     imagrow=find(imag(oo_.steady_state));
     if ~isempty(imagrow)
@@ -83,8 +73,6 @@ else
         disp_string='';
     end
 end
-M_.Sigma_e = Sigma_e;
-
 
 % Display the non-zero residuals if no return value
 if nargout == 0
@@ -129,5 +117,3 @@ if nargout == 0
     end
     skipline(2)
 end
-
-oo_.steady_state = steady_state_old;

commit 838bb2f4b5913f3202206673800513594eda8edc

display_static_residuals.m: fix bug introduced in 4598dba40c959509b3157e258ef33b87cad1f41f

matlab/display_static_residuals.m
@@ -32,7 +32,7 @@ function z = display_static_residuals(M_, options_, oo_,options_resid_)
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <https://www.gnu.org/licenses/>.
 
-non_zero = (isfield(options_resid_, 'non_zero') && options_resid_.non_zero);
+non_zero = (nargin > 3 && isfield(options_resid_, 'non_zero') && options_resid_.non_zero);
 
 tags  = M_.equations_tags;
 istag = 0;

matlab/steady.m
@@ -86,7 +86,7 @@ if info(1) == 0
 else
     if ~options_.noprint
         if ~isempty(oo_.steady_state)
-            display_static_residuals;
+            display_static_residuals(M_, options_, oo_);
         else
             skipline()
             disp('Residuals of the static equations cannot be computed because the steady state routine returned an empty vector.')