7cfe69995b7186b6649624a3052a0597ce67d55f
Sébastien Villemot <sebastien@dynare.org>
2023-10-10 17:32:33 -0400

    Bytecode: do not restrict print_bytecode_{static,dynamic}_model to certain values of {stack_,}solve_algo
    (manually cherry picked from commit 9525dbe3a88de10878755bef642774b6a77c6d7a)

--- mex/sources/bytecode/bytecode.cc
+++ mex/sources/bytecode/bytecode.cc
@@ -683,10 +683,10 @@ mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     mexWarnMsgTxt("Not enough space. Filename is truncated.");
   string file_name = fname;
 
-  if (stack_solve_algo == 7 && !steady_state)
+  if (stack_solve_algo == 7 && !steady_state && !print)
     mexErrMsgTxt("Bytecode: Can't use option stack_solve_algo=7\n");
 
-  if (steady_state && !evaluate && (solve_algo < 5 || solve_algo > 8))
+  if (steady_state && !evaluate && !print && (solve_algo < 5 || solve_algo > 8))
     mexErrMsgTxt("Bytecode: solve_algo must be between 5 and 8 when using the internal steady state solver\n");
 
   size_t size_of_direction = col_y*row_y*sizeof(double);
