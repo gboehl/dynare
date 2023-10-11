gboehl/dynare

commit f84753025d7608e0515fe9d44b7e68814ed9a335

Bytecode: the block and non-block versions now coexist in parallel
As a consequence, a new “block_decomposed” option of the bytecode MEX has been
introduced to explicitly select the block-decomposed version.

Note that we do not always use the “block_decomposed” option even when the
“block” option has been passed to the user, in situations where the block
decomposition brings nothing (e.g. when evaluating the residuals of the whole
model).

matlab/perfect-foresight-models/det_cond_forecast.m
@@ -12,7 +12,7 @@ function data_set = det_cond_forecast(varargin)
 %  dataset                [dseries]     Returns a dseries containing the forecasted endgenous variables and shocks
 %
 
-% Copyright © 2013-2020 Dynare Team
+% Copyright © 2013-2023 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -161,7 +161,11 @@ else
             if options_.bytecode
                 save_options_dynatol_f = options_.dynatol.f;
                 options_.dynatol.f = 1e-7;
-                [endo, exo] = bytecode('extended_path', plan, oo_.endo_simul, oo_.exo_simul, M_.params, oo_.steady_state, options_.periods);
+                if options_.block
+                    [endo, exo] = bytecode('extended_path', 'block_decomposed', plan, oo_.endo_simul, oo_.exo_simul, M_.params, oo_.steady_state, options_.periods);
+                else
+                    [endo, exo] = bytecode('extended_path', plan, oo_.endo_simul, oo_.exo_simul, M_.params, oo_.steady_state, options_.periods);
+                end
                 options_.dynatol.f = save_options_dynatol_f;
 
                 oo_.endo_simul = endo;
@@ -466,16 +470,13 @@ if pf && ~surprise
         indx_x = [];
         for k = 1 : per
             if k == 1
-                if (isfield(M_,'block_structure'))
-                    data1 = M_.block_structure.block;
-                    Size = length(M_.block_structure.block);
-                else
-                    data1 = M_;
-                    Size = 1;
-                end
                 data1 = M_;
                 if (options_.bytecode)
-                    [zz, data1]= bytecode('dynamic','evaluate', z, zx, M_.params, oo_.steady_state, k, data1);
+                    if options_.block
+                        [zz, data1]= bytecode('dynamic','block_decomposed','evaluate', z, zx, M_.params, oo_.steady_state, k, data1);
+                    else
+                        [zz, data1]= bytecode('dynamic','evaluate', z, zx, M_.params, oo_.steady_state, k, data1);
+                    end
                 else
                     [zz, g1b] = feval([M_.fname '.dynamic'], z', zx, M_.params, oo_.steady_state, k);
                     data1.g1_x = g1b(:,end - M_.exo_nbr + 1:end);
@@ -736,16 +737,13 @@ else
             indx_x = [];
             for k = 1 : per
                 if k == 1
-                    if (isfield(M_,'block_structure'))
-                        data1 = M_.block_structure.block;
-                        Size = length(M_.block_structure.block);
-                    else
-                        data1 = M_;
-                        Size = 1;
-                    end
                     data1 = M_;
                     if (options_.bytecode)
-                        [zz, data1]= bytecode('dynamic','evaluate', z, zx, M_.params, oo_.steady_state, k, data1);
+                        if options_.block
+                            [zz, data1]= bytecode('dynamic','block_decomposed','evaluate', z, zx, M_.params, oo_.steady_state, k, data1);
+                        else
+                            [zz, data1]= bytecode('dynamic','evaluate', z, zx, M_.params, oo_.steady_state, k, data1);
+                        end
                     else
                         [zz, g1b] = feval([M_.fname '.dynamic'], z', zx, M_.params, oo_.steady_state, k);
                         data1.g1_x = g1b(:,end - M_.exo_nbr + 1:end);

matlab/block_bytecode_mfs_steadystate.m
@@ -2,7 +2,7 @@ function [r, g1] = block_bytecode_mfs_steadystate(y, b, y_all, exo, params, T, M
 % Wrapper around the *_static.m file, for use with dynare_solve,
 % when block_mfs option is given to steady.
 
-% Copyright © 2009-2022 Dynare Team
+% Copyright © 2009-2023 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -21,4 +21,4 @@ function [r, g1] = block_bytecode_mfs_steadystate(y, b, y_all, exo, params, T, M
 
 indx = M.block_structure_stat.block(b).variable;
 y_all(indx) = y;
-[r, g1] = bytecode(y_all, exo, params, y_all, 1, y_all, T, 'evaluate', 'static', ['block = ' int2str(b) ]);
+[r, g1] = bytecode(y_all, exo, params, y_all, 1, y_all, T, 'evaluate', 'static', 'block_decomposed', ['block = ' int2str(b) ]);

matlab/dr_block.m
@@ -36,7 +36,7 @@ function [dr,info,M_,oo_] = dr_block(dr,task,M_,options_,oo_,varargin)
 %   none.
 %
 
-% Copyright © 2010-2022 Dynare Team
+% Copyright © 2010-2023 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -79,7 +79,7 @@ end
 data = M_.block_structure.block;
 
 if options_.bytecode
-    [~, data]= bytecode('dynamic','evaluate', z, zx, M_.params, dr.ys, 1, data);
+    [~, data]= bytecode('dynamic', 'evaluate', 'block_decomposed', z, zx, M_.params, dr.ys, 1, data);
 else
     T=NaN(M_.block_structure.dyn_tmp_nbr, 1);
     it_=M_.maximum_lag+1;

matlab/dynare_solve_block_or_bytecode.m
@@ -1,6 +1,6 @@
 function [x,info] = dynare_solve_block_or_bytecode(y, exo, params, options, M)
 
-% Copyright © 2010-2022 Dynare Team
+% Copyright © 2010-2023 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -54,7 +54,11 @@ if options.block && ~options.bytecode
 elseif options.bytecode
     if options.solve_algo >= 5 && options.solve_algo <= 8
         try
-            x = bytecode('static', x, exo, params);
+            if options.block
+                x = bytecode('static', 'block_decomposed', x, exo, params);
+            else
+                x = bytecode('static', x, exo, params);
+            end
         catch ME
             disp(ME.message);
             info = 1;
@@ -79,7 +83,7 @@ elseif options.bytecode
             % Also update the temporary terms vector (needed for the dynare_solve case)
             try
                 [~, ~, x, T] = bytecode(x, exo, params, x, 1, x, T, 'evaluate', 'static', ...
-                                        ['block = ' int2str(b)]);
+                                        'block_decomposed', ['block = ' int2str(b)]);
             catch ME
                 disp(ME.message);
                 info = 1;

matlab/model_diagnostics.m
@@ -16,7 +16,7 @@ function model_diagnostics(M,options,oo)
 %   none.
 %
 
-% Copyright © 1996-2020 Dynare Team
+% Copyright © 1996-2023 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -153,7 +153,7 @@ for b=1:nb
                                     'evaluate', 'static');
         else
             [res, jacob] = bytecode(dr.ys, exo, M.params, dr.ys, 1, exo, ...
-                                    'evaluate', 'static',['block=' ...
+                                    'evaluate', 'static', 'block_decomposed', ['block=' ...
                                 int2str(b)]);
         end
     else

matlab/perfect-foresight-models/perfect_foresight_solver_core.m
@@ -11,7 +11,7 @@ function [oo_, maxerror] = perfect_foresight_solver_core(M_, options_, oo_)
 % - oo_                 [struct] contains results
 % - maxerror            [double] contains the maximum absolute error
 
-% Copyright © 2015-2022 Dynare Team
+% Copyright © 2015-2023 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -55,7 +55,7 @@ if options_.block
     end
     if options_.bytecode
         try
-            oo_.endo_simul = bytecode('dynamic', oo_.endo_simul, oo_.exo_simul, M_.params, repmat(oo_.steady_state,1, periods+2), periods);
+            oo_.endo_simul = bytecode('dynamic', 'block_decomposed', oo_.endo_simul, oo_.exo_simul, M_.params, repmat(oo_.steady_state,1, periods+2), periods);
             oo_.deterministic_simulation.status = true;
         catch ME
             disp(ME.message)

matlab/print_bytecode_dynamic_model.m
@@ -11,7 +11,7 @@ function print_bytecode_dynamic_model()
 % SPECIAL REQUIREMENTS
 %   none
 
-% Copyright © 2001-2017 Dynare Team
+% Copyright © 2001-2023 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -29,7 +29,11 @@ function print_bytecode_dynamic_model()
 % along with Dynare.  If not, see <https://www.gnu.org/licenses/>.
 global options_
 if options_.bytecode
-    bytecode('print','dynamic');
+    if options_.block
+        bytecode('print','dynamic','block_decomposed');
+    else
+        bytecode('print','dynamic');
+    end
 else
     disp('You have to use bytecode option in model command to use print_bytecode_dynamic_model');
 end

matlab/print_bytecode_static_model.m
@@ -11,7 +11,7 @@ function print_bytecode_static_model()
 % SPECIAL REQUIREMENTS
 %   none
 
-% Copyright © 2001-2017 Dynare Team
+% Copyright © 2001-2023 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -29,7 +29,11 @@ function print_bytecode_static_model()
 % along with Dynare.  If not, see <https://www.gnu.org/licenses/>.
 global options_
 if options_.bytecode
-    bytecode('print','static');
+    if options_.block
+        bytecode('print','static','block_decomposed');
+    else
+        bytecode('print','static');
+    end
 else
     disp('You have to use bytecode option in model command to use print_bytecode_static_model');
 end

commit b59dc2cf1a97307b048272cd02e1076265d3650c

det_cond_forecast: fix bug with bytecode+block
The plan must immediately follow the “extended_path” string in the input
arguments of bytecode. Bug introduced in f84753025d7608e0515fe9d44b7e68814ed9a335.

matlab/perfect-foresight-models/det_cond_forecast.m
@@ -162,7 +162,7 @@ else
                 save_options_dynatol_f = options_.dynatol.f;
                 options_.dynatol.f = 1e-7;
                 if options_.block
-                    [endo, exo] = bytecode('extended_path', 'block_decomposed', plan, oo_.endo_simul, oo_.exo_simul, M_.params, oo_.steady_state, options_.periods);
+                    [endo, exo] = bytecode('extended_path', plan, 'block_decomposed', oo_.endo_simul, oo_.exo_simul, M_.params, oo_.steady_state, options_.periods);
                 else
                     [endo, exo] = bytecode('extended_path', plan, oo_.endo_simul, oo_.exo_simul, M_.params, oo_.steady_state, options_.periods);
                 end