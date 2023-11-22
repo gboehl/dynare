701afd2c7cafa7cc93ec1f52cebafe874cdd8360
Sébastien Villemot <sebastien@dynare.org>
2023-11-22 17:24:31 +0100

    Consolidate the routines for loading JSON files, now that jsondecode is always available

--- matlab/loadjson_.m
+++ matlab/loadjson_.m
@@ -1,15 +1,17 @@
 function o = loadjson_(jsonfilename)
 
-% Reads a json file using jsondecode builtin.
+% Reads a JSON file using jsondecode builtin.
+%
+% Returns the output in a format consistent with the output of the JSONlab
+% toolbox, that we used to rely on (jsondecode builtin was introduced in MATLAB
+% R2016b and in Octave 7). Now we no longer depend on JSONlab, but for
+% historical reasons we still use its format.
 %
 % INPUTS
 % - jsonfilename   [char]      1×n char array, name of the JSON file.
 %
 % OUTPUTS
 % - o              [struct]    content of the JSON file.
-%
-% REMARKS
-% jsondecode builtin was introduced in MATLAB R2016b and in Octave 7.
 
 % Copyright © 2020-2023 Dynare Team
 %
@@ -30,5 +32,19 @@ function o = loadjson_(jsonfilename)
 
 json = fileread(jsonfilename);
 
-o = jsondecode(json); clear('json');
-o = convertjsondecode(o);
+o = jsondecode(json);
+
+clear json
+
+fnames = fieldnames(o);
+
+for i=1:length(fnames)
+    tmp = o.(fnames{i});
+    if isstruct(tmp) && length(tmp)>1
+        TMP = cell(length(tmp), 1);
+        for j=1:length(tmp)
+            TMP{j} = tmp(j);
+        end
+        o.(fnames{i}) = TMP;
+    end
+end
