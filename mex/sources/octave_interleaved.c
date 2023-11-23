f21fda7dfac7db734f708b50c8b1a9547c42cb71
Sébastien Villemot <sebastien@dynare.org>
2023-11-23 18:14:13 +0100

    Build system: enable the interleaved API in MEX files for Octave ⩾ 8.4.0
    Unfortunately it is not possible to enable it for all supported Octave
    versions (which would have allowed us to remove the old codepath), because of
    this bug: https://savannah.gnu.org/bugs/?64687

---
+++ mex/sources/octave_interleaved.c
@@ -0,0 +1,4 @@
+/* This file must be compiled in any Octave MEX for which we want the
+   interleaved API. This is similar to what mkoctfile does when this API is
+   requested. */
+const int __mx_has_interleaved_complex__ = 1;
