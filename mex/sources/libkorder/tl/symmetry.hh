3ab9be71c81aed4728362a5867deb9fb8e3897a1
Sébastien Villemot <sebastien@dynare.org>
2024-01-05 18:08:54 +0100

    🐛 k-order MEX: equality operator on symiterator had wrong name

--- mex/sources/libkorder/tl/symmetry.hh
+++ mex/sources/libkorder/tl/symmetry.hh
@@ -1,6 +1,6 @@
 /*
  * Copyright © 2004 Ondra Kamenik
- * Copyright © 2019-2023 Dynare Team
+ * Copyright © 2019-2024 Dynare Team
  *
  * This file is part of Dynare.
  *
@@ -127,14 +127,14 @@ public:
     return run;
   }
   bool
-  operator=(const symiterator& it)
+  operator==(const symiterator& it)
   {
     return dim == it.dim && run == it.run;
   }
   bool
   operator!=(const symiterator& it)
   {
-    return !operator=(it);
+    return !operator==(it);
   }
 };
 
