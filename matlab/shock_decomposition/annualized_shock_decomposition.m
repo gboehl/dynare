gboehl/dynare

commit 735bd66d4dea2f244918687fa0fa57051df59673

Remove unused output arguments.

matlab/shock_decomposition/annualized_shock_decomposition.m
@@ -129,7 +129,7 @@ if realtime_==0
         myopts=options_;
         myopts.plot_shock_decomp.type='qoq';
         myopts.plot_shock_decomp.realtime=0;
-        [z, ~] = plot_shock_decomposition(M_,oo_,myopts,[]);
+        z = plot_shock_decomposition(M_,oo_,myopts,[]);
     else
         z = oo_;
     end
@@ -158,7 +158,7 @@ if realtime_ && isstruct(oo_) && isfield(oo_, 'realtime_shock_decomposition')
         myopts.plot_shock_decomp.realtime=1;
         myopts.plot_shock_decomp.vintage=i;
         % retrieve quarterly shock decomp
-        [z, ~] = plot_shock_decomposition(M_,oo_,myopts,[]);
+        z = plot_shock_decomposition(M_,oo_,myopts,[]);
         zdim = size(z);
         z = z(i_var,:,:);
         if isstruct(aux)
@@ -185,7 +185,7 @@ if realtime_ && isstruct(oo_) && isfield(oo_, 'realtime_shock_decomposition')
             if qvintage_>i-4 && qvintage_<i
                 myopts.plot_shock_decomp.vintage=qvintage_;
                 % retrieve quarterly shock decomp
-                [z, ~] = plot_shock_decomposition(M_,oo_,myopts,[]);
+                z = plot_shock_decomposition(M_,oo_,myopts,[]);
                 z(:,:,end+1:zdim(3))=nan; % fill with nan's remaining time points to reach Q4
                 z = z(i_var,:,:);
                 if isstruct(aux)

matlab/shock_decomposition/plot_shock_decomposition.m
@@ -471,7 +471,7 @@ switch type
                 end
                 i_var0 = i_var;
                 steady_state_0 = steady_state;
-                [za, endo_names, endo_names_tex, steady_state, i_var, oo_] = ...
+                [za, endo_names, endo_names_tex, steady_state, i_var] = ...
                     annualized_shock_decomposition(z,M_, options_, i_var, t0, options_.nobs, realtime_, vintage_, steady_state,q2a);
                 if options_.plot_shock_decomp.interactive && ~isempty(options_.plot_shock_decomp.use_shock_groups)
                     mygroup = options_.plot_shock_decomp.use_shock_groups;

commit 2ed416532b271e9b1f60caf6c095d8db83a57c7c

annualized_shock_decomposition.m: fix bug introduced in 735bd66d
Use dedicated indicator instead of nargout to only request decomposition; closes #1919

matlab/shock_decomposition/annualized_shock_decomposition.m
@@ -129,7 +129,7 @@ if realtime_==0
         myopts=options_;
         myopts.plot_shock_decomp.type='qoq';
         myopts.plot_shock_decomp.realtime=0;
-        z = plot_shock_decomposition(M_,oo_,myopts,[]);
+        z = plot_shock_decomposition(M_,oo_,myopts,[],true);
     else
         z = oo_;
     end
@@ -139,7 +139,7 @@ if realtime_==0
             myopts=options_;
             myopts.plot_shock_decomp.type='qoq';
             myopts.plot_shock_decomp.realtime=0;
-            [y_aux, steady_state_aux] = plot_shock_decomposition(M_,oo_,myopts,aux.y);
+            [y_aux, steady_state_aux] = plot_shock_decomposition(M_,oo_,myopts,aux.y,true);
             aux.y=y_aux;
             aux.yss=steady_state_aux;
         end
@@ -158,13 +158,13 @@ if realtime_ && isstruct(oo_) && isfield(oo_, 'realtime_shock_decomposition')
         myopts.plot_shock_decomp.realtime=1;
         myopts.plot_shock_decomp.vintage=i;
         % retrieve quarterly shock decomp
-        z = plot_shock_decomposition(M_,oo_,myopts,[]);
+        z = plot_shock_decomposition(M_,oo_,myopts,[],true);
         zdim = size(z);
         z = z(i_var,:,:);
         if isstruct(aux)
             if ischar(aux0.y)
                 % retrieve quarterly shock decomp for aux variable
-                [y_aux, steady_state_aux] = plot_shock_decomposition(M_,oo_,myopts,aux0.y);
+                [y_aux, steady_state_aux] = plot_shock_decomposition(M_,oo_,myopts,aux0.y,true);
                 aux.y=y_aux;
                 aux.yss=steady_state_aux;
             end
@@ -185,13 +185,13 @@ if realtime_ && isstruct(oo_) && isfield(oo_, 'realtime_shock_decomposition')
             if qvintage_>i-4 && qvintage_<i
                 myopts.plot_shock_decomp.vintage=qvintage_;
                 % retrieve quarterly shock decomp
-                z = plot_shock_decomposition(M_,oo_,myopts,[]);
+                z = plot_shock_decomposition(M_,oo_,myopts,[],true);
                 z(:,:,end+1:zdim(3))=nan; % fill with nan's remaining time points to reach Q4
                 z = z(i_var,:,:);
                 if isstruct(aux)
                     if ischar(aux0.y)
                         % retrieve quarterly shock decomp for aux variable
-                        [y_aux, steady_state_aux] = plot_shock_decomposition(M_,oo_,myopts,aux0.y);
+                        [y_aux, steady_state_aux] = plot_shock_decomposition(M_,oo_,myopts,aux0.y,true);
                         y_aux(:,:,end+1:zdim(3))=nan; % fill with nan's remaining time points to reach Q4
                         aux.y=y_aux;
                         aux.yss=steady_state_aux;

matlab/shock_decomposition/plot_shock_decomposition.m
@@ -1,4 +1,4 @@
-function [out, steady_state] = plot_shock_decomposition(M_,oo_,options_,varlist)
+function [out, steady_state] = plot_shock_decomposition(M_,oo_,options_,varlist,get_decomp_only)
 % function plot_shock_decomposition(M_,oo_,options_,varlist)
 % Plots the results of shock_decomposition
 %
@@ -7,11 +7,12 @@ function [out, steady_state] = plot_shock_decomposition(M_,oo_,options_,varlist)
 %    oo_:         [structure]  Storage of results
 %    options_:    [structure]  Options
 %    varlist:     [char]       List of variables
-%
+%    get_decomp_only    [bool] indicator on whether to only return with
+%                               basic decomposition (required for e.g. annualized_shock_decomposition)
 % SPECIAL REQUIREMENTS
 %    none
 
-% Copyright © 2016-2019 Dynare Team
+% Copyright © 2016-2023 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -27,6 +28,10 @@ function [out, steady_state] = plot_shock_decomposition(M_,oo_,options_,varlist)
 %
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <https://www.gnu.org/licenses/>.
+
+if nargin<5
+    get_decomp_only=false;
+end
 
 options_.nodisplay = options_.plot_shock_decomp.nodisplay;
 options_.graph_format = options_.plot_shock_decomp.graph_format;
@@ -532,7 +537,7 @@ if steadystate
     options_.plot_shock_decomp.steady_state=steady_state;
 end
 
-if nargout == 2
+if get_decomp_only
     out=z(i_var,:,:);
     steady_state = steady_state(i_var);
     return