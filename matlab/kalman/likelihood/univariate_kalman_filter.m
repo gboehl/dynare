a9a28f4f2903326c8de98d1a2b4041bfe11dfa3e
Johannes Pfeifer <jpfeifer@gmx.de>
2024-02-21 20:48:36 +0100

    univariate_kalman_filter.m: minor clean-up

--- matlab/kalman/likelihood/univariate_kalman_filter.m
+++ matlab/kalman/likelihood/univariate_kalman_filter.m
@@ -126,7 +126,6 @@ QQ   = R*Q*transpose(R);   % Variance of R times the vector of structural innova
 t    = start;              % Initialization of the time index.
 lik  = zeros(smpl,pp);     % Initialization of the matrix gathering the densities at each time and each observable
 LIK  = Inf;                % Default value of the log likelihood.
-oldP = Inf;
 l2pi = log(2*pi);
 notsteady = 1;
 
@@ -149,11 +148,9 @@ else
     else
         C=Z;
     end
-    dC = zeros(pp,mm,k);   % either selection matrix or schur have zero derivatives
     if analytic_derivation==2
         Hess  = zeros(k,k);                             % Initialization of the Hessian
         D2a    = zeros(mm,k,k);                             % State vector.
-        d2C = zeros(pp,mm,k,k);
     else
         asy_hess=D2T;
         Hess=[];
@@ -178,7 +175,6 @@ while notsteady && t<=last %loop over t
     else
         z = Z(d_index);
     end
-    oldP = P(:);
     for i=1:rows(z) %loop over i
         if Zflag
             prediction_error = Y(d_index(i),t) - z(i,:)*a;  % nu_{t,i} in 6.13 in DK (2012)
